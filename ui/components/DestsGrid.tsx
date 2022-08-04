0bb17bd273f6f14e4586459b1904df12988cf85f
Eden Federman <eden@keyval.dev>
2022-08-04 13:48:36 +0300

    ui fixes

--- ui/components/DestsGrid.tsx
+++ ui/components/DestsGrid.tsx
@@ -10,7 +10,7 @@ export default function DestsGrid({ vendors, title }: DestsGridProps) {
   return (
     <div className="space-y-3">
       <div className="text-2xl font-medium">{title}</div>
-      <div className="grid lg:grid-cols-3 2xl:grid-cols-6 gap-4 pr-4">
+      <div className="grid lg:grid-cols-3 2xl:grid-cols-5 max-w-7xl gap-4 pr-4">
         {vendors.map((dest: ObservabilityVendor) => {
           return <DestCard key={dest.name} {...dest} />;
         })}
