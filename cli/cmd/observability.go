00b69688e4d96200d9efd909e78a3228039e7762
Eden Federman <eden@keyval.dev>
2022-10-08 17:15:31 +0300

    CLI first draft (#23)

---
+++ cli/cmd/observability.go
@@ -0,0 +1,71 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/keyval-dev/odigos/cli/cmd/observability/backend"
+	"github.com/spf13/cobra"
+)
+
+// observabilityCmd represents the observability command
+var observabilityCmd = &cobra.Command{
+	Use:   "observability",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := isValidBackend(backendFlag); err != nil {
+			return err
+		}
+
+		be := backend.Get(backendFlag)
+		if err := be.ValidateFlags(cmd); err != nil {
+			return err
+		}
+
+		fmt.Println("observability called")
+		return nil
+	},
+}
+
+var (
+	backendFlag string
+	apiKeyFlag  string
+	urlFlag     string
+)
+
+func isValidBackend(name string) error {
+	avail := backend.GetAvailableBackends()
+	for _, s := range avail {
+		if name == s {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("invalid backend %s, choose from %+v", name, avail)
+}
+
+func init() {
+	skillCmd.AddCommand(observabilityCmd)
+	observabilityCmd.Flags().StringVar(&backendFlag, "backend", "", "Backend for observability data")
+	//observabilityCmd.MarkFlagRequired("backend")
+	observabilityCmd.Flags().StringVarP(&urlFlag, "url", "u", "", "URL of the backend for observability data")
+	//observabilityCmd.MarkFlagRequired("url")
+	observabilityCmd.Flags().StringVar(&apiKeyFlag, "api-key", "", "API key for the selected backend")
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// observabilityCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// observabilityCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cli/cmd/resources/scheduler.go
@@ -0,0 +1,285 @@
+package resources
+
+import (
+	"fmt"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+const (
+	schedulerImage = "ghcr.io/keyval-dev/odigos/scheduler"
+)
+
+func NewSchedulerServiceAccount() *corev1.ServiceAccount {
+	return &corev1.ServiceAccount{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ServiceAccount",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "odigos-scheduler",
+		},
+	}
+}
+
+func NewSchedulerRoleBinding() *rbacv1.RoleBinding {
+	return &rbacv1.RoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "RoleBinding",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "odigos-scheduler-leader-election",
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind: "ServiceAccount",
+				Name: "odigos-scheduler",
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "Role",
+			Name:     "odigos-leader-election-role",
+		},
+	}
+}
+
+func NewSchedulerClusterRole() *rbacv1.ClusterRole {
+	return &rbacv1.ClusterRole{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ClusterRole",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "odigos-scheduler",
+		},
+		Rules: []rbacv1.PolicyRule{
+			{
+				Verbs: []string{
+					"create",
+					"delete",
+					"get",
+					"list",
+					"patch",
+					"update",
+					"watch",
+				},
+				APIGroups: []string{
+					"odigos.io",
+				},
+				Resources: []string{
+					"collectorsgroups",
+				},
+			},
+			{
+				Verbs: []string{
+					"update",
+				},
+				APIGroups: []string{
+					"odigos.io",
+				},
+				Resources: []string{
+					"collectorsgroups/finalizers",
+				},
+			},
+			{
+				Verbs: []string{
+					"get",
+					"patch",
+					"update",
+				},
+				APIGroups: []string{
+					"odigos.io",
+				},
+				Resources: []string{
+					"collectorsgroups/status",
+				},
+			},
+			{
+				Verbs: []string{
+					"create",
+					"delete",
+					"get",
+					"list",
+					"patch",
+					"update",
+					"watch",
+				},
+				APIGroups: []string{
+					"odigos.io",
+				},
+				Resources: []string{
+					"destinations",
+				},
+			},
+			{
+				Verbs: []string{
+					"update",
+				},
+				APIGroups: []string{
+					"odigos.io",
+				},
+				Resources: []string{
+					"destinations/finalizers",
+				},
+			},
+			{
+				Verbs: []string{
+					"get",
+					"patch",
+					"update",
+				},
+				APIGroups: []string{
+					"odigos.io",
+				},
+				Resources: []string{
+					"destinations/status",
+				},
+			},
+		},
+	}
+}
+
+func NewSchedulerClusterRoleBinding(ns string) *rbacv1.ClusterRoleBinding {
+	return &rbacv1.ClusterRoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ClusterRoleBinding",
+			APIVersion: "rbac.authorization.k8s.io/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "odigos-scheduler",
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      "odigos-scheduler",
+				Namespace: ns,
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     "odigos-scheduler",
+		},
+	}
+}
+
+func NewSchedulerDeployment(version string) *appsv1.Deployment {
+	return &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "odigos-scheduler",
+			Labels: map[string]string{
+				"app": "odigos-scheduler",
+			},
+			Annotations: map[string]string{
+				"odigos.io/skip": "true",
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: ptrint32(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "odigos-scheduler",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "odigos-scheduler",
+					},
+					Annotations: map[string]string{
+						"kubectl.kubernetes.io/default-container": "manager",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "apis-rbac-proxy",
+							Image: "gcr.io/kubebuilder/kube-rbac-proxy:v0.11.0",
+							Args: []string{
+								"--secure-listen-address=0.0.0.0:8443",
+								"--upstream=http://127.0.0.1:8080/",
+								"--logtostderr=true",
+								"--v=0",
+							},
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "https",
+									HostPort:      0,
+									ContainerPort: 8443,
+									Protocol:      "TCP",
+								},
+							},
+							Resources: corev1.ResourceRequirements{},
+						},
+						{
+							Name:  "manager",
+							Image: fmt.Sprintf("%s:%s", schedulerImage, version),
+							Command: []string{
+								"/app",
+							},
+							Args: []string{
+								"--health-probe-bind-address=:8081",
+								"--metrics-bind-address=127.0.0.1:8080",
+								"--leader-elect",
+							},
+							Env: []corev1.EnvVar{
+								{
+									Name: "CURRENT_NS",
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{
+											FieldPath: "metadata.namespace",
+										},
+									},
+								},
+							},
+							Resources: corev1.ResourceRequirements{
+								Limits: corev1.ResourceList{
+									"cpu":    resource.MustParse("500m"),
+									"memory": *resource.NewQuantity(134217728, resource.BinarySI),
+								},
+								Requests: corev1.ResourceList{
+									"cpu":    resource.MustParse("10m"),
+									"memory": *resource.NewQuantity(67108864, resource.BinarySI),
+								},
+							},
+							LivenessProbe: &corev1.Probe{
+								ProbeHandler: corev1.ProbeHandler{
+									HTTPGet: &corev1.HTTPGetAction{
+										Path: "/healthz",
+										Port: intstr.IntOrString{
+											Type:   intstr.Type(0),
+											IntVal: 8081,
+										},
+									},
+								},
+								InitialDelaySeconds: 15,
+								TimeoutSeconds:      0,
+								PeriodSeconds:       20,
+								SuccessThreshold:    0,
+								FailureThreshold:    0,
+							},
+							SecurityContext: &corev1.SecurityContext{},
+						},
+					},
+					TerminationGracePeriodSeconds: ptrint64(10),
+					ServiceAccountName:            "odigos-scheduler",
+					SecurityContext: &corev1.PodSecurityContext{
+						RunAsNonRoot: ptrbool(true),
+					},
+				},
+			},
+			Strategy:        appsv1.DeploymentStrategy{},
+			MinReadySeconds: 0,
+		},
+	}
+}
