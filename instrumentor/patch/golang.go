0caf5046a97e13d71c3209071f4d531fdc689b68
Eden Federman <eden@keyval.dev>
2023-02-13 14:42:10 -0800

    update go agent to version v0.6.3

--- instrumentor/patch/golang.go
+++ instrumentor/patch/golang.go
@@ -12,7 +12,7 @@ import (
 const (
 	golangKernelDebugVolumeName = "kernel-debug"
 	golangKernelDebugHostPath   = "/sys/kernel/debug"
-	golangAgentName             = "keyval/otel-go-agent:v0.6.2"
+	golangAgentName             = "keyval/otel-go-agent:v0.6.3"
 	golangExporterEndpoint      = "OTEL_EXPORTER_OTLP_ENDPOINT"
 	golangServiceNameEnv        = "OTEL_SERVICE_NAME"
 	golangTargetExeEnv          = "OTEL_TARGET_EXE"
