15a1793f1a4700ad1f1d61bd5e5c891b1c115e85
d-lite <olabemiwodaniel@gmail.com>
2023-04-29 00:26:04 +0100

    feat: add destination search functions to odigos destination page

--- ui/pages/dest/new/index.tsx
+++ ui/pages/dest/new/index.tsx
@@ -1,25 +1,81 @@
 import DestsGrid from "@/components/DestsGrid";
 import { getConfiguration } from "@/utils/config";
 import Vendors, { VendorType } from "@/vendors/index";
+import { Combobox } from "@headlessui/react";
 import type { NextPage } from "next";
+import { useState } from "react";
 
 const AddNewDestinationPage: NextPage = () => {
+
+  const people = [
+  'Durward Reynolds',
+  'Kenton Towne',
+  'Therese Wunsch',
+  'Benedict Kessler',
+  'Katelyn Rohan',
+]
+
+
+  const [selectedDest, setSelectedDest] = useState(Vendors[0])
+  const [query, setQuery] = useState('')
+
+  const filteredDest =
+    query === ''
+      ? Vendors
+      : Vendors.filter((vendor) => {
+          return vendor.displayName.toLowerCase().includes(query.toLowerCase()) || vendor.name.toLowerCase().includes(query.toLowerCase())
+        })
+      
+
   return (
     <div className="flex flex-col">
+      <div className="flex flex-col md:flex-row">
       <div className="text-4xl font-medium">Add New Destination</div>
-      <div className="text-2xl mt-4 mb-6">
+      <div className="mx-1 md:mx-20 z-30 bg-white relative">
+      <Combobox value={selectedDest} onChange={setSelectedDest}>
+        <Combobox.Input className="w-full md:w-[600px] rounded-[2px] overflow-hidden list-none" placeholder="Search a destination" onChange={(event) => setQuery(event.target.value)} />
+          <Combobox.Options>
+            {filteredDest.map((vendor) => (
+              <Combobox.Option onClick={() => setQuery(vendor.name)} key={vendor?.name} value={vendor.name}>
+                {vendor.name}
+              </Combobox.Option>
+            ))}
+          </Combobox.Options>
+      </Combobox>
+      </div>
+      
+      </div>
+      <div className="text-2xl mt-24 mb-6 absolute">
         Choose an observability backend from the list
       </div>
-      <div className="space-y-10">
-        <DestsGrid
-          vendors={Vendors.filter((v) => v.type === VendorType.MANAGED)}
-          title="Managed"
-        />
-        <DestsGrid
-          vendors={Vendors.filter((v) => v.type === VendorType.HOSTED)}
-          title="Self-hosted"
-        />
-      </div>
+
+      {query && 
+          <div className="space-y-10 absolute mt-36">
+              <>
+                <DestsGrid
+                  vendors={filteredDest.filter((v) => v.type === VendorType.MANAGED)}
+                  title="Managed"
+                />   
+                <DestsGrid
+                  vendors={filteredDest.filter((v) => v.type === VendorType.HOSTED)}
+                  title="Self-hosted"
+                />
+              </>
+          </div>
+      }
+
+      { !query && 
+        <div className="space-y-10 absolute mt-36">
+          <DestsGrid
+            vendors={Vendors.filter((v) => v.type === VendorType.MANAGED)}
+            title="Managed"
+          />
+          <DestsGrid
+            vendors={Vendors.filter((v) => v.type === VendorType.HOSTED)}
+            title="Self-hosted"
+          />
+        </div>
+      }
     </div>
   );
 };
