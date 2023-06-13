a8508a52d8591472e0386df42778fff769d0c763
Eden Federman <eden@keyval.dev>
2023-06-13 13:58:57 +0300

    standalone frontend

---
+++ frontend/cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	assetsDir = "webapp/out"
+	port      = 3000
+)
+
+func main() {
+	// Serve all files in `web/` directory
+	http.Handle("/", http.FileServer(http.Dir(assetsDir)))
+	http.Handle("/api/test", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Write json response
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"message": "Hello world!"}`))
+	}))
+
+	// Start server
+	log.Println("Starting frontend server ...")
+	log.Printf("Frontend is available at http://localhost:%d", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Printf("Error starting server: %s", err)
+		os.Exit(-1)
+	}
+}
