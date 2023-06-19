4ed3e566116b4ccd3f94772dcbd5e0c0a515c750
Eden Federman <eden@keyval.dev>
2023-06-19 14:38:08 +0300

    Update python instrumentation to 0.39b0 (#250)
    This PR updates the OpenTelemetry Python instrumentation to version
    `0.39b0`

--- odiglet/pkg/instrumentation/instrumentlang/python.go
+++ odiglet/pkg/instrumentation/instrumentlang/python.go
@@ -2,32 +2,35 @@ package instrumentlang
 
 import (
 	"fmt"
+
 	"github.com/keyval-dev/odigos/odiglet/pkg/env"
 	"github.com/keyval-dev/odigos/odiglet/pkg/instrumentation/consts"
 	"k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
 )
 
 const (
-	pythonVolumeName        = "agentdir-python"
-	pythonMountPath         = "/otel-auto-instrumentation"
-	envOtelTracesExporter   = "OTEL_TRACES_EXPORTER"
-	envOtelMetricsExporter  = "OTEL_METRICS_EXPORTER"
-	envValOtelHttpExporter  = "otlp_proto_http"
-	envLogCorrelation       = "OTEL_PYTHON_LOG_CORRELATION"
-	pythonInitContainerName = "copy-python-agent"
-	envPythonPath           = "PYTHONPATH"
+	envOtelTracesExporter              = "OTEL_TRACES_EXPORTER"
+	envOtelMetricsExporter             = "OTEL_METRICS_EXPORTER"
+	envValOtelHttpExporter             = "otlp"
+	envLogCorrelation                  = "OTEL_PYTHON_LOG_CORRELATION"
+	envPythonPath                      = "PYTHONPATH"
+	envOtelExporterOTLPTracesProtocol  = "OTEL_EXPORTER_OTLP_TRACES_PROTOCOL"
+	envOtelExporterOTLPMetricsProtocol = "OTEL_EXPORTER_OTLP_METRICS_PROTOCOL"
+	httpProtobufProtocol               = "http/protobuf"
 )
 
 func Python(deviceId string) *v1beta1.ContainerAllocateResponse {
 	otlpEndpoint := fmt.Sprintf("http://%s:%d", env.Current.NodeIP, consts.OTLPHttpPort)
 	return &v1beta1.ContainerAllocateResponse{
 		Envs: map[string]string{
-			envLogCorrelation:             "true",
-			envPythonPath:                 "/var/odigos/python/opentelemetry/instrumentation/auto_instrumentation:/var/odigos/python",
-			"OTEL_EXPORTER_OTLP_ENDPOINT": otlpEndpoint,
-			"OTEL_RESOURCE_ATTRIBUTES":    fmt.Sprintf("service.name=%s,odigos.device=python", deviceId),
-			envOtelTracesExporter:         envValOtelHttpExporter,
-			envOtelMetricsExporter:        "",
+			envLogCorrelation:                  "true",
+			envPythonPath:                      "/var/odigos/python/opentelemetry/instrumentation/auto_instrumentation:/var/odigos/python",
+			"OTEL_EXPORTER_OTLP_ENDPOINT":      otlpEndpoint,
+			"OTEL_RESOURCE_ATTRIBUTES":         fmt.Sprintf("service.name=%s,odigos.device=python", deviceId),
+			envOtelTracesExporter:              envValOtelHttpExporter,
+			envOtelMetricsExporter:             envValOtelHttpExporter,
+			envOtelExporterOTLPTracesProtocol:  httpProtobufProtocol,
+			envOtelExporterOTLPMetricsProtocol: httpProtobufProtocol,
 		},
 		Mounts: []*v1beta1.Mount{
 			{
