e0d856bbd1e426c3d6b5454476f4e9b3b9ff1fca
Eden Federman <eden@keyval.dev>
2023-06-26 15:56:46 +0300

    add port and address args

--- frontend/cmd/main.go
+++ frontend/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,15 +11,16 @@ import (
 //go:generate yarn --cwd ../webapp build
 
 const (
-	assetsDir = "webapp/out"
-	port      = 3000
+	assetsDir   = "webapp/out"
+	defaultPort = 3000
 )
 
 func main() {
-	//var addressFlag string
-	//var portFlag int
-	//flag.StringVar(&addressFlag, "address", "localhost", "Address to listen on")
-	//flag.IntVar(&portFlag, "port", port, "Port to listen on")
+	var addressFlag string
+	var portFlag int
+	flag.StringVar(&addressFlag, "address", "localhost", "Address to listen on")
+	flag.IntVar(&portFlag, "port", defaultPort, "Port to listen on")
+	flag.Parse()
 
 	// Serve all files in `web/` directory
 	http.Handle("/", http.FileServer(http.Dir(assetsDir)))
@@ -30,8 +32,8 @@ func main() {
 
 	// Start server
 	log.Println("Starting Odigos UI...")
-	log.Printf("Odigos UI is available at: http://localhost:%d", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	log.Printf("Odigos UI is available at: http://%s:%d", addressFlag, portFlag)
+	err := http.ListenAndServe(fmt.Sprintf("%s:%d", addressFlag, portFlag), nil)
 	if err != nil {
 		log.Printf("Error starting server: %s", err)
 		os.Exit(-1)
