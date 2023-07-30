3f0700a90cdd81b24a269cde73c8f6c401765944
alonkeyval <alon@keyval.dev>
2023-07-30 12:38:53 +0300

    WIP

--- frontend/webapp/containers/overview/sources/sources.tsx
+++ frontend/webapp/containers/overview/sources/sources.tsx
@@ -1,42 +1,18 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { KeyvalLoader } from "@/design.system";
-import { NOTIFICATION, OVERVIEW, QUERIES } from "@/utils/constants";
+import { OVERVIEW, QUERIES } from "@/utils/constants";
 import { useQuery } from "react-query";
 import { getSources } from "@/services";
 import { OverviewHeader } from "@/components/overview";
-import { useNotification } from "@/hooks";
 import { SourcesContainerWrapper } from "./sources.styled";
 
 export function SourcesContainer() {
-  const { show, Notification } = useNotification();
-
   const {
     data: sources,
     refetch,
     isLoading,
   } = useQuery([QUERIES.API_SOURCES], getSources);
-
-  useEffect(() => {
-    console.log({ sources });
-  }, [sources]);
-
-  function onSuccess(message = OVERVIEW.DESTINATION_UPDATE_SUCCESS) {
-    refetch();
-
-    show({
-      type: NOTIFICATION.SUCCESS,
-      message,
-    });
-  }
-
-  function onError({ response }) {
-    const message = response?.data?.message;
-    show({
-      type: NOTIFICATION.ERROR,
-      message,
-    });
-  }
 
   if (isLoading) {
     return <KeyvalLoader />;
@@ -45,7 +21,6 @@ export function SourcesContainer() {
   return (
     <SourcesContainerWrapper>
       <OverviewHeader title={OVERVIEW.MENU.SOURCES} />
-      <Notification />
     </SourcesContainerWrapper>
   );
 }
