e06928feae73febaaace075105f77d9e39f6f78c
Amir Blum <amirgiraffe@gmail.com>
2023-08-01 11:05:43 +0300

    fix: do not attempt to instrument system namespaces

--- frontend/endpoints/utils.go
+++ frontend/endpoints/utils.go
@@ -1,9 +1,21 @@
 package endpoints
 
-import "path"
+import (
+	"path"
+
+	"github.com/keyval-dev/odigos/common/consts"
+)
 
 const cdnUrl = "https://d15jtxgb40qetw.cloudfront.net"
 
 func GetImageURL(image string) string {
 	return path.Join(cdnUrl, image)
+}
+
+func IsSystemNamespace(namespace string) bool {
+	return namespace == "kube-system" ||
+		namespace == consts.DefaultNamespace ||
+		namespace == "local-path-storage" ||
+		namespace == "istio-system" ||
+		namespace == "linkerd"
 }
