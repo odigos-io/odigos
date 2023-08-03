4c2847220a06aabfc0410105af75160631640742
alonkeyval <alon@keyval.dev>
2023-08-03 16:52:40 +0300

    move page logic to container

--- frontend/webapp/containers/overview/index.tsx
+++ frontend/webapp/containers/overview/index.tsx
@@ -2,3 +2,4 @@ export { OverviewContainer } from "./overview/overview";
 export { DestinationContainer } from "./destination/destination";
 export { SourcesContainer } from "./sources/sources";
 export { NewDestinationFlow } from "./destination/new.destination.flow";
+export { NewDestinationForm } from "./destination/new.destination.form";
