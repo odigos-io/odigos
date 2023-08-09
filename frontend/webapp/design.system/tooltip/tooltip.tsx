ada2ecd49832c6b8533aa8e33af0461eed6154a2
alonkeyval <alon@keyval.dev>
2023-08-09 12:04:20 +0300

    migrate video component

--- frontend/webapp/design.system/tooltip/tooltip.tsx
+++ frontend/webapp/design.system/tooltip/tooltip.tsx
@@ -1,33 +1,6 @@
-import React, { useState } from "react";
-import Question from "@/assets/icons/question.svg";
-import { TooltipContentWrapper, TooltipWrapper } from "./tooltip.styled";
-import { KeyvalText } from "../text/text";
+import React from "react";
+import { Tooltip } from "@keyval-org/design-system";
 
 export function KeyvalTooltip(props: any) {
-  let timeout: ReturnType<typeof setTimeout>;
-  const [active, setActive] = useState(false);
-
-  const showTip = () => {
-    timeout = setTimeout(() => {
-      setActive(true);
-    }, props.delay || 400);
-  };
-
-  const hideTip = () => {
-    clearInterval(timeout);
-    setActive(false);
-  };
-
-  return (
-    <TooltipWrapper onMouseEnter={showTip} onMouseLeave={hideTip}>
-      {active && (
-        <TooltipContentWrapper>
-          <KeyvalText size={12} weight={500}>
-            {props.content}
-          </KeyvalText>
-        </TooltipContentWrapper>
-      )}
-      <Question />
-    </TooltipWrapper>
-  );
+  return <Tooltip {...props} />;
 }
