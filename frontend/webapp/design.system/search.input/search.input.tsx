f1ba02b0c1d3a5a0ae3ce0bbcb18c3549638eddc
alonkeyval <alon@keyval.dev>
2023-08-09 11:39:55 +0300

    migrate search input and counters

--- frontend/webapp/design.system/search.input/search.input.tsx
+++ frontend/webapp/design.system/search.input/search.input.tsx
@@ -1,14 +1,5 @@
 import React from "react";
-import {
-  LoaderWrapper,
-  SearchInputWrapper,
-  StyledSearchInput,
-} from "./search.input.styled";
-import Glass from "@/assets/icons/glass.svg";
-import X from "@/assets/icons/X.svg";
-import { KeyvalLottie } from "../lottie/lottie";
-import loader from "@/assets/lotties/loader.json";
-import { SETUP } from "@/utils/constants";
+import { SearchInput } from "@keyval-org/design-system";
 interface KeyvalSearchInputProps {
   placeholder?: string;
   value?: string;
@@ -19,49 +10,6 @@ interface KeyvalSearchInputProps {
   showClear?: boolean;
 }
 
-export function KeyvalSearchInput({
-  placeholder = SETUP.MENU.SEARCH_PLACEHOLDER,
-  value = "",
-  onChange = () => {},
-  loading = false,
-  containerStyle = {},
-  inputStyle = {},
-  showClear = true,
-}: KeyvalSearchInputProps) {
-  const clear = value
-    ? () =>
-        onChange({
-          target: {
-            value: "",
-          },
-        })
-    : null;
-
-  return (
-    <SearchInputWrapper
-      active={!!value || undefined}
-      style={{ ...containerStyle }}
-    >
-      <Glass />
-      <StyledSearchInput
-        style={{ ...inputStyle }}
-        value={value}
-        active={!!value || undefined}
-        placeholder={placeholder}
-        onChange={onChange}
-      />
-      {loading && (
-        <LoaderWrapper>
-          <KeyvalLottie
-            animationData={loader}
-            autoplay
-            loop
-            width={26}
-            height={26}
-          />
-        </LoaderWrapper>
-      )}
-      {showClear && <X onClick={clear} style={{ cursor: "pointer" }} />}
-    </SearchInputWrapper>
-  );
+export function KeyvalSearchInput(props: KeyvalSearchInputProps) {
+  return <SearchInput {...props} />;
 }
