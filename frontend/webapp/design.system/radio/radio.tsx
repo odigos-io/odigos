2c258eb139345cba5f57ff32f7f2f37a0439c6da
alonkeyval <alon@keyval.dev>
2023-08-09 11:35:45 +0300

    radio component migrate

--- frontend/webapp/design.system/radio/radio.tsx
+++ frontend/webapp/design.system/radio/radio.tsx
@@ -1,28 +1,12 @@
-import React, { useState, FC, ChangeEvent } from "react";
-import { RadioButtonContainer, RadioButtonBorder } from "./radio.styled";
-import { KeyvalText } from "@/design.system/text/text";
-import Checked from "assets/icons/checked-radio.svg";
+import React, { FC, ChangeEvent } from "react";
+
+import { RadioButton } from "@keyval-org/design-system";
 interface RadioButtonProps {
   label?: string;
   value?: string;
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const KeyvalRadioButton: FC<RadioButtonProps> = ({
-  label = "",
-  onChange,
-  value,
-}) => {
-  function handleChange() {
-    onChange && onChange({} as ChangeEvent<HTMLInputElement>);
-  }
-
-  return (
-    <RadioButtonContainer>
-      <div onClick={handleChange}>
-        {value ? <Checked width={25} height={25} /> : <RadioButtonBorder />}
-      </div>
-      <KeyvalText>{label}</KeyvalText>
-    </RadioButtonContainer>
-  );
+export const KeyvalRadioButton: FC<RadioButtonProps> = (props) => {
+  return <RadioButton {...props} />;
 };
