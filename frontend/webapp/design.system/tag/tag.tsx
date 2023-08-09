aed124840de3d5ca483ce953b70401c403a08918
alonkeyval <alon@keyval.dev>
2023-08-09 11:54:38 +0300

    migrate tag and tap

--- frontend/webapp/design.system/tag/tag.tsx
+++ frontend/webapp/design.system/tag/tag.tsx
@@ -1,26 +1,11 @@
 import React from "react";
-import styled from "styled-components";
-import { KeyvalText } from "../text/text";
+import { Tag } from "@keyval-org/design-system";
 
 interface TagProps {
   title: string;
   color?: string;
 }
 
-const TagWrapper = styled.div`
-  display: flex;
-  padding: 4px 8px;
-  align-items: flex-start;
-  gap: 10px;
-  border-radius: 10px;
-`;
-
 export function KeyvalTag({ title = "", color = "#033869" }: TagProps) {
-  return (
-    <TagWrapper style={{ backgroundColor: color }}>
-      <KeyvalText weight={500} size={13} color={"#CCD0D2"}>
-        {title}
-      </KeyvalText>
-    </TagWrapper>
-  );
+  return <Tag color={color} title={title} />;
 }
