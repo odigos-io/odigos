994fdf8e93de8daf107cb87446a487d761082620
alonkeyval <alon@keyval.dev>
2023-08-09 11:14:12 +0300

    migrate link component

--- frontend/webapp/design.system/link/link.tsx
+++ frontend/webapp/design.system/link/link.tsx
@@ -1,23 +1,11 @@
 import React from "react";
-import { KeyvalText } from "../text/text";
-import { styled } from "styled-components";
+import { Link } from "@keyval-org/design-system";
 
 interface KeyvalLinkProps {
   value: string;
   onClick?: () => void;
 }
 
-const LinkContainer = styled.div`
-  cursor: pointer;
-  .p {
-    cursor: pointer !important;
-  }
-`;
-
-export function KeyvalLink({ value, onClick }: KeyvalLinkProps) {
-  return (
-    <LinkContainer onClick={onClick}>
-      <KeyvalText color="#0EE6F3">{value}</KeyvalText>
-    </LinkContainer>
-  );
+export function KeyvalLink(props: KeyvalLinkProps) {
+  return <Link {...props} />;
 }
