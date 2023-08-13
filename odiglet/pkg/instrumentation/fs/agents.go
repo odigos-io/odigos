0aef0bd58a33ff454b00cbe8056ebc37b24051e7
Amir Blum <amirgiraffe@gmail.com>
2023-08-13 11:27:57 +0300

    fix: delete the content of /var/odigos before copying into it

--- odiglet/pkg/instrumentation/fs/agents.go
+++ odiglet/pkg/instrumentation/fs/agents.go
@@ -1,6 +1,9 @@
 package fs
 
 import (
+	"os"
+	"path/filepath"
+
 	cp "github.com/otiai10/copy"
 )
 
@@ -10,9 +13,26 @@ const (
 )
 
 func CopyAgentsDirectoryToHost() error {
-	return cp.Copy(containerDir, hostDir, cp.Options{
-		OnDirExists: func(_, _ string) cp.DirExistsAction {
-			return cp.Replace
-		},
-	})
+
+	// remove the current content of /var/odigos
+	// as we want a fresh copy of instrumentation agents with no previous content from previous versions.
+	// we cannot remove /var/odigos itself: "unlinkat /var/odigos: device or resource busy"
+	// so we will just remove it's content
+	entries, err := os.ReadDir(hostDir)
+	if err != nil {
+		return err
+	}
+	for _, entry := range entries {
+		entryPath := filepath.Join(hostDir, entry.Name())
+		err := os.RemoveAll(entryPath)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = cp.Copy(containerDir, hostDir, cp.Options{})
+	if err != nil {
+		return err
+	}
+	return nil
 }
