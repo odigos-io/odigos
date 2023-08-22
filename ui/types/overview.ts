992da4dfd80acb7411fbb9f580ad463c9ba10d8a
Eden Federman <eden@keyval.dev>
2023-08-22 09:43:54 +0300

    Remove old UI (#431)
    Odigos moved to use a completely new UI located at the `frontend`
    directory

--- ui/types/overview.ts
+++
@@ -1,9 +0,0 @@
-import { Collector } from "@/types/collectors";
-import { ApplicationData } from "@/types/apps";
-import { OverviewDestResponseItem } from "@/types/dests";
-
-export interface OverviewApiResponse {
-  collectors: Collector[];
-  dests: OverviewDestResponseItem[];
-  sources: ApplicationData[];
-}
