04d382b33d71651896bd3227a1e762025cf546c3
HeetVekariya <heetvekariya50@gmail.com>
2023-10-19 21:13:58 +0530

    refactor: remove unused import

--- cli/cmd/uninstall.go
+++ cli/cmd/uninstall.go
@@ -14,7 +14,6 @@ import (
 	"github.com/keyval-dev/odigos/cli/pkg/log"
 	"github.com/keyval-dev/odigos/common/consts"
 	v1 "k8s.io/api/core/v1"
-	// "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 
