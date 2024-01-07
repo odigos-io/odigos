bbd61a7ab0edf0bd150f50de798b352c138d650e
Amir Blum <amirgiraffe@gmail.com>
2024-01-07 09:32:26 +0200

    test(instrumentor): refactor deployment tests and add namespace controller tests (#875)
    Co-authored-by: Eden Federman <eden@keyval.dev>

---
+++ instrumentor/internal/testutil/helpers.go
@@ -0,0 +1,30 @@
+package testutil
+
+import (
+	"github.com/keyval-dev/odigos/common/consts"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func SetOdigosInstrumentationEnabled[W client.Object](obj W) W {
+	copy := obj.DeepCopyObject().(W)
+	copy.SetLabels(map[string]string{consts.OdigosInstrumentationLabel: consts.InstrumentationEnabled})
+	return copy
+}
+
+func SetOdigosInstrumentationDisabled[W client.Object](obj W) W {
+	copy := obj.DeepCopyObject().(W)
+	copy.SetLabels(map[string]string{consts.OdigosInstrumentationLabel: consts.InstrumentationDisabled})
+	return copy
+}
+
+func DeleteOdigosInstrumentationLabel[W client.Object](obj W) W {
+	copy := obj.DeepCopyObject().(W)
+	delete(copy.GetLabels(), consts.OdigosInstrumentationLabel)
+	return copy
+}
+
+func SetReportedNameAnnotation[W client.Object](obj W, reportedName string) W {
+	copy := obj.DeepCopyObject().(W)
+	copy.SetAnnotations(map[string]string{consts.OdigosReportedNameAnnotation: reportedName})
+	return copy
+}
