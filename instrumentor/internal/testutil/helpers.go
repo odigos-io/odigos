e8a99b8eca6d3fbafd7468df679743398bd25fd4
Amir Blum <amirgiraffe@gmail.com>
2024-01-07 07:22:26 +0200

    chore: removed unused helper functions

--- instrumentor/internal/testutil/helpers.go
+++ instrumentor/internal/testutil/helpers.go
@@ -1,40 +1,9 @@
 package testutil
 
 import (
-	"context"
-
 	"github.com/keyval-dev/odigos/common/consts"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
-
-func DeleteNsLabels(ctx context.Context, k8sClient client.Client, nsName string) error {
-	return k8sClient.Patch(ctx, &corev1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: nsName,
-		},
-	}, client.RawPatch(types.MergePatchType, []byte(`{
-		"metadata": {
-			"labels": null
-		}
-	}`)))
-}
-
-func SetNsInstrumentationEnabled(ctx context.Context, k8sClient client.Client, nsName string) error {
-	return k8sClient.Patch(ctx, &corev1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: nsName,
-		},
-	}, client.RawPatch(types.MergePatchType, []byte(`{
-		"metadata": {
-			"labels": {
-				"`+consts.OdigosInstrumentationLabel+`": "`+consts.InstrumentationEnabled+`"
-			}
-		}
-	}`)))
-}
 
 func SetOdigosInstrumentationEnabled[W client.Object](obj W) W {
 	copy := obj.DeepCopyObject().(W)
