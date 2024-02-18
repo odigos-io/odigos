7073756d6a4917fbd92e5191b639443547373eaf
alonkeyval <alon@keyval.dev>
2024-02-18 13:30:30 +0200

    chore: wip

--- frontend/webapp/components/setup/sources/sources.list/sources.list.styled.tsx
+++ frontend/webapp/components/setup/sources/sources.list/sources.list.styled.tsx
@@ -17,31 +17,14 @@ export const SourcesTitleWrapper = styled.div`
   margin: 2% 0;
 `;
 
-export const SourcesListWrapper = styled.div<{ repeat: number }>`
-  width: 100%;
-  height: 400px;
-  padding-bottom: 300px;
-  gap: 1vh;
-  overflow-y: scroll;
-  scrollbar-width: none;
-  -ms-overflow-style: none;
+export const SourcesListWrapper = styled.div`
   display: grid;
-  grid-template-columns: ${({ repeat }) => `repeat(${repeat},1fr)`};
+  grid-template-columns: repeat(auto-fill, minmax(242px, 1fr));
+  gap: 24px;
 
-  ::-webkit-scrollbar {
-    display: none;
-  }
-
-  @media screen and (max-width: 1750px) {
-    grid-template-columns: repeat(4, 1fr);
-  }
-
-  @media screen and (max-width: 1500px) {
-    grid-template-columns: repeat(3, 1fr);
-    height: 300px;
-  }
-  @media screen and (max-width: 1150px) {
-    grid-template-columns: repeat(2, 1fr);
-    height: 200px;
+  height: 56vh; /* Example fixed height */
+  overflow-y: auto; /* Enables vertical scrolling if content overflows */
+  @media screen and (max-width: 1200px) {
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   }
 `;
