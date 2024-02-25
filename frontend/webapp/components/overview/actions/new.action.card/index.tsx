49e21672e3415b529a7c96b092fd3c0ccb92bf2e
alonkeyval <alon@keyval.dev>
2024-02-25 15:39:07 +0200

    chore: render action list

--- frontend/webapp/components/overview/actions/new.action.card/index.tsx
+++ frontend/webapp/components/overview/actions/new.action.card/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
+import theme from '@/styles/palette';
 import styled from 'styled-components';
+import { ACTION_ICONS } from '@/assets';
 import { ActionItemCard } from '@/types';
 import { KeyvalCard, KeyvalText } from '@/design.system';
-import theme from '@/styles/palette';
-import { ACTION_ICONS } from '@/assets';
 
 interface NewActionCardProps {
   item: ActionItemCard;
+  onClick: () => void;
 }
 
 const CardContentWrapper = styled.div`
   padding: 12px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  max-width: 220px;
+  height: 100%;
+  gap: 8px;
   border: 1px solid transparent;
   cursor: pointer;
   &:hover {
@@ -23,12 +23,13 @@ const CardContentWrapper = styled.div`
     border: 1px solid ${theme.colors.secondary};
   }
 `;
-export function NewActionCard({ item }: NewActionCardProps) {
+
+export function NewActionCard({ item, onClick }: NewActionCardProps) {
   const SvgIcon = ACTION_ICONS[item.icon];
 
   return (
     <KeyvalCard>
-      <CardContentWrapper>
+      <CardContentWrapper onClick={onClick}>
         <SvgIcon style={{ width: 56, height: 56 }} />
         <KeyvalText size={18} weight={700}>
           {item.title}
--- frontend/webapp/containers/overview/actions/choose-action/index.tsx
+++ frontend/webapp/containers/overview/actions/choose-action/index.tsx
@@ -1,5 +1,9 @@
-import { NewActionCard } from '@/components';
 import React from 'react';
+import styled from 'styled-components';
+import { NewActionCard } from '@/components';
+import { KeyvalLink, KeyvalText } from '@/design.system';
+
+const ACTION_DOCS_LINK = 'https://docs.odigos.io/pipeline/actions/introduction';
 
 const ITEMS = [
   {
@@ -19,16 +23,61 @@ const ITEMS = [
   },
 ];
 
+const ActionsListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+  gap: 16px;
+  padding: 0 24px 24px 24px;
+  overflow-y: auto;
+  align-items: start;
+  max-height: 100%;
+  padding-bottom: 220px;
+  box-sizing: border-box;
+`;
+
+const DescriptionWrapper = styled.div`
+  padding: 24px;
+  gap: 4px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+
+const LinkWrapper = styled.div`
+  width: 100px;
+`;
+
 export function ChooseActionContainer() {
+  function onItemClick() {
+    console.log('Item clicked');
+  }
+
   function renderActionsList() {
     return ITEMS.map((item) => {
       return (
-        <div key={item.id}>
-          <NewActionCard item={item} />
+        <div style={{ height: '100%', maxHeight: 220 }} key={item.id}>
+          <NewActionCard item={item} onClick={onItemClick} />
         </div>
       );
     });
   }
 
-  return <>{renderActionsList()}</>;
+  return (
+    <>
+      <DescriptionWrapper>
+        <KeyvalText>
+          {
+            'Actions are a way to modify the OpenTelemetry data recorded by Odigos Sources, before it is exported to your Odigos Destinations.'
+          }
+        </KeyvalText>
+        <LinkWrapper>
+          <KeyvalLink
+            value="Learn more"
+            onClick={() => window.open(ACTION_DOCS_LINK, '_blank')}
+          />
+        </LinkWrapper>
+      </DescriptionWrapper>
+      <ActionsListWrapper>{renderActionsList()}</ActionsListWrapper>
+    </>
+  );
 }
