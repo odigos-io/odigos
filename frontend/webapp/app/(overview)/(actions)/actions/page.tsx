574f68be971cfa2296dfb8ce2968115acb92bd1e
alonkeyval <alon@keyval.dev>
2024-02-28 09:44:38 +0200

    chore: wip

--- frontend/webapp/app/(overview)/(actions)/actions/page.tsx
+++ frontend/webapp/app/(overview)/(actions)/actions/page.tsx
@@ -5,6 +5,7 @@ import { OverviewHeader } from '@/components';
 import { ManagedActionsContainer } from '@/containers';
 
 export default function ActionsPage() {
+  //
   return (
     <>
       <OverviewHeader title={OVERVIEW.MENU.ACTIONS} />
