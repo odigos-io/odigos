cc1d2fe5c71045bbb71b237de22124791cc4103b
Amir Blum <amirgiraffe@gmail.com>
2024-03-03 17:20:15 +0200

    feat: add generic otlp destination to odigos ui (#1035)

--- autoscaler/controllers/gateway/config/genericotlp.go
+++ autoscaler/controllers/gateway/config/genericotlp.go
@@ -4,10 +4,11 @@ import (
 	odigosv1 "github.com/keyval-dev/odigos/api/odigos/v1alpha1"
 	commonconf "github.com/keyval-dev/odigos/autoscaler/controllers/common"
 	"github.com/keyval-dev/odigos/common"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 const (
-	genericOtlpUrlKey = "OTLP_URL"
+	genericOtlpUrlKey = "OTLP_GRPC_ENDPOINT"
 )
 
 type GenericOTLP struct{}
@@ -17,33 +18,45 @@ func (g *GenericOTLP) DestType() common.DestinationType {
 }
 
 func (g *GenericOTLP) ModifyConfig(dest *odigosv1.Destination, currentConfig *commonconf.Config) {
-	if url, exists := dest.Spec.Data[genericOtlpUrlKey]; exists {
-		genericOtlpExporterName := "otlp/generic-" + dest.Name
-		currentConfig.Exporters[genericOtlpExporterName] = commonconf.GenericMap{
-			"endpoint": url,
-			"tls": commonconf.GenericMap{
-				"insecure": true,
-			},
-		}
-		if isTracingEnabled(dest) {
-			tracesPipelineName := "traces/generic-" + dest.Name
-			currentConfig.Service.Pipelines[tracesPipelineName] = commonconf.Pipeline{
-				Exporters: []string{genericOtlpExporterName},
-			}
-		}
 
-		if isMetricsEnabled(dest) {
-			metricsPipelineName := "metrics/generic-" + dest.Name
-			currentConfig.Service.Pipelines[metricsPipelineName] = commonconf.Pipeline{
-				Exporters: []string{genericOtlpExporterName},
-			}
-		}
+	url, exists := dest.Spec.Data[genericOtlpUrlKey]
+	if !exists {
+		log.Log.V(0).Info("Generic OTLP gRPC endpoint not specified, gateway will not be configured for otlp")
+		return
+	}
 
-		if isLoggingEnabled(dest) {
-			logsPipelineName := "logs/generic-" + dest.Name
-			currentConfig.Service.Pipelines[logsPipelineName] = commonconf.Pipeline{
-				Exporters: []string{genericOtlpExporterName},
-			}
+	grpcEndpoint, err := parseUnencryptedOtlpGrpcUrl(url)
+	if err != nil {
+		log.Log.V(0).Error(err, "otlp endpoint invalid, gateway will not be configured for otlp")
+		return
+	}
+
+	genericOtlpExporterName := "otlp/generic-" + dest.Name
+	currentConfig.Exporters[genericOtlpExporterName] = commonconf.GenericMap{
+		"endpoint": grpcEndpoint,
+		"tls": commonconf.GenericMap{
+			"insecure": true,
+		},
+	}
+
+	if isTracingEnabled(dest) {
+		tracesPipelineName := "traces/generic-" + dest.Name
+		currentConfig.Service.Pipelines[tracesPipelineName] = commonconf.Pipeline{
+			Exporters: []string{genericOtlpExporterName},
+		}
+	}
+
+	if isMetricsEnabled(dest) {
+		metricsPipelineName := "metrics/generic-" + dest.Name
+		currentConfig.Service.Pipelines[metricsPipelineName] = commonconf.Pipeline{
+			Exporters: []string{genericOtlpExporterName},
+		}
+	}
+
+	if isLoggingEnabled(dest) {
+		logsPipelineName := "logs/generic-" + dest.Name
+		currentConfig.Service.Pipelines[logsPipelineName] = commonconf.Pipeline{
+			Exporters: []string{genericOtlpExporterName},
 		}
 	}
 }
