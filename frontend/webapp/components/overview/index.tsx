38fc07d13f70c2b05a1d9dcac421956e2f4f86d3
alonkeyval <alon@keyval.dev>
2024-03-10 11:12:12 +0200

    chore: remove unused files

--- frontend/webapp/components/overview/index.tsx
+++ frontend/webapp/components/overview/index.tsx
@@ -1,6 +1,5 @@
 export { OverviewHeader } from './overview.header/overview.header';
 export { ManageDestination } from './destination/manage.destination/manage.destination';
-export { DestinationsManagedList } from './destination/destination.list/destinations.managed.list';
 export { DeleteSource } from './sources/delete.source/delete.source';
 
 export * from './add.item.menu';
