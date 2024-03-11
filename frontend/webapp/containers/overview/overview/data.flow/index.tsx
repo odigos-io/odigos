2d75494721d5896d087315d98939b348a2fa5b09
alonkeyval <alon@keyval.dev>
2024-03-11 17:04:06 +0200

    chore: update overview component

--- frontend/webapp/containers/overview/overview/data.flow/index.tsx
+++ frontend/webapp/containers/overview/overview/data.flow/index.tsx
@@ -1,90 +1,60 @@
 'use client';
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { OverviewDataFlowWrapper } from './styled';
 import { KeyvalDataFlow, KeyvalLoader } from '@/design.system';
 import { useActions, useDestinations, useSources } from '@/hooks';
-import { getEdges, groupSourcesNamespace, getNodes } from './utils';
+import { buildFlowNodesAndEdges } from '@keyval-dev/design-system';
 
-const NAMESPACE_NODE_HEIGHT = 84;
-const NAMESPACE_NODE_POSITION = 0;
-const DESTINATION_NODE_HEIGHT = 136;
-const DESTINATION_NODE_POSITION = 800;
+interface FlowNode {
+  id: string;
+  type: string;
+  position: {
+    x: number;
+    y: number;
+  };
+  data: any;
+}
+interface FlowEdge {
+  id: string;
+  source: string;
+  target: string;
+  animated: boolean;
+  label?: string;
+  style?: Record<string, any>;
+  data?: any;
+}
 
 export function DataFlowContainer() {
-  const [containerHeight, setContainerHeight] = useState(0);
-
-  const containerRef = useCallback((node: HTMLDivElement) => {
-    if (node !== null) {
-      setContainerHeight(node.getBoundingClientRect().height);
-    }
-  }, []);
+  const [nodes, setNodes] = useState<FlowNode[]>();
+  const [edges, setEdges] = useState<FlowEdge[]>();
 
   const { sources } = useSources();
   const { destinationList } = useDestinations();
   const { actions } = useActions();
 
   useEffect(() => {
-    console.log({ sources });
-    console.log({ destinationList });
-    console.log({ actions });
-  }, [actions, destinationList, sources]);
-  const sourcesNodes = useMemo(() => {
-    const groupedSources = groupSourcesNamespace(sources);
+    if (!sources || !destinationList || !actions) return;
 
-    const nodes = getNodes(
-      containerHeight,
-      groupedSources.length > 1 ? groupSourcesNamespace(sources) : sources,
-      'namespace',
-      NAMESPACE_NODE_HEIGHT,
-      NAMESPACE_NODE_POSITION,
-      true
+    const filteredActions = actions.filter(
+      (action) => action.spec.disabled !== true
     );
 
-    return nodes;
-  }, [sources, containerHeight]);
+    const { nodes, edges } = buildFlowNodesAndEdges(
+      sources,
+      destinationList,
+      filteredActions
+    );
+    setNodes(nodes);
+    setEdges(edges);
+  }, [actions, destinationList, sources]);
 
-  const destinationsNodes = useMemo(
-    () =>
-      getNodes(
-        containerHeight,
-        destinationList,
-        'destination',
-        destinationList?.length > 1
-          ? DESTINATION_NODE_HEIGHT
-          : NAMESPACE_NODE_HEIGHT,
-        DESTINATION_NODE_POSITION
-      ),
-    [destinationList, containerHeight]
-  );
-
-  const actionsNodes = useMemo(
-    () =>
-      getNodes(
-        containerHeight,
-        actions,
-        'namespace',
-        NAMESPACE_NODE_HEIGHT,
-        DESTINATION_NODE_POSITION + 200
-      ),
-    [actions, containerHeight]
-  );
-
-  const edges = useMemo(() => {
-    if (!destinationsNodes || !sourcesNodes) return [];
-
-    return getEdges(destinationsNodes, sourcesNodes);
-  }, [destinationsNodes, sourcesNodes]);
-
-  if (!destinationsNodes || !sourcesNodes) {
+  if (!nodes || !edges) {
     return <KeyvalLoader />;
   }
 
   return (
-    <OverviewDataFlowWrapper ref={containerRef}>
-      <KeyvalDataFlow
-        nodes={[...destinationsNodes, ...sourcesNodes]}
-        edges={edges}
-      />
+    <OverviewDataFlowWrapper>
+      <KeyvalDataFlow nodes={nodes} edges={edges} />
     </OverviewDataFlowWrapper>
   );
 }
