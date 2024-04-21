bc0d01e63c573daaf298d08aec1bf5383b26b17b
esara <esara@causely.io>
2024-04-20 22:44:56 -0400

    install odigos in openshift

--- odiglet/pkg/instrumentation/fs/agents.go
+++ odiglet/pkg/instrumentation/fs/agents.go
@@ -2,14 +2,20 @@ package fs
 
 import (
 	"os"
+	"os/exec"
 	"path/filepath"
+	"syscall"
 
+	"github.com/odigos-io/odigos/odiglet/pkg/log"
 	cp "github.com/otiai10/copy"
 )
 
 const (
-	containerDir = "/instrumentations"
-	hostDir      = "/var/odigos"
+	containerDir   = "/instrumentations"
+	hostDir        = "/var/odigos"
+	chrootDir      = "/host"
+	semanagePath   = "/sbin/semanage"
+	restoreconPath = "/sbin/restorecon"
 )
 
 func CopyAgentsDirectoryToHost() error {
@@ -34,5 +40,30 @@ func CopyAgentsDirectoryToHost() error {
 	if err != nil {
 		return err
 	}
+
+	// Check if the semanage command exists when running on RHEL/CoreOS
+	_, err = exec.LookPath(filepath.Join(chrootDir, semanagePath))
+	if err == nil {
+		// Run the semanage command to add the new directory to the container_ro_file_t context
+		cmd := exec.Command(semanagePath, "fcontext", "-a", "-t", "container_ro_file_t", "/var/odigos(/.*)?")
+		syscall.Chroot(chrootDir)
+		err = cmd.Run()
+		if err != nil {
+			log.Logger.Error(err, "Error running semanage command")
+		}
+	}
+
+	// Check if the restorecon command exists when running on RHEL/CoreOS
+	_, err = exec.LookPath(filepath.Join(chrootDir, restoreconPath))
+	if err != nil {
+		// Run the restorecon command to apply the new context
+		cmd := exec.Command(restoreconPath, "-r", "/var/odigos")
+		syscall.Chroot(chrootDir)
+		err = cmd.Run()
+		if err != nil {
+			log.Logger.Error(err, "Error running restorecon command")
+		}
+	}
+
 	return nil
 }
