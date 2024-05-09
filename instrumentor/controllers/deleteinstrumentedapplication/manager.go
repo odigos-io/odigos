eb343a4f20495f7d61988f684cbb082b0d99e0a1
Amir Blum <amirgiraffe@gmail.com>
2024-05-09 16:03:46 +0300

    fix: race condition in instrumented application creation and deletion (#1179)

--- instrumentor/controllers/deleteinstrumentedapplication/manager.go
+++ instrumentor/controllers/deleteinstrumentedapplication/manager.go
@@ -1,6 +1,7 @@
 package deleteinstrumentedapplication
 
 import (
+	odigosv1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -51,6 +52,17 @@ func SetupWithManager(mgr ctrl.Manager) error {
 		For(&corev1.Namespace{}).
 		WithEventFilter(predicate.LabelChangedPredicate{}).
 		Complete(&NamespaceReconciler{
+			Client: mgr.GetClient(),
+			Scheme: mgr.GetScheme(),
+		})
+	if err != nil {
+		return err
+	}
+
+	err = builder.
+		ControllerManagedBy(mgr).
+		For(&odigosv1.InstrumentedApplication{}).
+		Complete(&InstrumentedApplicationReconciler{
 			Client: mgr.GetClient(),
 			Scheme: mgr.GetScheme(),
 		})
