764cd1697128683c5bf6e6af95ace9252eddc2c0
Rauno Viskus <rauno56@gmail.com>
2024-05-13 20:19:43 +0300

    fix: remove unused imports in processors.go

--- autoscaler/controllers/common/processors.go
+++ autoscaler/controllers/common/processors.go
@@ -1,17 +1,10 @@
 package common
 
 import (
-	"fmt"
 	"sort"
 
-	"encoding/json"
-
 	odigosv1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
-	"github.com/odigos-io/odigos/common"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 )
-
-
 
 func FilterAndSortProcessorsByOrderHint(processors *odigosv1.ProcessorList, collectorRole odigosv1.CollectorsGroupRole) []*odigosv1.Processor {
 	filteredProcessors := []*odigosv1.Processor{}
