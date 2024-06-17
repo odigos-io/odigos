08591549652aceba553c59b4dd7d7bbd842a2c1c
Rauno Viskus <rauno56@gmail.com>
2024-06-17 15:24:02 +0300

    fix: avoid looking for hsperfdata

--- procdiscovery/pkg/inspectors/java/java.go
+++ procdiscovery/pkg/inspectors/java/java.go
@@ -1,8 +1,6 @@
 package java
 
 import (
-	"fmt"
-	"os"
 	"strings"
 
 	"github.com/odigos-io/odigos/common"
@@ -12,34 +10,11 @@ import (
 type JavaInspector struct{}
 
 const processName = "java"
-const hsperfdataDir = "hsperfdata"
 
 func (j *JavaInspector) Inspect(p *process.Details) (common.ProgrammingLanguage, bool) {
 	if strings.Contains(p.ExeName, processName) || strings.Contains(p.CmdLine, processName) {
 		return common.JavaProgrammingLanguage, true
 	}
 
-	if j.searchForHsperfdata(p.ProcessID) {
-		return common.JavaProgrammingLanguage, true
-	}
-
 	return "", false
-}
-
-func (j *JavaInspector) searchForHsperfdata(pid int) bool {
-	tmpDir := fmt.Sprintf("/proc/%d/root/tmp/", pid)
-	files, err := os.ReadDir(tmpDir)
-	if err != nil {
-		return false
-	}
-
-	for _, f := range files {
-		if f.IsDir() {
-			name := f.Name()
-			if strings.Contains(name, hsperfdataDir) {
-				return true
-			}
-		}
-	}
-	return false
 }
