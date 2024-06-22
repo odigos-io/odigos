64f1080cab1439a85e91f7d0e95ae4b49297733b
Amir Blum <amirgiraffe@gmail.com>
2024-06-22 23:28:35 +0300

    chore: remove non-opamp version

--- agent-nodejs/src/autoinstrumentation.ts
+++ agent-nodejs/src/autoinstrumentation.ts
@@ -7,70 +7,70 @@ import {
   TELEMETRYSDKLANGUAGEVALUES_NODEJS,
   SEMRESATTRS_PROCESS_PID,
 } from "@opentelemetry/semantic-conventions";
-import { Resource, envDetectorSync, hostDetectorSync, processDetectorSync } from "@opentelemetry/resources";
+import {
+  Resource,
+  envDetectorSync,
+  hostDetectorSync,
+  processDetectorSync,
+} from "@opentelemetry/resources";
 import { DiagConsoleLogger, DiagLogLevel, diag } from "@opentelemetry/api";
 import { VERSION } from "./version";
 
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+// For development, uncomment the following line to see debug logs
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
 // not yet published in '@opentelemetry/semantic-conventions'
-const SEMRESATTRS_TELEMETRY_DISTRO_NAME = 'telemetry.distro.name';
-const SEMRESATTRS_TELEMETRY_DISTRO_VERSION = 'telemetry.distro.version';
+const SEMRESATTRS_TELEMETRY_DISTRO_NAME = "telemetry.distro.name";
+const SEMRESATTRS_TELEMETRY_DISTRO_VERSION = "telemetry.distro.version";
 
 const opampServerHost = process.env.ODIGOS_OPAMP_SERVER_HOST;
 const instrumentationDeviceId = process.env.ODIGOS_INSTRUMENTATION_DEVICE_ID;
 
-if (opampServerHost && instrumentationDeviceId) {
-  const opampClient = new OpAMPClientHttp({
-    instrumentationDeviceId: instrumentationDeviceId,
-    opAMPServerHost: opampServerHost,
-    agentDescriptionIdentifyingAttributes: {
-      [SEMRESATTRS_TELEMETRY_SDK_LANGUAGE]: TELEMETRYSDKLANGUAGEVALUES_NODEJS,
-      [SEMRESATTRS_TELEMETRY_DISTRO_VERSION]: VERSION,
-      [SEMRESATTRS_PROCESS_PID]: process.pid,
-    },
-    agentDescriptionNonIdentifyingAttributes: {},
-  });
-
-  opampClient.start();
-
-  const sdk = new NodeSDK({
-    resourceDetectors: [
-      envDetectorSync, // env detector reads resource attributes from the environment
-      processDetectorSync, // info about executable, runtime, command, etc
-      hostDetectorSync, // host name, arch, machine id, etc
-      opampClient // attributes from OpAMP server, regarding k8s, service name, etc
-    ],
-    // record additional data about the odigos distro
-    resource: new Resource({
-      [SEMRESATTRS_TELEMETRY_DISTRO_NAME]: 'odigos',
-      [SEMRESATTRS_TELEMETRY_DISTRO_VERSION]: VERSION,
-    }),
-    instrumentations: [getNodeAutoInstrumentations()],
-    traceExporter: new OTLPTraceExporter(),
-  });
-  sdk.start();
-
-  const shutdown = async () => {
-    try {
-      diag.info('Shutting down OpenTelemetry SDK and OpAMP client');
-      await Promise.all([sdk.shutdown(), opampClient.shutdown()]);
-    } catch(err) {
-      diag.error('Error shutting down OpenTelemetry SDK and OpAMP client', err);
-    }
-  }
-
-  process.on('SIGTERM', shutdown);  
-  process.on('SIGINT', shutdown);
-  process.on('exit', shutdown);
-
-} else {
-  const sdk = new NodeSDK({
-    autoDetectResources: true,
-    instrumentations: [getNodeAutoInstrumentations()],
-    traceExporter: new OTLPTraceExporter(),
-  });  
-  sdk.start();
+if (!opampServerHost || !instrumentationDeviceId) {
+  diag.error(
+    "Missing required environment variables ODIGOS_OPAMP_SERVER_HOST and ODIGOS_INSTRUMENTATION_DEVICE_ID"
+  );
 }
 
+const opampClient = new OpAMPClientHttp({
+  instrumentationDeviceId: instrumentationDeviceId!,
+  opAMPServerHost: opampServerHost!,
+  agentDescriptionIdentifyingAttributes: {
+    [SEMRESATTRS_TELEMETRY_SDK_LANGUAGE]: TELEMETRYSDKLANGUAGEVALUES_NODEJS,
+    [SEMRESATTRS_TELEMETRY_DISTRO_VERSION]: VERSION,
+    [SEMRESATTRS_PROCESS_PID]: process.pid,
+  },
+  agentDescriptionNonIdentifyingAttributes: {},
+});
 
+opampClient.start();
+
+const sdk = new NodeSDK({
+  resourceDetectors: [
+    envDetectorSync, // env detector reads resource attributes from the environment
+    processDetectorSync, // info about executable, runtime, command, etc
+    hostDetectorSync, // host name, arch, machine id, etc
+    opampClient, // attributes from OpAMP server, regarding k8s, service name, etc
+  ],
+  // record additional data about the odigos distro
+  resource: new Resource({
+    [SEMRESATTRS_TELEMETRY_DISTRO_NAME]: "odigos",
+    [SEMRESATTRS_TELEMETRY_DISTRO_VERSION]: VERSION,
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+  traceExporter: new OTLPTraceExporter(),
+});
+sdk.start();
+
+const shutdown = async () => {
+  try {
+    diag.info("Shutting down OpenTelemetry SDK and OpAMP client");
+    await Promise.all([sdk.shutdown(), opampClient.shutdown()]);
+  } catch (err) {
+    diag.error("Error shutting down OpenTelemetry SDK and OpAMP client", err);
+  }
+};
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
+process.on("exit", shutdown);
