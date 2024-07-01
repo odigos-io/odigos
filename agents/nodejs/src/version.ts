be4f2cbd7478f23430fe618a03493210ea42d03f
Amir Blum <amirgiraffe@gmail.com>
2024-07-01 17:21:39 +0300

    feat: opamp  implementation for nodejs agent (#1287)

---
+++ agents/nodejs/src/version.ts
@@ -0,0 +1,3 @@
+
+// the content of the file is replaced with the real version in the odiglet DOCKERFILE
+export const VERSION = "development";
---
+++ opampserver/pkg/server/consts.go
@@ -0,0 +1,5 @@
+package server
+
+const (
+	OpAmpServerDefaultPort = 4320
+)
