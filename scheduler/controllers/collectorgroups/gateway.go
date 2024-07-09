71996badd99d021334233da8cc382ba88110befe
Ron Federman <73110295+RonFed@users.noreply.github.com>
2024-07-09 16:11:25 +0300

    Add owner reference to destination over secret. Add secret reconciler to autoscaler. (#1340)

--- scheduler/controllers/collectorgroups/gateway.go
+++ scheduler/controllers/collectorgroups/gateway.go
@@ -2,17 +2,14 @@ package collectorgroups
 
 import (
 	odigosv1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
+	"github.com/odigos-io/odigos/k8sutils/pkg/consts"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
-const (
-	gatewayName = "odigos-gateway"
 )
 
 func NewGateway(namespace string) *odigosv1.CollectorsGroup {
 	return &odigosv1.CollectorsGroup{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      gatewayName,
+			Name:      consts.OdigosClusterCollectorDeploymentName,
 			Namespace: namespace,
 		},
 		Spec: odigosv1.CollectorsGroupSpec{
