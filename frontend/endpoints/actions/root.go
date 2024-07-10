f64bde55134a2d6a390f591012e6cc60b64f018c
Tamir David <tamirdavid@Tamirs-MacBook-Pro.local>
2024-07-10 14:49:02 +0300

    fix: adding latencysampler routes to the frontend

--- frontend/endpoints/actions/root.go
+++ frontend/endpoints/actions/root.go
@@ -64,5 +64,21 @@ func GetActions(c *gin.Context, odigosns string) {
 		})
 	}
 
+	lsActions, err := kube.DefaultClient.ActionsClient.LatencySamplers(odigosns).List(c, metav1.ListOptions{})
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	for _, action := range lsActions.Items {
+		response = append(response, IcaInstanceResponse{
+			Id:   action.Name,
+			Type: action.Kind,
+			Spec: action.Spec,
+		})
+	}
+
 	c.JSON(200, response)
 }
--- frontend/main.go
+++ frontend/main.go
@@ -137,6 +137,12 @@ func startHTTPServer(flags *Flags) (*gin.Engine, error) {
 		apis.POST("/actions/types/RenameAttribute", func(c *gin.Context) { actions.CreateRenameAttribute(c, flags.Namespace) })
 		apis.PUT("/actions/types/RenameAttribute/:id", func(c *gin.Context) { actions.UpdateRenameAttribute(c, flags.Namespace, c.Param("id")) })
 		apis.DELETE("/actions/types/RenameAttribute/:id", func(c *gin.Context) { actions.DeleteRenameAttribute(c, flags.Namespace, c.Param("id")) })
+
+		// LatencySampler
+		apis.GET("/actions/types/LatencySampler/:id", func(c *gin.Context) { actions.GetLatencySampler(c, flags.Namespace, c.Param("id")) })
+		apis.POST("/actions/types/LatencySampler", func(c *gin.Context) { actions.CreateLatencySampler(c, flags.Namespace) })
+		apis.PUT("/actions/types/LatencySampler/:id", func(c *gin.Context) { actions.UpdateLatencySampler(c, flags.Namespace, c.Param("id")) })
+		apis.DELETE("/actions/types/LatencySampler/:id", func(c *gin.Context) { actions.DeleteLatencySampler(c, flags.Namespace, c.Param("id")) })
 	}
 
 	return r, nil
