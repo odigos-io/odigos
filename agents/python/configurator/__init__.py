0beff5f03236cce5292e63ae88d7a9d95829f314
tamirdavid1 <tamir@odigos.io>
2024-07-15 13:18:21 +0300

    Python agent opamp (#1346)
    Co-authored-by: Tamir David <tamirdavid@Tamirs-MacBook-Pro.local>

--- agents/python/configurator/__init__.py
+++ agents/python/configurator/__init__.py
@@ -1,16 +1,18 @@
 # my_otel_configurator/__init__.py
 import opentelemetry.sdk._configuration as sdk_config
+import threading
+import atexit
 import os
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.resources import ProcessResourceDetector, OTELResourceDetector
 from .version import VERSION
-
+from opamp.http_client import OpAMPHTTPClient
 
 class OdigosPythonConfigurator(sdk_config._BaseConfigurator):
     def _configure(self, **kwargs):
-        _initialize_components(kwargs.get("auto_instrumentation_version"))
+        _initialize_components()
 
-def _initialize_components(auto_instrumentation_version):
+def _initialize_components():
     trace_exporters, metric_exporters, log_exporters = sdk_config._import_exporters(
         sdk_config._get_exporter_names("traces"),
         sdk_config._get_exporter_names("metrics"),
@@ -21,14 +23,24 @@ def _initialize_components(auto_instrumentation_version):
         "telemetry.distro.name": "odigos",
         "telemetry.distro.version": VERSION,
     }
+    
+    resource_attributes_event = threading.Event()
+    client = start_opamp_client(resource_attributes_event)
+    resource_attributes_event.wait(timeout=30)  # Wait for the resource attributes to be received for 30 seconds
 
-    resource = Resource.create(auto_resource) \
-        .merge(OTELResourceDetector().detect()) \
-        .merge(ProcessResourceDetector().detect())
+    received_value = client.resource_attributes
+    
+    if received_value:
+        auto_resource.update(received_value)
 
-    initialize_traces_if_enabled(trace_exporters, resource)
-    initialize_metrics_if_enabled(metric_exporters, resource)
-    initialize_logging_if_enabled(log_exporters, resource)
+        resource = Resource.create(auto_resource) \
+            .merge(OTELResourceDetector().detect()) \
+            .merge(ProcessResourceDetector().detect())
+
+        initialize_traces_if_enabled(trace_exporters, resource)
+        initialize_metrics_if_enabled(metric_exporters, resource)
+        initialize_logging_if_enabled(log_exporters, resource)
+
 
 def initialize_traces_if_enabled(trace_exporters, resource):
     traces_enabled = os.getenv(sdk_config.OTEL_TRACES_EXPORTER, "none").strip().lower()
@@ -40,9 +52,23 @@ def initialize_traces_if_enabled(trace_exporters, resource):
 def initialize_metrics_if_enabled(metric_exporters, resource):
     metrics_enabled = os.getenv(sdk_config.OTEL_METRICS_EXPORTER, "none").strip().lower()
     if metrics_enabled != "none":
-        sdk_config._init_metrics(metric_exporters,resource)
+        sdk_config._init_metrics(metric_exporters, resource)
 
 def initialize_logging_if_enabled(log_exporters, resource):
     logging_enabled = os.getenv(sdk_config.OTEL_LOGS_EXPORTER, "none").strip().lower()
     if logging_enabled != "none":
         sdk_config._init_logging(log_exporters, resource)
+
+
+def start_opamp_client(event):
+    condition = threading.Condition(threading.Lock())
+    client = OpAMPHTTPClient(event, condition)
+    client.start()
+    
+    def shutdown():
+        client.shutdown()
+
+    # Ensure that the shutdown function is called on program exit
+    atexit.register(shutdown)
+
+    return client
