a36cb154b0df690dc4802066e45976c71cff0970
Eden Federman <eden@keyval.dev>
2024-07-21 16:20:34 +0300

    tempo distributed

--- tests/common/wait_for_dest.sh
+++ tests/common/wait_for_dest.sh
@@ -6,8 +6,8 @@ set -e
 # function to verify tempo is ready
 function wait_for_ready() {
   local dest_namespace="traces"
-  local dest_service="e2e-tests-tempo"
-  local dest_port="tempo-prom-metrics"
+  local dest_service="e2e-tests-tempo-query-frontend"
+  local dest_port="http-metrics"
   local response=$(kubectl get --raw /api/v1/namespaces/$dest_namespace/services/$dest_service:$dest_port/proxy/ready)
   if [ "$response" != "ready" ]; then
     echo "Tempo is not ready yet. Retrying in 2 seconds..."
