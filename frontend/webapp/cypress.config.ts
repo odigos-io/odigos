bbaf0f4f5ae8c70193bd644cc68b2bbea98404e2
yodigos <yoav@odigos.io>
2024-07-30 14:02:53 +0300

    Remove Cypress Key

--- frontend/webapp/cypress.config.ts
+++ frontend/webapp/cypress.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  projectId: process.env.CYPRESS_PROJECT_ID,
-  env: {
-    CYPRESS_RECORD_KEY: process.env.CYPRESS_RECORD_KEY,
-  },
+  projectId: 'aydhz4',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
