2493b9a4c74a908f66c2bd97fc1c8517f3c46d7e
yodigos <yoav@odigos.io>
2024-08-04 11:18:01 +0300

    feat:Elasticsearch destination (#1412)

--- frontend/endpoints/destination_recognition/destination_finder.go
+++ frontend/endpoints/destination_recognition/destination_finder.go
@@ -2,16 +2,14 @@ package destination_recognition
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/odigos-io/odigos/common"
+	"github.com/odigos-io/odigos/frontend/kube"
+	"github.com/odigos-io/odigos/k8sutils/pkg/client"
 	k8s "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-type DestinationType string
-
-const (
-	JaegerDestinationType DestinationType = "jaeger"
-)
-
-var SupportedDestinationType = []DestinationType{JaegerDestinationType}
+var SupportedDestinationType = []common.DestinationType{common.JaegerDestinationType, common.ElasticsearchDestinationType}
 
 type DestinationDetails struct {
 	Name      string `json:"name"`
@@ -36,31 +34,42 @@ func (d *DestinationFinder) fetchDestinationDetails(service k8s.Service) Destina
 }
 
 func GetAllPotentialDestinationDetails(ctx *gin.Context, namespaces []k8s.Namespace) ([]DestinationDetails, error) {
-	helmManagedServices, err := getAllHelmManagedServices(ctx, namespaces)
-	if err != nil {
-		return nil, err
-	}
-
 	var destinationFinder *DestinationFinder
 	var destinationDetails []DestinationDetails
-	for _, service := range helmManagedServices {
-		for _, destinationType := range SupportedDestinationType {
-			destinationFinder = getDestinationFinder(destinationType)
-			if destinationFinder.isPotentialService(service) {
-				destinationDetails = append(destinationDetails, destinationFinder.fetchDestinationDetails(service))
-				break
-			}
-		}
+	var err error
+
+	for _, ns := range namespaces {
+		err = client.ListWithPages(client.DefaultPageSize, kube.DefaultClient.CoreV1().Services(ns.Name).List,
+			ctx, metav1.ListOptions{}, func(services *k8s.ServiceList) error {
+				for _, service := range services.Items {
+					for _, destinationType := range SupportedDestinationType {
+						destinationFinder = getDestinationFinder(destinationType)
+						if destinationFinder.isPotentialService(service) {
+							destinationDetails = append(destinationDetails, destinationFinder.fetchDestinationDetails(service))
+							break
+						}
+					}
+				}
+				return nil
+			})
+	}
+
+	if err != nil {
+		return nil, err
 	}
 
 	return destinationDetails, nil
 }
 
-func getDestinationFinder(destinationType DestinationType) *DestinationFinder {
+func getDestinationFinder(destinationType common.DestinationType) *DestinationFinder {
 	switch destinationType {
-	case JaegerDestinationType:
+	case common.JaegerDestinationType:
 		return &DestinationFinder{
 			destinationFinder: &JaegerDestinationFinder{},
+		}
+	case common.ElasticsearchDestinationType:
+		return &DestinationFinder{
+			destinationFinder: &ElasticSearchDestinationFinder{},
 		}
 	}
 
