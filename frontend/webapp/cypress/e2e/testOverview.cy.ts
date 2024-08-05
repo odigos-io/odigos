a2953f63721f41ede620c19cff77a19f5333e68f
yodigos <yoav@odigos.io>
2024-08-05 14:19:49 +0300

    Check Tests run e2e with elements

--- frontend/webapp/cypress/e2e/testOverview.cy.ts
+++ frontend/webapp/cypress/e2e/testOverview.cy.ts
@@ -1,11 +1,18 @@
-describe('Basic UI Tests', () => {
-    it('Main page loads', () => {
+describe('Overview Page Tests', () => {
+    it('Overview page loads', () => {
         cy.visit('localhost:3000')
+        cy.url().should('eq', 'http://localhost:3000/overview');
+    })
+
+    it('Test Sources exists correctly', () => {
         cy.get('[data-id="namespace-0"]').should('have.text', 'defaultcoupon')
         cy.get('[data-id="namespace-1"]').should('have.text', 'defaultfrontend')
         cy.get('[data-id="namespace-2"]').should('have.text', 'defaultinventory')
         cy.get('[data-id="namespace-3"]').should('have.text', 'defaultmembership')
         cy.get('[data-id="namespace-4"]').should('have.text', 'defaultpricing')
+    })
+
+    it('Check Destination exist correctly', () => {
         cy.get('[data-id="destination-0"]').should('have.text', 'e2e-testsTempo')
     })
 
