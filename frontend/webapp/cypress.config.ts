0f80c84faefd58373912e50f402b0d5e3712076e
yodigos <yoav@odigos.io>
2024-08-06 09:34:32 +0300

    feat: FE e2e tests (#1423)

--- frontend/webapp/cypress.config.ts
+++ frontend/webapp/cypress.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'cypress';
+import {defineConfig} from 'cypress';
 
 export default defineConfig({
-  projectId: 'aydhz4',
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+        },
     },
-  },
 });
