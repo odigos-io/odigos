693e80567916e5ee659b702b390b898290820afe
yodigos <yoav@odigos.io>
2024-08-08 18:18:28 +0300

    Added Version

--- procdiscovery/pkg/inspectors/detailsEnvs.go
+++
@@ -1,8 +0,0 @@
-package inspectors
-
-const NodeVersionConst = "NODE_VERSION"
-const PythonVersionConst = "PYTHON_VERSION"
-const JavaVersionConst = "JAVA_VERSION"
-
-// EnvDetailsSeparatorMap is a map of environment variables and their separators
-var EnvDetailsSeparatorMap = []string{NodeVersionConst, PythonVersionConst, JavaVersionConst}
