8cae134146226be62bc6436f93809f463c4bcd26
yodigos <yoav@odigos.io>
2024-08-08 15:09:35 +0300

    feat: FE test action (#1430)

--- tests/common/ui-tests/run_cypress_tests.sh
+++ tests/common/ui-tests/run_cypress_tests.sh
@@ -8,6 +8,7 @@ run_cypress_test() {
   if [ $status -ne 0 ]; then
     echo "Cypress tests failed"
     # Stop the background process
+    cd ../../tests/e2e/fe-synthetic || exit
     kill "$(cat odigos-ui.pid)"
     rm odigos-ui.pid
     rm ../../odigos-ui.log
@@ -23,5 +24,9 @@ if [ "$1" = "include-onboarding-flow" ]; then
 fi
 
 run_cypress_test "cypress/e2e/test-overview.cy.ts"
+
+if [ "$2" = "action-addition" ]; then
+  run_cypress_test "cypress/e2e/action-testing.cy.ts"
+fi
 
 echo "Cypress tests passed"
