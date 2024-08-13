50d70b626e29af9ad7baab0c6f452f40972e431e
Tamir David <tamirdavid@Tamirs-MacBook-Pro.local>
2024-08-13 16:05:55 +0300

    fix: remove only protected c files and their directories

--- odiglet/pkg/instrumentation/fs/remove.go
+++ odiglet/pkg/instrumentation/fs/remove.go
@@ -4,43 +4,59 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/odigos-io/odigos/odiglet/pkg/log"
 )
 
-func removeFilesInDir(hostDir string) error {
+func removeFilesInDir(hostDir string, filesToKeep map[string]struct{}) error {
 	shouldRecreateCFiles := ShouldRecreateAllCFiles()
-	log.Logger.V(0).Info(fmt.Sprintf("Removing files in directory: %s, shouldRecreateCFiles: %s", hostDir, fmt.Sprintf("%t", shouldRecreateCFiles)))
+	log.Logger.V(0).Info(fmt.Sprintf("Removing files in directory: %s, shouldRecreateCFiles: %t", hostDir, shouldRecreateCFiles))
+
+	// Mark directories as protected if they contain a file that needs to be preserved.
+	// If C files should be recreated, skip marking any directories as protected.
+	protectedDirs := make(map[string]bool)
+	if !shouldRecreateCFiles {
+		for file := range filesToKeep {
+			dir := filepath.Dir(file)
+			for dir != hostDir {
+				protectedDirs[dir] = true
+				dir = filepath.Dir(dir)
+			}
+			protectedDirs[hostDir] = true // Protect the main directory
+		}
+	}
 
 	return filepath.Walk(hostDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		// Skip the root directory itself]
 		if path == hostDir {
 			return nil
 		}
 
-		if info.IsDir() {
-			return nil
-		}
-
-		if !shouldRecreateCFiles {
-			// filter out C files in ebpf directories
-			if strings.Contains(filepath.Dir(path), "ebpf") {
-				switch ext := filepath.Ext(info.Name()); ext {
-				case ".so", ".node", "node.d", ".a":
-					return nil
-				}
+		// Skip removing any files listed in filesToKeepMap
+		if !info.IsDir() {
+			if _, found := filesToKeep[path]; found {
+				log.Logger.V(0).Info(fmt.Sprintf("Skipping protected file: %s", path))
+				return nil
 			}
 		}
 
-		// Remove the file
-		err = os.Remove(path)
-		if err != nil {
-			return err
+		// Skip removing protected directories
+		if info.IsDir() && protectedDirs[path] {
+			log.Logger.V(0).Info(fmt.Sprintf("Skipping protected directory: %s", path))
+			return nil
+		}
+
+		// Remove removing unprotected files and directories
+		if err := os.RemoveAll(path); err != nil {
+			return fmt.Errorf("error removing %s: %w", path, err)
+		}
+
+		// Skip further processing in this directory since it has been removed
+		if info.IsDir() {
+			return filepath.SkipDir
 		}
 
 		return nil
