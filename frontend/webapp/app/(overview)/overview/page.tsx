f8f90f830044e46c1e1d60265cdbfa080885bd66
alonkeyval <alon@keyval.dev>
2024-08-15 17:34:56 +0300

    chore: wip

--- frontend/webapp/app/(overview)/overview/page.tsx
+++ frontend/webapp/app/(overview)/overview/page.tsx
@@ -1,23 +1,10 @@
 'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { OVERVIEW } from '@/utils';
 import { OverviewHeader } from '@/components';
 import { DataFlowContainer } from '@/containers';
-import { useQuery } from '@apollo/client';
-import { GET_COMPUTE_PLATFORM } from '@/graphql';
 
 export default function OverviewPage() {
-  const { data, loading, error } = useQuery(GET_COMPUTE_PLATFORM);
-
-  useEffect(() => {
-    if (error) {
-      console.error(error);
-    }
-    if (data) {
-      console.log(data);
-    }
-  }, [error, data]);
-
   return (
     <>
       <OverviewHeader title={OVERVIEW.MENU.OVERVIEW} />
