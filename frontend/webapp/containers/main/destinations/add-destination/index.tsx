99937735da1681552899487bb2a4ffdbc865074f
alonkeyval <alon@keyval.dev>
2024-08-18 16:55:43 +0300

    chore: notification note component

--- frontend/webapp/containers/main/destinations/add-destination/index.tsx
+++ frontend/webapp/containers/main/destinations/add-destination/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useRouter } from 'next/navigation';
-import { SectionTitle } from '@/reuseable-components';
+import { NotificationNote, SectionTitle } from '@/reuseable-components';
 import { AddDestinationModal } from './add-destination-modal';
 import { AddDestinationButton, SetupHeader } from '@/components';
 import { ConfiguredDestinationsList } from './configured-destinations-list';
+import { useSelector } from 'react-redux';
 
 const AddDestinationButtonWrapper = styled.div`
   width: 100%;
@@ -20,10 +21,32 @@ const HeaderWrapper = styled.div`
   width: 100vw;
 `;
 
+const NotificationNoteWrapper = styled.div`
+  margin-top: 24px;
+`;
+
 export function ChooseDestinationContainer() {
   const [isModalOpen, setModalOpen] = useState(false);
 
   const router = useRouter();
+
+  const sourcesList = useSelector(({ app }) => app.sources);
+  console.log({ sourcesList });
+
+  const isSourcesListEmpty = () => {
+    const sourceLen = Object.keys(sourcesList).length === 0;
+    if (sourceLen) {
+      return true;
+    }
+    let empty = true;
+    for (const source in sourcesList) {
+      if (sourcesList[source].length > 0) {
+        empty = false;
+        break;
+      }
+    }
+    return empty;
+  };
 
   const handleOpenModal = () => setModalOpen(true);
   const handleCloseModal = () => setModalOpen(false);
@@ -52,6 +75,11 @@ export function ChooseDestinationContainer() {
           title="Configure destinations"
           description="Add backend destinations where collected data will be sent and configure their settings."
         />
+        {isSourcesListEmpty() && (
+          <NotificationNoteWrapper>
+            <NotificationNote type={'warning'} text={'No sources selected.'} />
+          </NotificationNoteWrapper>
+        )}
         <AddDestinationButtonWrapper>
           <AddDestinationButton onClick={() => handleOpenModal()} />
         </AddDestinationButtonWrapper>
