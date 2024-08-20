ab11fe326a5ceb020ec02c99abb430649d9add01
Tamir David <tamirdavid@Tamirs-MacBook-Pro.local>
2024-08-20 12:24:26 +0300

    feat: remove files that now consumed from odigos-opentelemetry-python

--- agents/python/opamp/health_status.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class AgentHealthStatus(str, Enum):
-    HEALTHY = "Healthy"
-    STARTING = "Starting"
-    UNSUPPORTED_RUNTIME_VERSION = "UnsupportedRuntimeVersion"
-    TERMINATED = "ProcessTerminated"
-    AGENT_FAILURE = "AgentFailure"
