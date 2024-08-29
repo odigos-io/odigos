bf6b7f3e7807ab073e8af4c2cbef53cb2b6bf984
yodigos <yoav@odigos.io>
2024-08-29 09:49:33 +0300

    PR Fix

--- procdiscovery/pkg/inspectors/golang/golang.go
+++ procdiscovery/pkg/inspectors/golang/golang.go
@@ -9,9 +9,7 @@ import (
 	"regexp"
 )
 
-type GolangInspector struct {
-	*buildinfo.BuildInfo
-}
+type GolangInspector struct{}
 
 const GolangVersionRegex = `go(\d+\.\d+\.\d+)`
 
@@ -19,21 +17,21 @@ var re = regexp.MustCompile(GolangVersionRegex)
 
 func (g *GolangInspector) Inspect(p *process.Details) (common.ProgrammingLanguage, bool) {
 	file := fmt.Sprintf("/proc/%d/exe", p.ProcessID)
-	buildInfo, err := buildinfo.ReadFile(file)
+	_, err := buildinfo.ReadFile(file)
 	if err != nil {
 		return "", false
 	}
-
-	g.BuildInfo = buildInfo
 
 	return common.GoProgrammingLanguage, true
 }
 
 func (g *GolangInspector) GetRuntimeVersion(p *process.Details, containerURL string) *version.Version {
-	if g.BuildInfo == nil {
+	file := fmt.Sprintf("/proc/%d/exe", p.ProcessID)
+	buildInfo, err := buildinfo.ReadFile(file)
+	if err != nil || buildInfo == nil {
 		return nil
 	}
-	match := re.FindStringSubmatch(g.BuildInfo.GoVersion)
+	match := re.FindStringSubmatch(buildInfo.GoVersion)
 
 	return common.GetVersion(match[1])
 
