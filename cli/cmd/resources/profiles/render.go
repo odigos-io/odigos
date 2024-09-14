2642e8dbb3baae6cac4e91e4499754daac341c21
Amir Blum <amirgiraffe@gmail.com>
2024-09-14 16:01:08 +0300

    chore: remove unused functions

--- cli/cmd/resources/profiles/render.go
+++ cli/cmd/resources/profiles/render.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"reflect"
 
-	actions "github.com/odigos-io/odigos/api/actions/v1alpha1"
-	odigosv1alpha1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/yaml"
 )
@@ -35,56 +33,4 @@ func GetEmbeddedYAMLFileAsObjects(filename string, obj client.Object) ([]client.
 
 	// Return the object wrapped in a slice
 	return []client.Object{newObj.(client.Object)}, nil
-}
-
-// GetEmbeddedYAMLFilesAsObjects reads embedded YAML files and converts them into runtime.Object
-func GetEmbeddedYAMLInstrumentationRuleFileAsObjects(filename string) ([]client.Object, error) {
-
-	// Read the embedded YAML file content
-	yamlBytes, err := embeddedFiles.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read embedded file %s: %v", filename, err)
-	}
-
-	var instrumentationRule odigosv1alpha1.InstrumentationRule
-	err = yaml.Unmarshal(yamlBytes, &instrumentationRule)
-	if err != nil {
-		return nil, err
-	}
-
-	return []client.Object{&instrumentationRule}, nil
-}
-
-func GetEmbeddedYAMLRenameAttributeActionFileAsObjects(filename string) ([]client.Object, error) {
-
-	// Read the embedded YAML file content
-	yamlBytes, err := embeddedFiles.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read embedded file %s: %v", filename, err)
-	}
-
-	var action actions.RenameAttribute
-	err = yaml.Unmarshal(yamlBytes, &action)
-	if err != nil {
-		return nil, err
-	}
-
-	return []client.Object{&action}, nil
-}
-
-func GetEmbeddedYAMLProcessorFileAsObjects(filename string) ([]client.Object, error) {
-
-	// Read the embedded YAML file content
-	yamlBytes, err := embeddedFiles.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read embedded file %s: %v", filename, err)
-	}
-
-	var processor odigosv1alpha1.Processor
-	err = yaml.Unmarshal(yamlBytes, &processor)
-	if err != nil {
-		return nil, err
-	}
-
-	return []client.Object{&processor}, nil
 }
