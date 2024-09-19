74fb66f88c98c7e644adae954dc1b844eac66bea
yodigos <yoav@odigos.io>
2024-09-19 14:03:51 +0300

    Fix CR

--- cli/cmd/diagnose.go
+++ cli/cmd/diagnose.go
@@ -13,6 +13,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
@@ -81,41 +82,49 @@ func fetchOdigosComponentsLogs(ctx context.Context, client *kube.Client, logDir 
 		return err
 	}
 
+	var wg sync.WaitGroup
+
 	for _, pod := range pods.Items {
-		if err = fetchPodLogs(ctx, client, odigosNamespace, pod, logDir); err != nil {
-			return err
-		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fetchPodLogs(ctx, client, odigosNamespace, pod, logDir)
+		}()
 	}
+
+	wg.Wait()
 
 	return nil
 }
 
-func fetchPodLogs(ctx context.Context, client *kube.Client, odigosNamespace string, pod v1.Pod, logDir string) error {
+func fetchPodLogs(ctx context.Context, client *kube.Client, odigosNamespace string, pod v1.Pod, logDir string) {
 	for _, container := range pod.Spec.Containers {
-		fmt.Printf("Fetching logs for Pod: %s, Container: %s in Node: %s\n", pod.Name, container.Name, pod.Spec.NodeName)
+		logPrefix := fmt.Sprintf("Fetching logs for Pod: %s, Container: %s, Node: %s", pod.Name, container.Name, pod.Spec.NodeName)
+		fmt.Printf(logPrefix + "\n")
 
 		// Define the log file path for saving compressed logs
 		logFilePath := filepath.Join(logDir, pod.Name+"_"+container.Name+"_"+pod.Spec.NodeName+".log.gz")
 		logFile, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 		if err != nil {
-			return err
+			fmt.Printf(logPrefix+" - Failed - Error creating log file: %v\n", err)
+			return
 		}
 
 		req := client.CoreV1().Pods(odigosNamespace).GetLogs(pod.Name, &v1.PodLogOptions{})
 		logStream, err := req.Stream(ctx)
 		if err != nil {
-			return err
+			fmt.Printf(logPrefix+" - Failed - Error creating log stream: %v\n", err)
+			return
 		}
 
 		if err = saveLogsToGzipFileInBatches(logFile, logStream, logBufferSize); err != nil {
-			return err
+			fmt.Printf(logPrefix+" - Failed - Error saving logs to file: %v\n", err)
+			return
 		}
 
 		logStream.Close()
 		logFile.Close()
 	}
-
-	return nil
 }
 
 func saveLogsToGzipFileInBatches(logFile *os.File, logStream io.ReadCloser, bufferSize int) error {
