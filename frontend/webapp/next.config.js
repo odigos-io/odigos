cbca70c679864c4cb21c38ce685b7368d45afd6c
alonkeyval <alon@keyval.dev>
2024-09-24 15:17:16 +0300

    chore: init pnp

--- frontend/webapp/next.config.js
+++ frontend/webapp/next.config.js
@@ -1,21 +1,33 @@
 /** @type {import('next').NextConfig} */
 
+const PnpWebpackPlugin = require('pnp-webpack-plugin');
+
 module.exports = {
   reactStrictMode: false,
   images: {
     unoptimized: true,
   },
-  output: "export",
+  output: 'export',
   webpack(config) {
+    // Add pnp-webpack-plugin to the resolver
+    config.resolve.plugins = config.resolve.plugins || [];
+    config.resolve.plugins.push(PnpWebpackPlugin);
+
+    // Add pnp-webpack-plugin to the loader resolver
+    config.resolveLoader = config.resolveLoader || {};
+    config.resolveLoader.plugins = config.resolveLoader.plugins || [];
+    config.resolveLoader.plugins.push(PnpWebpackPlugin.moduleLoader(module));
+
+    // SVG handling with SVGR
     config.module.rules.push({
       test: /\.svg$/,
       use: {
-        loader: "@svgr/webpack",
+        loader: '@svgr/webpack',
         options: {
           svgoConfig: {
             plugins: [
               {
-                name: "removeViewBox",
+                name: 'removeViewBox',
                 active: false,
               },
             ],
