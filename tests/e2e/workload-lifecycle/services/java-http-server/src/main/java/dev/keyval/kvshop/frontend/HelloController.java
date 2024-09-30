0f3061d357d33c3863801cfbe2dd604d40ae7169
yodigos <yoav@odigos.io>
2024-09-30 10:36:17 +0300

    Java special versions

--- tests/e2e/workload-lifecycle/services/java-http-server/src/main/java/dev/keyval/kvshop/frontend/HelloController.java
+++ tests/e2e/workload-lifecycle/services/java-http-server/src/main/java/dev/keyval/kvshop/frontend/HelloController.java
@@ -1,18 +1,22 @@
 package dev.keyval.kvshop.frontend;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
     @Autowired
-    public HelloController() {}
+    public HelloController() {
+        // Constructor logic can be added here if necessary
+    }
 
-@CrossOrigin(origins = "*")
-@GetMapping("/")
-public String printHello() {
-   System.out.println("Got Hello");
-   return "Hello";
-}
+    @CrossOrigin(origins = "*")
+    @GetMapping("/")
+    public String printHello() {
+        System.out.println("Received request for Hello");
+        return "Hello";
+    }
 }
