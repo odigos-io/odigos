448007e37ccb55078580e8237611a55c7bd788ae
Tamir David <tamirdavid@Tamirs-MacBook-Pro.local>
2024-09-30 17:58:56 +0300

    feat: adding the demo python app

---
+++ tests/e2e/workload-lifecycle/services/python-http-server/myapp_core/models.py
@@ -0,0 +1,9 @@
+# myapp_core/models.py
+from django.db import models
+
+class ExampleModel(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
