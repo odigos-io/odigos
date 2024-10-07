2f7728a8ea5644aedb2a152b9530fd155981461c
Tamir David <tamirdavid@Tamirs-MacBook-Pro.local>
2024-10-07 16:35:22 +0300

    fix: hard coded otlp http for python

--- agents/python/configurator/__init__.py
+++ agents/python/configurator/__init__.py
@@ -5,11 +5,10 @@ from initializer.components import initialize_components
 MINIMUM_PYTHON_SUPPORTED_VERSION = (3, 8)
 
 class OdigosPythonConfigurator(sdk_config._BaseConfigurator):
-    
     def _configure(self, **kwargs):
         trace_exporters, metric_exporters, log_exporters = sdk_config._import_exporters(
-            sdk_config._get_exporter_names("traces"),
-            sdk_config._get_exporter_names("metrics"),
-            sdk_config._get_exporter_names("logs"),
+            ['otlp_proto_http'] if sdk_config._get_exporter_names("traces") else [],
+            [],
+            [],
         )
         initialize_components(trace_exporters, metric_exporters, log_exporters)
