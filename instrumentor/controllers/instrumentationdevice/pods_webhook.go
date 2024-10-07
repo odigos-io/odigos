ef553699c7c3ac0dd9148058fd0a793a4245a92b
Tamir David <tamirdavid@Tamirs-MacBook-Pro.local>
2024-10-07 13:09:26 +0300

    feat: inject relevant variables to pods using mutation webhook

--- instrumentor/controllers/instrumentationdevice/pods_webhook.go
+++ instrumentor/controllers/instrumentationdevice/pods_webhook.go
@@ -7,9 +7,15 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	corev1 "k8s.io/api/core/v1"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	"k8s.io/apimachinery/pkg/runtime"
+)
+
+const (
+	EnvVarNamespace     = "ODIGOS_CONTAINER_NAMESPACE"
+	EnvVarWorkloadKind  = "ODIGOS_WORKLOAD_KIND"
+	EnvVarContainerName = "ODIGOS_CONTAINER_NAME"
+	EnvVarPodName       = "ODIGOS_POD_NAME"
 )
 
 type PodsWebhook struct{}
@@ -17,8 +23,6 @@ type PodsWebhook struct{}
 var _ webhook.CustomDefaulter = &PodsWebhook{}
 
 func (p *PodsWebhook) Default(ctx context.Context, obj runtime.Object) error {
-	// TODO(edenfed): add object selector to mutatingwebhookconfiguration
-	log := logf.FromContext(ctx)
 	pod, ok := obj.(*corev1.Pod)
 	if !ok {
 		return fmt.Errorf("expected a Pod but got a %T", obj)
@@ -28,7 +32,58 @@ func (p *PodsWebhook) Default(ctx context.Context, obj runtime.Object) error {
 		pod.Annotations = map[string]string{}
 	}
 
-	//pod.Annotations["odigos.io/instrumented-webhook"] = "true"
-	log.V(0).Info("Defaulted Pod", "name", pod.Name)
+	// Inject ODIGOS environment variables into all containers
+	injectOdigosEnvVars(pod)
+
 	return nil
+}
+
+func injectOdigosEnvVars(pod *corev1.Pod) {
+
+	namespace := pod.Namespace
+	workloadKind := getWorkloadKind(pod)
+
+	for i := range pod.Spec.Containers {
+		container := &pod.Spec.Containers[i]
+
+		envVars := []corev1.EnvVar{
+			{
+				Name:  EnvVarNamespace,
+				Value: namespace,
+			},
+			{
+				Name:  EnvVarWorkloadKind,
+				Value: workloadKind,
+			},
+			{
+				Name:  EnvVarContainerName,
+				Value: container.Name,
+			},
+			{
+				Name: EnvVarPodName,
+				ValueFrom: &corev1.EnvVarSource{
+					FieldRef: &corev1.ObjectFieldSelector{
+						FieldPath: "metadata.name",
+					},
+				},
+			},
+		}
+
+		container.Env = append(container.Env, envVars...)
+	}
+}
+
+// We can assume ReplicaSet is Deployment because this come after we identify the workload is supported by Odigos
+func getWorkloadKind(pod *corev1.Pod) string {
+	for _, ownerRef := range pod.OwnerReferences {
+		switch ownerRef.Kind {
+		case "ReplicaSet":
+			return "Deployment"
+		case "StatefulSet":
+			return "StatefulSet"
+		case "DaemonSet":
+			return "DaemonSet"
+		}
+	}
+	return "Unknown"
 }
