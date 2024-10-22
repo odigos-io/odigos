2487549cb0d2790b035c0ce651478a54e51b492d
Ben Elferink <ben.elferink@icloud.com>
2024-10-22 14:53:35 +0300

    fix: docs link

--- frontend/webapp/reuseable-components/button/docs-button.tsx
+++ frontend/webapp/reuseable-components/button/docs-button.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styled from 'styled-components';
 import { Button, ButtonProps } from '.';
+import { DOCS_LINK } from '@/utils';
 
 const StyledButton = styled(Button)`
   display: flex;
@@ -13,7 +14,7 @@ const StyledButton = styled(Button)`
 
 export const DocsButton = ({ endpoint = '/', variant = 'secondary' }: { endpoint?: string; variant?: ButtonProps['variant'] }) => {
   return (
-    <StyledButton variant={variant} onClick={() => window.open(`https://docs.odigos.io${endpoint}`, '_blank', 'noopener noreferrer')}>
+    <StyledButton variant={variant} onClick={() => window.open(`${DOCS_LINK}${endpoint}`, '_blank', 'noopener noreferrer')}>
       <Image src='/icons/common/notebook.svg' alt='Docs' width={18} height={18} />
       Docs
     </StyledButton>
