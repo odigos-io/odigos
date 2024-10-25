34ca2c33c1fbdf26541b334b542d90e32431d98c
Amir Blum <amirgiraffe@gmail.com>
2024-10-25 16:56:39 +0300

    feat: move default node collector port to avoid conflicts and add setting in config (#1618)
    Since node collector runs as a DaemonSet with host network (so that otlp
    ports 4317 and 4318 are accessible from the pods exporters), it also
    shares the ports namespace with the node.
    
    The ports it binds to might conflict with ports of other tools installed
    in the cluster that does similar things, and in this case data
    collection will fail to start with error: "listen tcp 0.0.0.0:8888:
    bind: address already in use".
    
    The collectors currently expose and scrape their own metrics on port
    `8888` which is good candidate for collistions.
    
    This PR:
    
    - makes the default port for own telemetry endpoint for node collector
    `55682` to have low chance of collision with something that already runs
    on the node (instead of using `8888` as default).
    - Adds an option to odigos config to set the port, in case one needs it
    hard-coded to a specific value to manage the public ports on the node).
    
    The port to use is written on the collectors group CRD by the scheduler
    to abstract away the default and odigos config resolving, and allow the
    resolved value to be more easer to consume.
    
    For the cluster collector, the collector runs as deployment with k8s
    service, thus we can safely use `8888` without colliding with anything
    else

--- scheduler/controllers/collectorgroups/cluster.go
+++ scheduler/controllers/collectorgroups/cluster.go
@@ -6,14 +6,23 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// The cluster gateway collector runs as a deployment and the pod is exposed as a service.
+// Thus it cannot collide with other ports on the same node, and we can use an handy default port.
+const ClusterCollectorDefaultOwnMetricsPort = 8888
+
 func NewClusterCollectorGroup(namespace string) *odigosv1.CollectorsGroup {
 	return &odigosv1.CollectorsGroup{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "CollectorsGroup",
+			APIVersion: "odigos.io/v1alpha1",
+		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      consts.OdigosClusterCollectorCollectorGroupName,
 			Namespace: namespace,
 		},
 		Spec: odigosv1.CollectorsGroupSpec{
-			Role: odigosv1.CollectorsGroupRoleClusterGateway,
+			Role:                    odigosv1.CollectorsGroupRoleClusterGateway,
+			CollectorOwnMetricsPort: ClusterCollectorDefaultOwnMetricsPort,
 		},
 	}
 }
