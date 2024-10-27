f9e49a6b6b72c34efd2c48363e699eef9198c0ae
alonkeyval <alon@keyval.dev>
2024-10-27 13:37:22 +0200

    chore: wip

--- frontend/services/actions/constants.go
+++ frontend/services/actions/constants.go
@@ -3,9 +3,9 @@ package services
 const (
 	ActionTypeAddClusterInfo       = "AddClusterInfo"
 	ActionTypeDeleteAttribute      = "DeleteAttribute"
+	ActionTypeRenameAttribute      = "RenameAttribute"
 	ActionTypePiiMasking           = "PiiMasking"
 	ActionTypeErrorSampler         = "ErrorSampler"
 	ActionTypeLatencySampler       = "LatencySampler"
 	ActionTypeProbabilisticSampler = "ProbabilisticSampler"
-	ActionTypeRenameAttribute      = "RenameAttribute"
 )
