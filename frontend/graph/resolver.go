3c141c8a1bbea0aaf0c6afd5814b795c4a75a6da
alonkeyval <alon@keyval.dev>
2024-11-03 13:54:55 +0200

    wip: remove unused comments

--- frontend/graph/resolver.go
+++ frontend/graph/resolver.go
@@ -5,7 +5,6 @@ import (
 )
 
 // This file will not be regenerated automatically.
-//
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
