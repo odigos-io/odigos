cb33ca000f85081523ff03e1f609e0f6345ed862
Ben Elferink <ben.elferink@icloud.com>
2024-11-06 14:53:05 +0200

    fix: metrics response

--- frontend/webapp/hooks/overview/useMetrics.ts
+++ frontend/webapp/hooks/overview/useMetrics.ts
@@ -1,44 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_METRICS } from '@/graphql/mutations/metrics';
-import type { ActualDestination, K8sActualSource, OverviewMetricsResponse } from '@/types';
+import type { OverviewMetricsResponse } from '@/types';
 
-export function useMetrics({ sources, destinations }: { sources: K8sActualSource[]; destinations: ActualDestination[] }) {
+export function useMetrics() {
   const { data, refetch } = useQuery<OverviewMetricsResponse>(GET_METRICS);
 
-  // this is just to re-render the mockup data
-  const [_, setCount] = useState(0);
-
   useEffect(() => {
-    const interval = setInterval(async () => {
-      await refetch();
-      setCount((n) => n + 1);
-    }, 3000);
-
-    return () => {
-      clearInterval(interval);
-    };
+    const interval = setInterval(async () => await refetch(), 3000);
+    return () => clearInterval(interval);
   }, [refetch]);
 
-  // this will 1st try to use real data, if non-exitant it will generate mockup data
-  const metricsMockup: OverviewMetricsResponse = {
-    sources: data?.sources?.length
-      ? data.sources
-      : sources.map(({ name, kind, namespace }) => ({
-          namespace,
-          kind,
-          name,
-          totalDataSent: 0,
-          throughput: Math.random() * 50000,
-        })),
-    destinations: data?.destinations?.length
-      ? data.destinations
-      : destinations.map(({ id }) => ({
-          id,
-          totalDataSent: 0,
-          throughput: Math.random() * 50000,
-        })),
-  };
-
-  return { metrics: metricsMockup };
+  return { metrics: data };
 }
