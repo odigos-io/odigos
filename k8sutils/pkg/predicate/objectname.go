9603a33304100f25e0dc949b9a3c470687e3ab18
Amir Blum <amirgiraffe@gmail.com>
2024-11-09 11:34:19 +0200

    refactor: extract common predicates to k8s utils (#1714)
    Using the right event filters for controllers is very important to
    reduce resource utilization, avoid un-needed calls to api-server, and
    reduce chance to race conditions when multiple things changes at the
    same time.
    
    We keep refining our controller event filters to achieve better
    stability for odigos.
    
    Right now, we define our custom predicates near the controller where
    they are used, but this make it less easy to reuse existing common
    predicates and share code. This PR move some of the common and reuseable
    predicates to `k8sutils` module and refactor how they are being set up

---
+++ k8sutils/pkg/predicate/objectname.go
@@ -0,0 +1,84 @@
+package predicate
+
+import (
+	"github.com/odigos-io/odigos/common/consts"
+	odigosk8sconsts "github.com/odigos-io/odigos/k8sutils/pkg/consts"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	cr_predicate "sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+type ObjectNamePredicate struct {
+	AllowedObjectName string
+}
+
+func (o ObjectNamePredicate) Create(e event.CreateEvent) bool {
+	if e.Object == nil {
+		return false
+	}
+	return e.Object.GetName() == o.AllowedObjectName
+}
+
+func (i ObjectNamePredicate) Update(e event.UpdateEvent) bool {
+	if e.ObjectNew == nil || e.ObjectOld == nil {
+		return false
+	}
+	return e.ObjectNew.GetName() == i.AllowedObjectName
+}
+
+func (i ObjectNamePredicate) Delete(e event.DeleteEvent) bool {
+	if e.Object == nil {
+		return false
+	}
+	return e.Object.GetName() == i.AllowedObjectName
+}
+
+func (i ObjectNamePredicate) Generic(e event.GenericEvent) bool {
+	if e.Object == nil {
+		return false
+	}
+	return e.Object.GetName() == i.AllowedObjectName
+}
+
+var _ cr_predicate.Predicate = &ObjectNamePredicate{}
+
+// This predicate will only allow config map events on the "odigos-config" object,
+// and will filter out events for possible other config maps which the reconciler should not handle.
+// Example usage:
+// import odigospredicates "github.com/odigos-io/odigos/k8sutils/pkg/predicate"
+// ...
+//
+//	err = ctrl.NewControllerManagedBy(mgr).
+//		For(&corev1.ConfigMap{}).
+//		WithEventFilter(&odigospredicates.OdigosConfigMapPredicate).
+//		Complete(r)
+var OdigosConfigMapPredicate = ObjectNamePredicate{
+	AllowedObjectName: consts.OdigosConfigurationName,
+}
+
+// use this event filter to reconcile only collectors group events for node collectors group objects
+// this is useful if you reconcile only depends on changes from the node collectors group and should not react to cluster collectors group changes
+// example usage:
+// import odigospredicates "github.com/odigos-io/odigos/k8sutils/pkg/predicate"
+// ...
+//
+//	err = ctrl.NewControllerManagedBy(mgr).
+//		For(&odigosv1.CollectorsGroup{}).
+//		WithEventFilter(&odigospredicates.OdigosCollectorsGroupCluster).
+//		Complete(r)
+var OdigosCollectorsGroupNodePredicate = ObjectNamePredicate{
+	AllowedObjectName: odigosk8sconsts.OdigosNodeCollectorCollectorGroupName,
+}
+
+// use this event filter to reconcile only collectors group events for cluster collectors group objects
+// this is useful if you reconcile only depends on changes from the cluster collectors group and should not react to node collectors group changes
+// example usage:
+// import odigospredicates "github.com/odigos-io/odigos/k8sutils/pkg/predicate"
+// ...
+//
+//	err = ctrl.NewControllerManagedBy(mgr).
+//		For(&odigosv1.CollectorsGroup{}).
+//		WithEventFilter(&odigospredicates.OdigosCollectorsGroupClusterPredicate).
+//		Complete(r)
+var OdigosCollectorsGroupClusterPredicate = ObjectNamePredicate{
+	AllowedObjectName: odigosk8sconsts.OdigosClusterCollectorCollectorGroupName,
+}
