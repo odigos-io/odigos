8c938e707da480b105c9fcbebed19fbc308106b8
alonkeyval <alon@odigos.io>
2024-11-10 09:49:58 +0200

    chore: remove files

--- frontend/webapp/design.system/loader/loader.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Loader } from '@keyval-dev/design-system';
-
-export function KeyvalLoader({ width = 24, height = 24 }) {
-  return <Loader width={width} height={height} />;
-}
--- frontend/webapp/design.system/tooltip/tooltip.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Tooltip } from "@keyval-dev/design-system";
-
-export function KeyvalTooltip(props: any) {
-  return <Tooltip {...props} />;
-}
