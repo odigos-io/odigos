65cf1186f0a56d3148d7abe3456d70c9a6e40013
alonkeyval <alonbraymok007@gmail.com>
2024-11-12 11:35:05 +0300

    chore: remove fetch from home path

--- frontend/webapp/app/page.tsx
+++ frontend/webapp/app/page.tsx
@@ -1,37 +1,37 @@
 'use client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useConfig, useNotify } from '@/hooks';
+import { useNotify } from '@/hooks';
 import { ROUTES, CONFIG, NOTIFICATION } from '@/utils';
 import { FadeLoader } from '@/reuseable-components';
 
 export default function App() {
   const router = useRouter();
   const notify = useNotify();
-  const { data, error } = useConfig();
+  // const { data, error } = useConfig();
 
   useEffect(() => {
-    if (error) {
-      notify({
-        type: NOTIFICATION.ERROR,
-        title: error.name,
-        message: error.message,
-      });
+    router.push(ROUTES.OVERVIEW);
+    // if (error) {
+    //   notify({
+    //     type: NOTIFICATION.ERROR,
+    //     title: error.name,
+    //     message: error.message,
+    //   });
 
-      router.push(ROUTES.OVERVIEW);
-    } else if (data) {
-      const { installation } = data;
+    // } else if (data) {
+    //   const { installation } = data;
 
-      switch (installation) {
-        case CONFIG.NEW:
-        case CONFIG.APPS_SELECTED:
-          router.push(ROUTES.CHOOSE_SOURCES);
-          break;
-        case CONFIG.FINISHED:
-          router.push(ROUTES.OVERVIEW);
-      }
-    }
-  }, [data, error]);
+    //   switch (installation) {
+    //     case CONFIG.NEW:
+    //     case CONFIG.APPS_SELECTED:
+    //       router.push(ROUTES.CHOOSE_SOURCES);
+    //       break;
+    //     case CONFIG.FINISHED:
+    //       router.push(ROUTES.OVERVIEW);
+    //   }
+    // }
+  }, []);
 
   return <FadeLoader />;
 }
