f37d3e61f0e6521ab9841d79a4d33e277e8d38aa
alonkeyval <alonbraymok007@gmail.com>
2024-11-12 13:41:43 +0300

    chore: revert config

--- frontend/webapp/app/page.tsx
+++ frontend/webapp/app/page.tsx
@@ -2,8 +2,8 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useNotify, useConfig } from '@/hooks';
-import { ROUTES, CONFIG, NOTIFICATION } from '@/utils';
 import { FadeLoader } from '@/reuseable-components';
+import { ROUTES, CONFIG, NOTIFICATION } from '@/utils';
 
 export default function App() {
   const router = useRouter();
@@ -11,27 +11,25 @@ export default function App() {
   const { data, error } = useConfig();
 
   useEffect(() => {
-    router.push(ROUTES.OVERVIEW);
-    // if (error) {
-    //   notify({
-    //     type: NOTIFICATION.ERROR,
-    //     title: error.name,
-    //     message: error.message,
-    //   });
+    if (error) {
+      notify({
+        type: NOTIFICATION.ERROR,
+        title: error.name,
+        message: error.message,
+      });
+    } else if (data) {
+      const { installation } = data;
 
-    // } else if (data) {
-    //   const { installation } = data;
-
-    //   switch (installation) {
-    //     case CONFIG.NEW:
-    //     case CONFIG.APPS_SELECTED:
-    //       router.push(ROUTES.CHOOSE_SOURCES);
-    //       break;
-    //     case CONFIG.FINISHED:
-    //       router.push(ROUTES.OVERVIEW);
-    //   }
-    // }
-  }, []);
+      switch (installation) {
+        case CONFIG.NEW:
+        case CONFIG.APPS_SELECTED:
+          router.push(ROUTES.CHOOSE_SOURCES);
+          break;
+        case CONFIG.FINISHED:
+          router.push(ROUTES.OVERVIEW);
+      }
+    }
+  }, [data]);
 
   return <FadeLoader />;
 }
--- frontend/webapp/utils/constants/urls.tsx
+++ frontend/webapp/utils/constants/urls.tsx
@@ -4,6 +4,7 @@ const IS_PRODUCTION = ENV === 'production';
 
 // Define base URLs depending on the environment and rendering context
 const LOCAL_API_BASE = 'http://localhost:8085';
+//we use localhost:8085 as the base URL for server environment
 const PRODUCTION_GQL_API_BASE = IS_PRODUCTION && typeof window !== 'undefined' ? `${window.location.origin}/graphql` : `${LOCAL_API_BASE}/graphql`;
 const API_BASE_URL = IS_PRODUCTION ? PRODUCTION_GQL_API_BASE : `${LOCAL_API_BASE}/graphql`;
 
