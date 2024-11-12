0de537e9b7e1a20610b288046aadc3b2b7d617ff
alonkeyval <alonbraymok007@gmail.com>
2024-11-12 11:00:59 +0300

    chore: check if window is define

--- frontend/webapp/utils/constants/urls.tsx
+++ frontend/webapp/utils/constants/urls.tsx
@@ -2,16 +2,15 @@
 const ENV = process.env.NODE_ENV;
 const IS_PRODUCTION = ENV === 'production';
 
-// Define base URLs depending on the environment
+// Define base URLs depending on the environment and rendering context
 const LOCAL_API_BASE = 'http://localhost:8085';
-const PRODUCTION_GQL_API_BASE = `${window.location.origin}/graphql`;
-const PRODUCTION_API_BASE = '/';
+const PRODUCTION_GQL_API_BASE = IS_PRODUCTION && typeof window !== 'undefined' ? `${window.location.origin}/graphql` : '/graphql';
 const API_BASE_URL = IS_PRODUCTION ? PRODUCTION_GQL_API_BASE : `${LOCAL_API_BASE}/graphql`;
 
 // Define endpoints based on the base URL
 const API = {
   BASE_URL: API_BASE_URL,
-  EVENTS: `${IS_PRODUCTION ? PRODUCTION_API_BASE : LOCAL_API_BASE}/api/events`,
+  EVENTS: `${IS_PRODUCTION ? '/' : LOCAL_API_BASE}/api/events`,
 };
 
 // Centralize external links
