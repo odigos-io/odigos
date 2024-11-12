289a5869e578b526669dc0f2adf868e769292e40
alonkeyval <alonbraymok007@gmail.com>
2024-11-12 11:04:42 +0300

    chore: change local gql

--- frontend/webapp/utils/constants/urls.tsx
+++ frontend/webapp/utils/constants/urls.tsx
@@ -4,7 +4,7 @@ const IS_PRODUCTION = ENV === 'production';
 
 // Define base URLs depending on the environment and rendering context
 const LOCAL_API_BASE = 'http://localhost:8085';
-const PRODUCTION_GQL_API_BASE = IS_PRODUCTION && typeof window !== 'undefined' ? `${window.location.origin}/graphql` : '/graphql';
+const PRODUCTION_GQL_API_BASE = IS_PRODUCTION && typeof window !== 'undefined' ? `${window.location.origin}/graphql` : `${LOCAL_API_BASE}/graphql`;
 const API_BASE_URL = IS_PRODUCTION ? PRODUCTION_GQL_API_BASE : `${LOCAL_API_BASE}/graphql`;
 
 // Define endpoints based on the base URL
