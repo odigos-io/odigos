9cb5531f6c5360cc98cc28055a466401ad2ed146
Ben Elferink <ben.elferink@icloud.com>
2024-11-17 11:52:30 +0200

    feat: add react flow controls

--- frontend/webapp/reuseable-components/nodes-data-flow/index.tsx
+++ frontend/webapp/reuseable-components/nodes-data-flow/index.tsx
@@ -3,10 +3,11 @@ import React, { useMemo } from 'react';
 import '@xyflow/react/dist/style.css';
 import AddNode from './nodes/add-node';
 import BaseNode from './nodes/base-node';
-import { ReactFlow } from '@xyflow/react';
+import { Controls, ReactFlow } from '@xyflow/react';
 import GroupNode from './nodes/group-node';
 import HeaderNode from './nodes/header-node';
 import LabeledEdge from './edges/labeled-edge';
+import styled from 'styled-components';
 
 interface NodeBaseDataFlowProps {
   nodes: any[];
@@ -14,6 +15,29 @@ interface NodeBaseDataFlowProps {
   onNodeClick?: (event: React.MouseEvent, object: any) => void;
   nodeWidth: number;
 }
+
+const FlowWrapper = styled.div`
+  height: calc(100vh - 160px);
+  .react-flow__attribution {
+    visibility: hidden;
+  }
+`;
+
+const ControllerWrapper = styled.div`
+  button {
+    padding: 8px;
+    margin: 8px;
+    border-radius: 8px;
+    border: 1px solid ${({ theme }) => theme.colors.border};
+    background-color: ${({ theme }) => theme.colors.dropdown_bg};
+    path {
+      fill: #fff;
+    }
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.dropdown_bg_2};
+    }
+  }
+`;
 
 export function NodeBaseDataFlow({ nodes, edges, onNodeClick, nodeWidth }: NodeBaseDataFlowProps) {
   const nodeTypes = useMemo(
@@ -23,28 +47,24 @@ export function NodeBaseDataFlow({ nodes, edges, onNodeClick, nodeWidth }: NodeB
       base: (props) => <BaseNode {...props} nodeWidth={nodeWidth} />,
       group: GroupNode,
     }),
-    [nodeWidth]
+    [nodeWidth],
   );
 
   const edgeTypes = useMemo(
     () => ({
       labeled: LabeledEdge,
     }),
-    []
+    [],
   );
 
   return (
-    <div style={{ height: 'calc(100vh - 100px)' }}>
-      <ReactFlow
-        nodes={nodes}
-        nodeTypes={nodeTypes}
-        edges={edges}
-        edgeTypes={edgeTypes}
-        onNodeClick={onNodeClick}
-        zoomOnScroll={false}
-        fitView={false}
-      />
-    </div>
+    <FlowWrapper>
+      <ReactFlow nodes={nodes} nodeTypes={nodeTypes} edges={edges} edgeTypes={edgeTypes} onNodeClick={onNodeClick} zoomOnScroll={false} fitView={false}>
+        <ControllerWrapper>
+          <Controls position='bottom-left' orientation='horizontal' showZoom showFitView showInteractive={false} />
+        </ControllerWrapper>
+      </ReactFlow>
+    </FlowWrapper>
   );
 }
 
