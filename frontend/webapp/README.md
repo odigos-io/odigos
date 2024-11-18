a320b6ffcf5a8b1c6d7ba6cc36d2e63e27b22718
Ben Elferink <ben.elferink@icloud.com>
2024-11-18 13:40:41 +0200

    [GEN-1699]: update dependencies (#1779)
    Co-authored-by: Alon Braymok <138359965+alonkeyval@users.noreply.github.com>

--- frontend/webapp/README.md
+++ frontend/webapp/README.md
@@ -22,8 +22,6 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 
 You can start editing the pages by modifying `app` folder. The page auto-updates as you edit the file.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
 ## Learn More
 
 To learn more about Next.js, take a look at the following resources:
--- frontend/webapp/app/layout.tsx
+++ frontend/webapp/app/layout.tsx
@@ -20,9 +20,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <head>
-        <link rel='icon' href={`/${METADATA.icons}`} type='image/svg+xml' />
         <title>{METADATA.title}</title>
         <meta name='description' content={METADATA.title} />
+        <link rel='icon' type='image/svg+xml' href={`/${METADATA.icons}`} />
+        <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='manifest' href='/manifest.json' />
       </head>
       <ApolloWrapper>
         <ThemeProviderWrapper>
