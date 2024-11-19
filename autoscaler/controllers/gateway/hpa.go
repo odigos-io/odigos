16d322b791c253e4e1231b543fb5aa0fa7bc52c0
tamirdavid1 <tamir@odigos.io>
2024-11-19 12:03:34 +0200

    fix: change hpa creation

--- autoscaler/controllers/gateway/hpa.go
+++ autoscaler/controllers/gateway/hpa.go
@@ -4,20 +4,20 @@ import (
 	"context"
 	"fmt"
 
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/version"
-	"sigs.k8s.io/yaml"
-
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	"k8s.io/apimachinery/pkg/api/resource"
 
 	odigosv1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
-	autoscaling "k8s.io/api/autoscaling/v2"
+	autoscalingv2 "k8s.io/api/autoscaling/v2"
+	autoscalingv2beta1 "k8s.io/api/autoscaling/v2beta1"
+	autoscalingv2beta2 "k8s.io/api/autoscaling/v2beta2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 const (
@@ -33,44 +33,118 @@ var (
 func syncHPA(gateway *odigosv1.CollectorsGroup, ctx context.Context, c client.Client, scheme *runtime.Scheme, memConfig *memoryConfigurations, kubeVersion *version.Version) error {
 	logger := log.FromContext(ctx)
 
+	var hpa metav1.Object
 	memLimit := memConfig.gomemlimitMiB * memoryLimitPercentageForHPA / 100.0
 	metricQuantity := resource.MustParse(fmt.Sprintf("%dMi", memLimit))
 
-	hpa := &autoscaling.HorizontalPodAutoscaler{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: getHPAVersion(kubeVersion),
-			Kind:       "HorizontalPodAutoscaler",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      gateway.Name,
-			Namespace: gateway.Namespace,
-		},
-		Spec: autoscaling.HorizontalPodAutoscalerSpec{
-			ScaleTargetRef: autoscaling.CrossVersionObjectReference{
-				APIVersion: "apps/v1",
-				Kind:       "Deployment",
-				Name:       gateway.Name,
+	switch {
+	case kubeVersion.LessThan(version.MustParse("1.23.0")):
+		fmt.Println("Kube version is less than 1.23.0")
+		hpa = &autoscalingv2beta1.HorizontalPodAutoscaler{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "autoscaling/v2beta1",
+				Kind:       "HorizontalPodAutoscaler",
 			},
-			MinReplicas: minReplicas,
-			MaxReplicas: maxReplicas,
-			Metrics: []autoscaling.MetricSpec{
-				{
-					Type: autoscaling.ResourceMetricSourceType,
-					Resource: &autoscaling.ResourceMetricSource{
-						Name: "memory",
-						Target: autoscaling.MetricTarget{
-							Type:         autoscaling.AverageValueMetricType,
-							AverageValue: &metricQuantity,
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      gateway.Name,
+				Namespace: gateway.Namespace,
+			},
+			Spec: autoscalingv2beta1.HorizontalPodAutoscalerSpec{
+				ScaleTargetRef: autoscalingv2beta1.CrossVersionObjectReference{
+					APIVersion: "apps/v1",
+					Kind:       "Deployment",
+					Name:       gateway.Name,
+				},
+				MinReplicas: minReplicas,
+				MaxReplicas: maxReplicas,
+				Metrics: []autoscalingv2beta1.MetricSpec{
+					{
+						Type: autoscalingv2beta1.ResourceMetricSourceType,
+						Resource: &autoscalingv2beta1.ResourceMetricSource{
+							Name:               "memory",
+							TargetAverageValue: &metricQuantity,
 						},
 					},
 				},
 			},
-			Behavior: &autoscaling.HorizontalPodAutoscalerBehavior{
-				ScaleDown: &autoscaling.HPAScalingRules{
-					StabilizationWindowSeconds: stabilizationWindowSeconds,
+		}
+	case kubeVersion.LessThan(version.MustParse("1.25.0")):
+		fmt.Println("Kube version is less than 1.25.0")
+		hpa = &autoscalingv2beta2.HorizontalPodAutoscaler{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "autoscaling/v2beta2",
+				Kind:       "HorizontalPodAutoscaler",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      gateway.Name,
+				Namespace: gateway.Namespace,
+			},
+			Spec: autoscalingv2beta2.HorizontalPodAutoscalerSpec{
+				ScaleTargetRef: autoscalingv2beta2.CrossVersionObjectReference{
+					APIVersion: "apps/v1",
+					Kind:       "Deployment",
+					Name:       gateway.Name,
+				},
+				MinReplicas: minReplicas,
+				MaxReplicas: maxReplicas,
+				Metrics: []autoscalingv2beta2.MetricSpec{
+					{
+						Type: autoscalingv2beta2.ResourceMetricSourceType,
+						Resource: &autoscalingv2beta2.ResourceMetricSource{
+							Name: "memory",
+							Target: autoscalingv2beta2.MetricTarget{
+								Type:         autoscalingv2beta2.AverageValueMetricType,
+								AverageValue: &metricQuantity,
+							},
+						},
+					},
+				},
+				Behavior: &autoscalingv2beta2.HorizontalPodAutoscalerBehavior{
+					ScaleDown: &autoscalingv2beta2.HPAScalingRules{
+						StabilizationWindowSeconds: stabilizationWindowSeconds,
+					},
 				},
 			},
-		},
+		}
+	default:
+		fmt.Println("Kube version is greater than or equal to 1.25.0")
+
+		hpa = &autoscalingv2.HorizontalPodAutoscaler{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "autoscaling/v2",
+				Kind:       "HorizontalPodAutoscaler",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      gateway.Name,
+				Namespace: gateway.Namespace,
+			},
+			Spec: autoscalingv2.HorizontalPodAutoscalerSpec{
+				ScaleTargetRef: autoscalingv2.CrossVersionObjectReference{
+					APIVersion: "apps/v1",
+					Kind:       "Deployment",
+					Name:       gateway.Name,
+				},
+				MinReplicas: minReplicas,
+				MaxReplicas: maxReplicas,
+				Metrics: []autoscalingv2.MetricSpec{
+					{
+						Type: autoscalingv2.ResourceMetricSourceType,
+						Resource: &autoscalingv2.ResourceMetricSource{
+							Name: "memory",
+							Target: autoscalingv2.MetricTarget{
+								Type:         autoscalingv2.AverageValueMetricType,
+								AverageValue: &metricQuantity,
+							},
+						},
+					},
+				},
+				Behavior: &autoscalingv2.HorizontalPodAutoscalerBehavior{
+					ScaleDown: &autoscalingv2.HPAScalingRules{
+						StabilizationWindowSeconds: stabilizationWindowSeconds,
+					},
+				},
+			},
+		}
 	}
 
 	if err := controllerutil.SetControllerReference(gateway, hpa, scheme); err != nil {
@@ -78,30 +152,76 @@ func syncHPA(gateway *odigosv1.CollectorsGroup, ctx context.Context, c client.Cl
 		return err
 	}
 
-	hpaBytes, _ := yaml.Marshal(hpa)
+	// Also need to type-assert for Patch
+	switch v := hpa.(type) {
+	case *autoscalingv2beta1.HorizontalPodAutoscaler:
+		// Create a JSON map of our HPA
+		jsonMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(v)
+		if err != nil {
+			logger.Error(err, "Failed to convert HPA to unstructured")
+			return err
+		}
 
-	force := true
-	patchOptions := client.PatchOptions{
-		FieldManager: "odigos",
-		Force:        &force,
-	}
+		obj := &unstructured.Unstructured{Object: jsonMap}
+		// Explicitly set these again to be sure
+		obj.SetAPIVersion("autoscaling/v2beta1")
+		obj.SetKind("HorizontalPodAutoscaler")
+		obj.SetName(gateway.Name)
+		obj.SetNamespace(gateway.Namespace)
 
-	return c.Patch(ctx, hpa, client.RawPatch(types.ApplyPatchType, hpaBytes), &patchOptions)
-}
+		force := true
+		patchOptions := client.PatchOptions{
+			FieldManager: "odigos",
+			Force:        &force,
+		}
+		return c.Patch(ctx, obj, client.Apply, &patchOptions)
+	case *autoscalingv2beta2.HorizontalPodAutoscaler:
+		jsonMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(v)
+		if err != nil {
+			logger.Error(err, "Failed to convert HPA to unstructured")
+			return err
+		}
 
-// getHPAVersion returns the HPA version to use based on the Kubernetes version
-func getHPAVersion(kubeVersion *version.Version) string {
-	if kubeVersion == nil {
-		return "autoscaling/v2" // Default to latest
-	}
+		obj := &unstructured.Unstructured{Object: jsonMap}
+		obj.SetAPIVersion("autoscaling/v2beta2")
+		obj.SetKind("HorizontalPodAutoscaler")
+		obj.SetName(gateway.Name)
+		obj.SetNamespace(gateway.Namespace)
 
-	// Kubernetes version compatibility for HPA
-	switch {
-	case kubeVersion.LessThan(version.MustParse("1.23.0")):
-		return "autoscaling/v2beta1"
-	case kubeVersion.LessThan(version.MustParse("1.25.0")):
-		return "autoscaling/v2beta2"
+		logger.Info("Unstructured object after setting fields",
+			"name", obj.GetName(),
+			"namespace", obj.GetNamespace(),
+			"apiVersion", obj.GetAPIVersion(),
+			"kind", obj.GetKind())
+
+		force := true
+		patchOptions := client.PatchOptions{
+			FieldManager: "odigos",
+			Force:        &force,
+		}
+		return c.Patch(ctx, obj, client.Apply, &patchOptions)
+	case *autoscalingv2.HorizontalPodAutoscaler:
+		logger.Info("Original HPA object", "name", v.GetName(), "namespace", v.GetNamespace())
+
+		jsonMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(v)
+		if err != nil {
+			logger.Error(err, "Failed to convert HPA to unstructured")
+			return err
+		}
+
+		obj := &unstructured.Unstructured{Object: jsonMap}
+		obj.SetAPIVersion("autoscaling/v2")
+		obj.SetKind("HorizontalPodAutoscaler")
+		obj.SetName(gateway.Name)
+		obj.SetNamespace(gateway.Namespace)
+
+		force := true
+		patchOptions := client.PatchOptions{
+			FieldManager: "odigos",
+			Force:        &force,
+		}
+		return c.Patch(ctx, obj, client.Apply, &patchOptions)
 	default:
-		return "autoscaling/v2"
+		return fmt.Errorf("unsupported HPA type")
 	}
 }
