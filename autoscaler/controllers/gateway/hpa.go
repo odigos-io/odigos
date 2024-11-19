d17f4a8b678b2e060fb5ca8f4559fc56dd464353
tamirdavid1 <tamir@odigos.io>
2024-11-19 16:26:38 +0200

    feat: create gateway hpa based on the k8s version

--- autoscaler/controllers/gateway/hpa.go
+++ autoscaler/controllers/gateway/hpa.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"k8s.io/apimachinery/pkg/util/version"
+	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -16,7 +17,6 @@ import (
 	autoscalingv2beta1 "k8s.io/api/autoscaling/v2beta1"
 	autoscalingv2beta2 "k8s.io/api/autoscaling/v2beta2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -33,22 +33,19 @@ var (
 func syncHPA(gateway *odigosv1.CollectorsGroup, ctx context.Context, c client.Client, scheme *runtime.Scheme, memConfig *memoryConfigurations, kubeVersion *version.Version) error {
 	logger := log.FromContext(ctx)
 
-	var hpa metav1.Object
+	var hpa client.Object
+
 	memLimit := memConfig.gomemlimitMiB * memoryLimitPercentageForHPA / 100.0
 	metricQuantity := resource.MustParse(fmt.Sprintf("%dMi", memLimit))
 
 	switch {
 	case kubeVersion.LessThan(version.MustParse("1.23.0")):
-		fmt.Println("Kube version is less than 1.23.0")
 		hpa = &autoscalingv2beta1.HorizontalPodAutoscaler{
 			TypeMeta: metav1.TypeMeta{
 				APIVersion: "autoscaling/v2beta1",
 				Kind:       "HorizontalPodAutoscaler",
 			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      gateway.Name,
-				Namespace: gateway.Namespace,
-			},
+			ObjectMeta: buildHPACommonFields(gateway),
 			Spec: autoscalingv2beta1.HorizontalPodAutoscalerSpec{
 				ScaleTargetRef: autoscalingv2beta1.CrossVersionObjectReference{
 					APIVersion: "apps/v1",
@@ -69,16 +66,12 @@ func syncHPA(gateway *odigosv1.CollectorsGroup, ctx context.Context, c client.Cl
 			},
 		}
 	case kubeVersion.LessThan(version.MustParse("1.25.0")):
-		fmt.Println("Kube version is less than 1.25.0")
 		hpa = &autoscalingv2beta2.HorizontalPodAutoscaler{
 			TypeMeta: metav1.TypeMeta{
 				APIVersion: "autoscaling/v2beta2",
 				Kind:       "HorizontalPodAutoscaler",
 			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      gateway.Name,
-				Namespace: gateway.Namespace,
-			},
+			ObjectMeta: buildHPACommonFields(gateway),
 			Spec: autoscalingv2beta2.HorizontalPodAutoscalerSpec{
 				ScaleTargetRef: autoscalingv2beta2.CrossVersionObjectReference{
 					APIVersion: "apps/v1",
@@ -107,17 +100,12 @@ func syncHPA(gateway *odigosv1.CollectorsGroup, ctx context.Context, c client.Cl
 			},
 		}
 	default:
-		fmt.Println("Kube version is greater than or equal to 1.25.0")
-
 		hpa = &autoscalingv2.HorizontalPodAutoscaler{
 			TypeMeta: metav1.TypeMeta{
 				APIVersion: "autoscaling/v2",
 				Kind:       "HorizontalPodAutoscaler",
 			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      gateway.Name,
-				Namespace: gateway.Namespace,
-			},
+			ObjectMeta: buildHPACommonFields(gateway),
 			Spec: autoscalingv2.HorizontalPodAutoscalerSpec{
 				ScaleTargetRef: autoscalingv2.CrossVersionObjectReference{
 					APIVersion: "apps/v1",
@@ -152,76 +140,24 @@ func syncHPA(gateway *odigosv1.CollectorsGroup, ctx context.Context, c client.Cl
 		return err
 	}
 
-	// Also need to type-assert for Patch
-	switch v := hpa.(type) {
-	case *autoscalingv2beta1.HorizontalPodAutoscaler:
-		// Create a JSON map of our HPA
-		jsonMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(v)
-		if err != nil {
-			logger.Error(err, "Failed to convert HPA to unstructured")
-			return err
-		}
+	// Use the Apply patch strategy
+	applyOpts := &client.PatchOptions{
+		FieldManager: "odigos",
+		Force:        pointer.Bool(true),
+	}
 
-		obj := &unstructured.Unstructured{Object: jsonMap}
-		// Explicitly set these again to be sure
-		obj.SetAPIVersion("autoscaling/v2beta1")
-		obj.SetKind("HorizontalPodAutoscaler")
-		obj.SetName(gateway.Name)
-		obj.SetNamespace(gateway.Namespace)
+	if err := c.Patch(ctx, hpa, client.Apply, applyOpts); err != nil {
+		logger.Error(err, "Failed to apply patch to HPA")
+		return err
+	}
 
-		force := true
-		patchOptions := client.PatchOptions{
-			FieldManager: "odigos",
-			Force:        &force,
-		}
-		return c.Patch(ctx, obj, client.Apply, &patchOptions)
-	case *autoscalingv2beta2.HorizontalPodAutoscaler:
-		jsonMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(v)
-		if err != nil {
-			logger.Error(err, "Failed to convert HPA to unstructured")
-			return err
-		}
+	logger.Info("Successfully applied HPA", "name", gateway.Name, "namespace", gateway.Namespace)
+	return nil
+}
 
-		obj := &unstructured.Unstructured{Object: jsonMap}
-		obj.SetAPIVersion("autoscaling/v2beta2")
-		obj.SetKind("HorizontalPodAutoscaler")
-		obj.SetName(gateway.Name)
-		obj.SetNamespace(gateway.Namespace)
-
-		logger.Info("Unstructured object after setting fields",
-			"name", obj.GetName(),
-			"namespace", obj.GetNamespace(),
-			"apiVersion", obj.GetAPIVersion(),
-			"kind", obj.GetKind())
-
-		force := true
-		patchOptions := client.PatchOptions{
-			FieldManager: "odigos",
-			Force:        &force,
-		}
-		return c.Patch(ctx, obj, client.Apply, &patchOptions)
-	case *autoscalingv2.HorizontalPodAutoscaler:
-		logger.Info("Original HPA object", "name", v.GetName(), "namespace", v.GetNamespace())
-
-		jsonMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(v)
-		if err != nil {
-			logger.Error(err, "Failed to convert HPA to unstructured")
-			return err
-		}
-
-		obj := &unstructured.Unstructured{Object: jsonMap}
-		obj.SetAPIVersion("autoscaling/v2")
-		obj.SetKind("HorizontalPodAutoscaler")
-		obj.SetName(gateway.Name)
-		obj.SetNamespace(gateway.Namespace)
-
-		force := true
-		patchOptions := client.PatchOptions{
-			FieldManager: "odigos",
-			Force:        &force,
-		}
-		return c.Patch(ctx, obj, client.Apply, &patchOptions)
-	default:
-		return fmt.Errorf("unsupported HPA type")
+func buildHPACommonFields(gateway *odigosv1.CollectorsGroup) metav1.ObjectMeta {
+	return metav1.ObjectMeta{
+		Name:      gateway.Name,
+		Namespace: gateway.Namespace,
 	}
 }
