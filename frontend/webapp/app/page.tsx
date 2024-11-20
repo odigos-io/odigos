7aad541c2a9ee94b0db7c12d4573e0cbed5ce49d
alonkeyval <alonbraymok007@gmail.com>
2024-11-20 17:01:15 +0200

    chore: wip

--- frontend/webapp/app/page.tsx
+++ frontend/webapp/app/page.tsx
@@ -8,53 +8,60 @@ import { getDestinations, getConfig } from '@/services';
 import { addNotification, store } from '@/store';
 export default function App() {
   const router = useRouter();
-  const { data, isLoading: isConfigLoading } = useQuery(
-    [QUERIES.API_CONFIG],
-    getConfig
-  );
+  // const { data, isLoading: isConfigLoading } = useQuery(
+  //   [QUERIES.API_CONFIG],
+  //   getConfig
+  // );
   const {
     isLoading: isDestinationLoading,
     data: destinationList,
     error,
   } = useQuery([QUERIES.API_DESTINATIONS], getDestinations);
 
-  useEffect(() => {
-    if (isConfigLoading || isDestinationLoading || error) return;
+  // useEffect(() => {
+  //   if (isConfigLoading || isDestinationLoading || error) return;
 
+  //   renderCurrentPage();
+  // }, [data, destinationList, isConfigLoading || error]);
+
+  // useEffect(() => {
+  //   if (!error) return;
+  //   store.dispatch(
+  //     addNotification({
+  //       id: '1',
+  //       message: 'An error occurred',
+  //       title: 'Error',
+  //       type: 'error',
+  //       target: 'notification',
+  //       crdType: 'notification',
+  //     })
+  //   );
+  //   router.push(ROUTES.OVERVIEW);
+  // }, [error]);
+
+  useEffect(() => {
+    console.log({ destinationList, isDestinationLoading });
+    if (isDestinationLoading) return;
     renderCurrentPage();
-  }, [data, destinationList, isConfigLoading || error]);
-
-  useEffect(() => {
-    if (!error) return;
-    store.dispatch(
-      addNotification({
-        id: '1',
-        message: 'An error occurred',
-        title: 'Error',
-        type: 'error',
-        target: 'notification',
-        crdType: 'notification',
-      })
-    );
-    router.push(ROUTES.OVERVIEW);
-  }, [error]);
+  }, [isDestinationLoading]);
 
   function renderCurrentPage() {
-    const { installation } = data;
-
+    // const { installation } = data;
     if (destinationList.length > 0) {
       router.push(ROUTES.OVERVIEW);
       return;
     }
 
-    switch (installation) {
-      case CONFIG.NEW:
-      case CONFIG.APPS_SELECTED:
-        router.push(ROUTES.CHOOSE_SOURCES);
-        break;
-      case CONFIG.FINISHED:
-        router.push(ROUTES.OVERVIEW);
-    }
+    router.push(ROUTES.CHOOSE_SOURCES);
+
+    // switch (installation) {
+    //   case CONFIG.NEW:
+    //   case CONFIG.APPS_SELECTED:
+    //     router.push(ROUTES.CHOOSE_SOURCES);
+    //     break;
+    //   case CONFIG.FINISHED:
+    //     router.push(ROUTES.OVERVIEW);
+    // }
   }
 
   return <Loader />;
--- frontend/webapp/utils/constants/urls.tsx
+++ frontend/webapp/utils/constants/urls.tsx
@@ -1,5 +1,7 @@
+const env = process.env.NODE_ENV;
+
 const LOCALHOST = 'http://localhost:8085/api';
-const BASE_URL = LOCALHOST;
+const BASE_URL = env === 'production' ? '/api' : LOCALHOST;
 
 const API = {
   EVENTS: `${BASE_URL}/events`,
