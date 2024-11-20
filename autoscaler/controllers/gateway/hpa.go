9232f7afcf48600540cf571f408f01ef4c3c6821
tamirdavid1 <tamir@odigos.io>
2024-11-20 12:21:14 +0200

    fix: change gateway name to const

--- autoscaler/controllers/gateway/hpa.go
+++ autoscaler/controllers/gateway/hpa.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/odigos-io/odigos/k8sutils/pkg/consts"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/util/version"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	"k8s.io/apimachinery/pkg/api/resource"
 
 	odigosv1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
 	autoscalingv2 "k8s.io/api/autoscaling/v2"
@@ -50,7 +50,7 @@ func syncHPA(gateway *odigosv1.CollectorsGroup, ctx context.Context, c client.Cl
 				ScaleTargetRef: autoscalingv2beta1.CrossVersionObjectReference{
 					APIVersion: "apps/v1",
 					Kind:       "Deployment",
-					Name:       gateway.Name,
+					Name:       consts.OdigosClusterCollectorDeploymentName,
 				},
 				MinReplicas: minReplicas,
 				MaxReplicas: maxReplicas,
@@ -76,7 +76,7 @@ func syncHPA(gateway *odigosv1.CollectorsGroup, ctx context.Context, c client.Cl
 				ScaleTargetRef: autoscalingv2beta2.CrossVersionObjectReference{
 					APIVersion: "apps/v1",
 					Kind:       "Deployment",
-					Name:       gateway.Name,
+					Name:       consts.OdigosClusterCollectorDeploymentName,
 				},
 				MinReplicas: minReplicas,
 				MaxReplicas: maxReplicas,
@@ -95,6 +95,11 @@ func syncHPA(gateway *odigosv1.CollectorsGroup, ctx context.Context, c client.Cl
 				Behavior: &autoscalingv2beta2.HorizontalPodAutoscalerBehavior{
 					ScaleDown: &autoscalingv2beta2.HPAScalingRules{
 						StabilizationWindowSeconds: stabilizationWindowSeconds,
+						Policies: []autoscalingv2beta2.HPAScalingPolicy{
+							{
+								
+							}
+						},
 					},
 				},
 			},
@@ -110,7 +115,7 @@ func syncHPA(gateway *odigosv1.CollectorsGroup, ctx context.Context, c client.Cl
 				ScaleTargetRef: autoscalingv2.CrossVersionObjectReference{
 					APIVersion: "apps/v1",
 					Kind:       "Deployment",
-					Name:       gateway.Name,
+					Name:       consts.OdigosClusterCollectorDeploymentName,
 				},
 				MinReplicas: minReplicas,
 				MaxReplicas: maxReplicas,
@@ -151,13 +156,13 @@ func syncHPA(gateway *odigosv1.CollectorsGroup, ctx context.Context, c client.Cl
 		return err
 	}
 
-	logger.Info("Successfully applied HPA", "name", gateway.Name, "namespace", gateway.Namespace)
+	logger.Info("Successfully applied HPA", "name", consts.OdigosClusterCollectorDeploymentName, "namespace", gateway.Namespace)
 	return nil
 }
 
 func buildHPACommonFields(gateway *odigosv1.CollectorsGroup) metav1.ObjectMeta {
 	return metav1.ObjectMeta{
-		Name:      gateway.Name,
+		Name:      consts.OdigosClusterCollectorDeploymentName,
 		Namespace: gateway.Namespace,
 	}
 }
