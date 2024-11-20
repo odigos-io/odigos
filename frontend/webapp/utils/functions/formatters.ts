572d4779102988d4e317426d413917d45a076a35
Ben Elferink <ben.elferink@icloud.com>
2024-11-20 17:27:08 +0200

    [GEN-1768]: fix value and label for "bytes" (#1803)
    Co-authored-by: Alon Braymok <138359965+alonkeyval@users.noreply.github.com>
    Co-authored-by: Tamir David <tamir@odigos.io>
    Co-authored-by: Tamir David <tamirdavid@Tamirs-MacBook-Pro.local>
    Co-authored-by: Amir Blum <amirgiraffe@gmail.com>
    Co-authored-by: Ron Federman <73110295+RonFed@users.noreply.github.com>
    Co-authored-by: Mike Dame <mike@odigos.io>
    Co-authored-by: alonbraymok <alonbraymok007@gmail.com>
    Co-authored-by: Daria Dovzhikova <daria@odigos.io>
    Co-authored-by: David <65228999+david336362@users.noreply.github.com>
    Co-authored-by: David <david@odigos.io>
    Co-authored-by: Eden Federman <eden@keyval.dev>

--- frontend/webapp/utils/functions/formatters.ts
+++ frontend/webapp/utils/functions/formatters.ts
@@ -3,11 +3,11 @@ import { ExportedSignals, OVERVIEW_ENTITY_TYPES, WorkloadId } from '@/types';
 export const formatBytes = (bytes?: number) => {
   if (!bytes) return '0 KB/s';
 
-  const sizes = ['Bytes', 'KB/s', 'MB/s', 'GB/s', 'TB/s'];
+  const sizes = ['Bytes/s', 'KB/s', 'MB/s', 'GB/s', 'TB/s'];
   const i = Math.floor(Math.log(bytes) / Math.log(1024));
   const value = bytes / Math.pow(1024, i);
 
-  return `${value.toFixed(1)} ${sizes[i]}`;
+  return `${value.toFixed(i === 0 ? 0 : 1)} ${sizes[i]}`;
 };
 
 export const extractMonitors = (exportedSignals: ExportedSignals) => {
