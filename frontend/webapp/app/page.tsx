33725c57de2fa280b05c21368efbd115000fd2ad
Ben Elferink <ben.elferink@icloud.com>
2024-11-20 21:45:25 +0200

    fix: root page styles

--- frontend/webapp/app/page.tsx
+++ frontend/webapp/app/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import { useNotify, useConfig } from '@/hooks';
 import { FadeLoader } from '@/reuseable-components';
 import { ROUTES, CONFIG, NOTIFICATION } from '@/utils';
+import { CenterThis } from '@/styles';
 
 export default function App() {
   const router = useRouter();
@@ -19,7 +20,6 @@ export default function App() {
       });
     } else if (data) {
       const { installation } = data;
-
       switch (installation) {
         case CONFIG.NEW:
         case CONFIG.APPS_SELECTED:
@@ -31,5 +31,9 @@ export default function App() {
     }
   }, [data]);
 
-  return <FadeLoader />;
+  return (
+    <CenterThis style={{ height: '100%' }}>
+      <FadeLoader style={{ scale: 2 }} />
+    </CenterThis>
+  );
 }
