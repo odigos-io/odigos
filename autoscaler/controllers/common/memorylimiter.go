a92a4651cf5d28029574a947cce0ebc6f3ff26d3
Amir Blum <amirgiraffe@gmail.com>
2024-11-23 08:35:22 +0200

    feat: add memory limiter for node collector to drop data

---
+++ autoscaler/controllers/common/memorylimiter.go
@@ -0,0 +1,17 @@
+package common
+
+import (
+	odigosv1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
+	"github.com/odigos-io/odigos/common/config"
+)
+
+func GetMemoryLimiterConfig(memorySettings odigosv1.CollectorsGroupMemorySettings) config.GenericMap {
+	// check_interval is currently hardcoded to 1s
+	// this seems to be a reasonable value for the memory limiter and what the processor uses in docs.
+	// preforming memory checks is expensive, so we trade off performance with fast reaction time to memory pressure.
+	return config.GenericMap{
+		"check_interval":  "1s",
+		"limit_mib":       memorySettings.MemoryLimiterLimitMiB,
+		"spike_limit_mib": memorySettings.MemoryLimiterSpikeLimitMiB,
+	}
+}
