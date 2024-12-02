ddefb4999c3fb5daa1b6ba7637eb99ba1709a061
Ben Elferink <ben.elferink@icloud.com>
2024-12-02 14:58:04 +0200

    feat: restructure functions dir

---
+++ frontend/webapp/utils/functions/icons/index.ts
@@ -0,0 +1,5 @@
+export * from './get-action-icon';
+export * from './get-entity-icon';
+export * from './get-programming-language-icon';
+export * from './get-rule-icon';
+export * from './get-status-icon';
