2d7d44dcd22810a37f3ad6e8dbba61ab5c3aef6f
Ben Elferink <ben.elferink@icloud.com>
2024-12-05 20:25:41 +0200

    fix: attempt to fix cypress comms w/ sources

--- frontend/webapp/cypress.config.ts
+++ frontend/webapp/cypress.config.ts
@@ -1,9 +1,6 @@
 import Cypress from 'cypress';
 
-// PORT=3001 uses the "production" build (served with Go)
-// PORT=3000 uses the "development" build (served with Next.js)
-// We have to use the "production" build when pushing to GitHub, feel free to change this for local tests...
-const PORT = 3001;
+const PORT = 3000;
 const BASE_URL = `http://localhost:${PORT}`;
 
 const config: Cypress.ConfigOptions = {
