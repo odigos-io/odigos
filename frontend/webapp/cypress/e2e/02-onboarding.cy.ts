9926caae84102df5d3534875ecc7335316ded2ca
Ben Elferink <ben.elferink@icloud.com>
2024-12-08 16:45:34 +0200

    fix: code review from GEN-1927

--- frontend/webapp/cypress/e2e/02-onboarding.cy.ts
+++ frontend/webapp/cypress/e2e/02-onboarding.cy.ts
@@ -1,19 +1,19 @@
 import { ROUTES } from '../../utils/constants/routes';
 
 describe('Onboarding', () => {
-  it('Should contain at least a "default" namespace', () => {
+  beforeEach(() => {
     cy.intercept('/graphql').as('gql');
-    cy.visit(ROUTES.CHOOSE_SOURCES);
+  });
 
+  it('Should contain at least a "default" namespace', () => {
+    cy.visit(ROUTES.CHOOSE_SOURCES);
     cy.wait('@gql').then(() => {
       expect('#namespace-default').to.exist;
     });
   });
 
   it('Should contain at least a "Jaeger" destination', () => {
-    cy.intercept('/graphql').as('gql');
     cy.visit(ROUTES.CHOOSE_DESTINATION);
-
     cy.wait('@gql').then(() => {
       cy.contains('button', 'ADD DESTINATION').click();
       expect('#destination-jaeger').to.exist;
@@ -21,9 +21,7 @@ describe('Onboarding', () => {
   });
 
   it('Should autocomplete the "Jaeger" destination', () => {
-    cy.intercept('/graphql').as('gql');
     cy.visit(ROUTES.CHOOSE_DESTINATION);
-
     cy.wait('@gql').then(() => {
       cy.contains('button', 'ADD DESTINATION').click();
       cy.get('#destination-jaeger').click();
@@ -33,10 +31,8 @@ describe('Onboarding', () => {
 
   it('Should allow the user to pass every step, and end-up on the "Overview" page.', () => {
     cy.visit(ROUTES.CHOOSE_SOURCES);
-
     cy.contains('button', 'NEXT').click();
     cy.location('pathname').should('eq', ROUTES.CHOOSE_DESTINATION);
-
     cy.contains('button', 'DONE').click();
     cy.location('pathname').should('eq', ROUTES.OVERVIEW);
   });
