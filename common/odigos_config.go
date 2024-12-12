a833ec12c7cb0a1bb957fc6b8216a2a629684a2d
Amir Blum <amirgiraffe@gmail.com>
2024-12-12 15:54:48 +0200

    feat: add node collector memory settings

--- common/odigos_config.go
+++ common/odigos_config.go
@@ -6,6 +6,31 @@ type CollectorNodeConfiguration struct {
 	// The port to use for exposing the collector's own metrics as a prometheus endpoint.
 	// This can be used to resolve conflicting ports when a collector is using the host network.
 	CollectorOwnMetricsPort int32 `json:"collectorOwnMetricsPort,omitempty"`
+
+	// RequestMemoryMiB is the memory request for the cluster gateway collector deployment.
+	// it will be embedded in the deployment as a resource request of the form "memory: <value>Mi"
+	// default value is 500Mi
+	RequestMemoryMiB int `json:"requestMemoryMiB,omitempty"`
+
+	// LimitMemoryMiB is the memory limit for the cluster gateway collector deployment.
+	// it will be embedded in the deployment as a resource limit of the form "memory: <value>Mi"
+	// default value is 1.25 the memory request.
+	LimitMemoryMiB int `json:"limitMemoryMiB,omitempty"`
+
+	// this parameter sets the "limit_mib" parameter in the memory limiter configuration for the collector gateway.
+	// it is the hard limit after which a force garbage collection will be performed.
+	// if not set, it will be 50Mi below the memory request.
+	MemoryLimiterLimitMiB int `json:"memoryLimiterLimitMiB,omitempty"`
+
+	// this parameter sets the "spike_limit_mib" parameter in the memory limiter configuration for the collector gateway.
+	// note that this is not the processor soft limit, but the diff in Mib between the hard limit and the soft limit.
+	// if not set, this will be set to 20% of the hard limit (so the soft limit will be 80% of the hard limit).
+	MemoryLimiterSpikeLimitMiB int `json:"memoryLimiterSpikeLimitMiB,omitempty"`
+
+	// the GOMEMLIMIT environment variable value for the collector gateway deployment.
+	// this is when go runtime will start garbage collection.
+	// if not specified, it will be set to 80% of the hard limit of the memory limiter.
+	GoMemLimitMib int `json:"goMemLimitMiB,omitempty"`
 }
 
 type CollectorGatewayConfiguration struct {
