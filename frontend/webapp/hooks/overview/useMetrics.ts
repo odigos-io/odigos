ac8a61a6d270160dc51d2f49130299f78ccc6d49
Ben Elferink <ben.elferink@icloud.com>
2024-12-15 09:50:41 +0200

    fix: change polling method for metrics

--- frontend/webapp/hooks/overview/useMetrics.ts
+++ frontend/webapp/hooks/overview/useMetrics.ts
@@ -1,15 +1,11 @@
-import { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_METRICS } from '@/graphql/mutations/metrics';
 import type { OverviewMetricsResponse } from '@/types';
 
-export function useMetrics() {
-  const { data, refetch } = useQuery<OverviewMetricsResponse>(GET_METRICS);
-
-  useEffect(() => {
-    const interval = setInterval(async () => await refetch(), 3000);
-    return () => clearInterval(interval);
-  }, [refetch]);
+export const useMetrics = () => {
+  const { data } = useQuery<OverviewMetricsResponse>(GET_METRICS, {
+    pollInterval: 3000,
+  });
 
   return { metrics: data };
-}
+};
