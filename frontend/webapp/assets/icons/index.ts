0179ef95b2c5844956f1c59f4be61eb1f9037450
Ben Elferink <ben.elferink@icloud.com>
2024-12-16 20:37:25 +0200

    feat: move common icons to notifications directory for better organization

--- frontend/webapp/assets/icons/index.ts
+++ frontend/webapp/assets/icons/index.ts
@@ -1,8 +1,9 @@
 export * from './actions';
 export * from './brand';
-export * from './common';
+// export * from './common';
 export * from './compute-platform';
 export * from './monitors';
+export * from './notifications';
 export * from './overview';
 export * from './rules';
 export * from './social';
---
+++ frontend/webapp/assets/icons/notifications/index.ts
@@ -0,0 +1,4 @@
+export * from './error-round-icon';
+export * from './error-triangle-icon';
+export * from './success-round-icon';
+export * from './warning-triangle-icon';
