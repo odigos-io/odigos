317828c192dc861c51bb15e3b77c304abf944bcf
Ben Elferink <ben.elferink@icloud.com>
2024-12-16 17:00:06 +0200

    feat: add monitor icons and refactor overview components for improved display

--- frontend/webapp/assets/icons/index.ts
+++ frontend/webapp/assets/icons/index.ts
@@ -2,7 +2,7 @@ export * from './actions';
 export * from './brand';
 // export * from './common';
 export * from './compute-platform';
-// export * from './monitors';
+export * from './monitors';
 // export * from './notification';
 export * from './overview';
 export * from './rules';
