ab014018cde1a24247eecbbd23c1b961a76c4b32
Eden Federman <eden@keyval.dev>
2024-12-23 12:22:59 +0200

    Add .NET cases to workflow lifecycle

--- tests/e2e/workload-lifecycle/services/dotnet-musl-server/Program.cs
+++ tests/e2e/workload-lifecycle/services/dotnet-musl-server/Program.cs
@@ -1,6 +1,50 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run("http://0.0.0.0:8080");
+namespace LegacyWebHostTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                // Configure Kestrel, etc.
+                .UseKestrel()
+
+                // Logging
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                    // Typically with 2.x hosting, AddConsole()
+                    // sets up the console logger, including logger for WebHost
+                })
+
+                // Minimal pipeline
+                .Configure(app =>
+                {
+                    app.Run(async context =>
+                    {
+                        var logger = context
+                            .RequestServices
+                            .GetService(typeof(ILogger<Program>))
+                            as ILogger<Program>;
+
+                        logger?.LogInformation("Handling request on path {path}", context.Request.Path);
+
+                        await context.Response.WriteAsync("Hello from a legacy webhost on .NET 6\n");
+                    });
+                })
+
+                // Build the host
+                .Build();
+
+            // Run
+            host.Run("http://0.0.0.0:8080");
+        }
+    }
+}
