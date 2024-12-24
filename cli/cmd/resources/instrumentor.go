3717f20d323afbdbc4201d54e8080995eb2df9c9
tamirdavid1 <tamir@odigos.io>
2024-12-24 09:16:18 +0200

    chore: add permissions to instrumentor to update instrumencationconfigs/status

--- cli/cmd/resources/instrumentor.go
+++ cli/cmd/resources/instrumentor.go
@@ -191,6 +191,11 @@ func NewInstrumentorClusterRole() *rbacv1.ClusterRole {
 				Resources: []string{"instrumentationconfigs"},
 				Verbs:     []string{"create", "delete", "get", "list", "patch", "update", "watch"},
 			},
+			{
+				APIGroups: []string{"odigos.io"},
+				Resources: []string{"instrumentationconfigs/status"},
+				Verbs:     []string{"update"},
+			},
 		},
 	}
 }
