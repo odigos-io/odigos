f59a0ef61ad5d45c04dd45e1564edd5c63b07a8a
Ben Elferink <ben.elferink@icloud.com>
2024-12-26 16:28:54 +0200

    refactor: simplify Lumigo configuration by removing endpoint validation and updating documentation

--- common/config/lumigo.go
+++ common/config/lumigo.go
@@ -1,20 +1,7 @@
 package config
 
 import (
-	"errors"
-	"strings"
-
 	"github.com/odigos-io/odigos/common"
-)
-
-const (
-	LumigoEndpoint = "LUMIGO_ENDPOINT"
-	LumigoToken    = "LUMIGO_TOKEN"
-)
-
-var (
-	ErrorLumigoEndpointMissing = errors.New("Lumigo is missing a required field (\"LUMIGO_ENDPOINT\"), Lumigo will not be configured")
-	ErrorLumigoTokenMissing    = errors.New("Lumigo is missing a required field (\"LUMIGO_TOKEN\"), Lumigo will not be configured")
 )
 
 type Lumigo struct{}
@@ -23,48 +10,36 @@ func (j *Lumigo) DestType() common.DestinationType {
 	return common.LumigoDestinationType
 }
 
-func (j *Lumigo) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) error {
-	config := dest.GetConfig()
+func (j *Lumigo) ModifyConfig(dest ExporterConfigurer, cfg *Config) error {
 	uniqueUri := "lumigo-" + dest.GetID()
-
-	url, exists := config[LumigoEndpoint]
-	if !exists {
-		return ErrorLumigoEndpointMissing
-	}
-	if !strings.HasPrefix(url, "https://") {
-		return errors.New("Lumigo Endpoint (\"LUMIGO_ENDPOINT\") malformed, HTTPS prefix is required, Lumigo will not be configured")
-	}
-	if strings.HasSuffix(url, "/") {
-		return errors.New("Lumigo Endpoint (\"LUMIGO_ENDPOINT\") malformed, forward-slash \"/\" suffix is forbidden, Lumigo will not be configured")
-	}
 
 	exporterName := "otlphttp/" + uniqueUri
 	exporterConfig := GenericMap{
-		"endpoint": url,
+		"endpoint": "https://ga-otlp.lumigo-tracer-edge.golumigo.com",
 		"headers": GenericMap{
 			"Authorization": "LumigoToken ${LUMIGO_TOKEN}",
 		},
 	}
 
-	currentConfig.Exporters[exporterName] = exporterConfig
+	cfg.Exporters[exporterName] = exporterConfig
 
 	if isTracingEnabled(dest) {
 		pipeName := "traces/" + uniqueUri
-		currentConfig.Service.Pipelines[pipeName] = Pipeline{
+		cfg.Service.Pipelines[pipeName] = Pipeline{
 			Exporters: []string{exporterName},
 		}
 	}
 
 	if isMetricsEnabled(dest) {
 		pipeName := "metrics/" + uniqueUri
-		currentConfig.Service.Pipelines[pipeName] = Pipeline{
+		cfg.Service.Pipelines[pipeName] = Pipeline{
 			Exporters: []string{exporterName},
 		}
 	}
 
 	if isLoggingEnabled(dest) {
 		pipeName := "logs/" + uniqueUri
-		currentConfig.Service.Pipelines[pipeName] = Pipeline{
+		cfg.Service.Pipelines[pipeName] = Pipeline{
 			Exporters: []string{exporterName},
 		}
 	}
