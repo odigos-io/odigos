899949e6d1398761a0138c3f1e61e2d223079861
Ben Elferink <ben.elferink@icloud.com>
2024-12-27 17:17:23 +0200

    add HIGHLIGHT_ENDPOINT configuration and update documentation

--- common/config/highlight.go
+++ common/config/highlight.go
@@ -1,7 +1,17 @@
 package config
 
 import (
+	"errors"
+
 	"github.com/odigos-io/odigos/common"
+)
+
+const (
+	HighlightEndpoint = "HIGHLIGHT_ENDPOINT"
+)
+
+var (
+	ErrorHighlightEndpointMissing = errors.New("Highlight is missing a required field (\"HIGHLIGHT_ENDPOINT\"), Highlight will not be configured")
 )
 
 type Highlight struct{}
@@ -11,11 +21,22 @@ func (j *Highlight) DestType() common.DestinationType {
 }
 
 func (j *Highlight) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) error {
+	config := dest.GetConfig()
 	uniqueUri := "highlight-" + dest.GetID()
+
+	url, exists := config[HighlightEndpoint]
+	if !exists {
+		return ErrorHighlightEndpointMissing
+	}
+
+	endpoint, err := parseOtlpHttpEndpoint(url)
+	if err != nil {
+		return err
+	}
 
 	exporterName := "otlp/" + uniqueUri
 	currentConfig.Exporters[exporterName] = GenericMap{
-		"endpoint": "https://otel.highlight.io:4318",
+		"endpoint": endpoint,
 		"headers": GenericMap{
 			"x-highlight-project": "${HIGHLIGHT_PROJECT_ID}",
 		},
