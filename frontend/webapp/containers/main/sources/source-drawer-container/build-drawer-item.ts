d2fa4ad9e841605a1126336534b4d1f8582d8090
Ben Elferink <ben.elferink@icloud.com>
2024-12-29 09:35:06 +0200

    fix: build error

--- frontend/webapp/containers/main/sources/source-drawer-container/build-drawer-item.ts
+++ frontend/webapp/containers/main/sources/source-drawer-container/build-drawer-item.ts
@@ -3,14 +3,13 @@ import type { K8sActualSource, WorkloadId } from '@/types';
 const buildDrawerItem = (id: WorkloadId, formData: { reportedName: string }, drawerItem: K8sActualSource): K8sActualSource => {
   const { namespace, name, kind } = id;
   const { reportedName } = formData;
-  const { numberOfInstances, serviceName, conditions, containers, selected } = drawerItem;
+  const { numberOfInstances, conditions, containers, selected } = drawerItem;
 
   return {
     namespace,
     name,
     kind,
     numberOfInstances,
-    serviceName,
     reportedName,
     conditions,
     containers,
