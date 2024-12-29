4eb5de5dec611c49cd668d7f81e441fad25cb839
Ben Elferink <ben.elferink@icloud.com>
2024-12-29 14:47:33 +0200

    Remove HIGHLIGHT_ENDPOINT configuration and handle endpoint internally in Highlight backend

--- common/config/highlight.go
+++ common/config/highlight.go
@@ -1,17 +1,7 @@
 package config
 
 import (
-	"errors"
-
 	"github.com/odigos-io/odigos/common"
-)
-
-const (
-	HighlightEndpoint = "HIGHLIGHT_ENDPOINT"
-)
-
-var (
-	ErrorHighlightEndpointMissing = errors.New("Highlight is missing a required field (\"HIGHLIGHT_ENDPOINT\"), Highlight will not be configured")
 )
 
 type Highlight struct{}
@@ -21,22 +11,11 @@ func (j *Highlight) DestType() common.DestinationType {
 }
 
 func (j *Highlight) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) error {
-	config := dest.GetConfig()
 	uniqueUri := "highlight-" + dest.GetID()
-
-	url, exists := config[HighlightEndpoint]
-	if !exists {
-		return ErrorHighlightEndpointMissing
-	}
-
-	endpoint, err := parseOtlpHttpEndpoint(url)
-	if err != nil {
-		return err
-	}
 
 	exporterName := "otlp/" + uniqueUri
 	currentConfig.Exporters[exporterName] = GenericMap{
-		"endpoint": endpoint,
+		"endpoint": "https://otel.highlight.io:4318",
 		"headers": GenericMap{
 			"x-highlight-project": "${HIGHLIGHT_PROJECT_ID}",
 		},
