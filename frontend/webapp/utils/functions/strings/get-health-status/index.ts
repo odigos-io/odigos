d12c5fcb9ef81be8e94b6cb7978a48026660f240
Ben Elferink <ben.elferink@icloud.com>
2024-12-29 18:49:25 +0200

    fix: improve health status determination logic

--- frontend/webapp/utils/functions/strings/get-health-status/index.ts
+++ frontend/webapp/utils/functions/strings/get-health-status/index.ts
@@ -3,7 +3,7 @@ import { STATUSES, type ActualDestination, type K8sActualSource } from '@/types'
 
 export const getHealthStatus = (item: K8sActualSource | ActualDestination) => {
   const conditions = item?.conditions || [];
-  const isUnhealthy = !conditions.length || !!conditions.find(({ status }) => status === BACKEND_BOOLEAN.FALSE);
+  const isUnhealthy = !!conditions.find(({ status }) => status === BACKEND_BOOLEAN.FALSE);
 
   return isUnhealthy ? STATUSES.UNHEALTHY : STATUSES.HEALTHY;
 };
