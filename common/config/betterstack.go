32e62c1c01e2b947a3dd08ed8f5e3fa079dca03d
Ben Elferink <ben.elferink@icloud.com>
2024-12-29 11:28:48 +0200

    refactor: remove endpoint configuration from Better Stack and update documentation

--- common/config/betterstack.go
+++ common/config/betterstack.go
@@ -1,19 +1,7 @@
 package config
 
 import (
-	"errors"
-
 	"github.com/odigos-io/odigos/common"
-)
-
-const (
-	BetterStackMetricsEndpoint = "BETTERSTACK_METRICS_ENDPOINT"
-	BetterStackLogsEndpoint    = "BETTERSTACK_LOGS_ENDPOINT"
-)
-
-var (
-	ErrorBetterStackMetricsEndpointMissing = errors.New("BetterStack is missing a required field (\"BETTERSTACK_METRICS_ENDPOINT\"), BetterStack will not be configured")
-	ErrorBetterStackLogsEndpointMissing    = errors.New("BetterStack is missing a required field (\"BETTERSTACK_LOGS_ENDPOINT\"), BetterStack will not be configured")
 )
 
 type BetterStack struct{}
@@ -23,7 +11,6 @@ func (j *BetterStack) DestType() common.DestinationType {
 }
 
 func (j *BetterStack) ModifyConfig(dest ExporterConfigurer, cfg *Config) error {
-	config := dest.GetConfig()
 	uniqueUri := "betterstack-" + dest.GetID()
 
 	processorName := "attributes/betterstack"
@@ -38,19 +25,9 @@ func (j *BetterStack) ModifyConfig(dest ExporterConfigurer, cfg *Config) error {
 	}
 
 	if isMetricsEnabled(dest) {
-		url, exists := config[BetterStackMetricsEndpoint]
-		if !exists {
-			return ErrorBetterStackMetricsEndpointMissing
-		}
-
-		endpoint, err := parseOtlpHttpEndpoint(url)
-		if err != nil {
-			return err
-		}
-
 		exporterName := "prometheusremotewrite/" + uniqueUri
 		cfg.Exporters[exporterName] = GenericMap{
-			"endpoint": endpoint,
+			"endpoint": "https://in-otel.logs.betterstack.com/metrics",
 		}
 
 		pipeName := "metrics/" + uniqueUri
@@ -61,19 +38,9 @@ func (j *BetterStack) ModifyConfig(dest ExporterConfigurer, cfg *Config) error {
 	}
 
 	if isLoggingEnabled(dest) {
-		url, exists := config[BetterStackLogsEndpoint]
-		if !exists {
-			return ErrorBetterStackLogsEndpointMissing
-		}
-
-		endpoint, err := parseOtlpHttpEndpoint(url)
-		if err != nil {
-			return err
-		}
-
 		exporterName := "otlp/" + uniqueUri
 		cfg.Exporters[exporterName] = GenericMap{
-			"endpoint": endpoint,
+			"endpoint": "https://in-otel.logs.betterstack.com:443",
 		}
 
 		pipeName := "logs/" + uniqueUri
