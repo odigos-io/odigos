7bf451557c02f88fac86513ac01e460dc673dcf5
Avihu Hanya <avihuhenya@gmail.com>
2025-01-01 10:27:46 +0200

    feat: add .env usage in pro command for debugging

--- cli/cmd/pro.go
+++ cli/cmd/pro.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/odigos-io/odigos/cli/cmd/resources"
 	cmdcontext "github.com/odigos-io/odigos/cli/pkg/cmd_context"
 	"github.com/odigos-io/odigos/cli/pkg/kube"
@@ -14,13 +15,22 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-
 var proCmd = &cobra.Command{
 	Use:   "pro",
 	Short: "manage odigos pro",
 	Long:  `The pro command provides various operations and functionalities specifically designed for enterprise users. Use this command to access advanced features and manage your pro account.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := cmd.Context()
+		var onPremToken string
+		if err := godotenv.Load(); err != nil {
+			fmt.Println("\033[31mERROR\033[0m Failed to load .env file")
+		}
+		if os.Getenv("DEBUG") == "true" {
+			fmt.Println("DEBUG mode is enabled. Loading environment variables from .env file")
+			onPremToken = os.Getenv("ONPREM_TOKEN")
+		} else {
+			onPremToken = cmd.Flag("onprem-token").Value.String()
+		}
 		client := cmdcontext.KubeClientFromContextOrExit(ctx)
 		ns, err := resources.GetOdigosNamespace(client, ctx)
 		if resources.IsErrNoOdigosNamespaceFound(err) {
@@ -30,19 +40,13 @@ var proCmd = &cobra.Command{
 			fmt.Printf("\033[31mERROR\033[0m Failed to check if Odigos is already installed: %s\n", err)
 			os.Exit(1)
 		}
-
-		onPremToken := cmd.Flag("onprem-token").Value.String()
-
-		tokenType := "onprem" 
-		tokenValue := onPremToken
-
-		err = updateOdigosToken(ctx, client, ns, tokenType, tokenValue)
+		err = updateOdigosToken(ctx, client, ns, onPremToken)
 		if err != nil {
 			fmt.Println("\033[31mERROR\033[0m Failed to update token:")
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		
+
 		fmt.Println()
 		fmt.Printf("\u001B[32mSUCCESS:\u001B[0m Token updated successfully in namespace %s\n", ns)
 		fmt.Println()
@@ -52,20 +56,20 @@ var proCmd = &cobra.Command{
 	},
 }
 
-func updateOdigosToken(ctx context.Context, client *kube.Client, namespace string, tokenType, tokenValue string) error {
+func updateOdigosToken(ctx context.Context, client *kube.Client, namespace string, onPremToken string) error {
 	secret, err := client.CoreV1().Secrets(namespace).Get(ctx, consts.OdigosProSecretName, metav1.GetOptions{})
 	if err != nil {
 		if apierrors.IsNotFound(err) {
-			return err;
+			return err
 		}
 		return fmt.Errorf("Tokens are not available in the open-source version of Odigos. Please use the on-premises version.")
 	}
-	secret.Data[consts.OdigosOnpremTokenSecretKey] = []byte(tokenValue)
+	secret.Data[consts.OdigosOnpremTokenSecretKey] = []byte(onPremToken)
 
 	_, err = client.CoreV1().Secrets(namespace).Update(ctx, secret, metav1.UpdateOptions{})
 	if err != nil {
 		if apierrors.IsNotFound(err) {
-			return err;
+			return err
 		}
 		return err
 	}
