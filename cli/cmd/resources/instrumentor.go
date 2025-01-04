892f2f08c8fa01fb3260852b77874f508bae742e
Ron Federman <ron@keyval.dev>
2025-01-04 11:47:58 +0200

    merge instrumentationconfigs/status cluster roles

--- cli/cmd/resources/instrumentor.go
+++ cli/cmd/resources/instrumentor.go
@@ -191,11 +191,6 @@ func NewInstrumentorClusterRole() *rbacv1.ClusterRole {
 				Resources: []string{"instrumentationconfigs"},
 				Verbs:     []string{"create", "delete", "get", "list", "patch", "update", "watch"},
 			},
-			{
-				APIGroups: []string{"odigos.io"},
-				Resources: []string{"instrumentationconfigs/status"},
-				Verbs:     []string{"update"},
-			},
 		},
 	}
 }
