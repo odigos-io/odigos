28b7cbbebf70f287068048c16b5ead872adffaff
Ben Elferink <ben.elferink@icloud.com>
2025-01-05 11:34:49 +0200

    feat: move restructureForPrettyMode function to useDescribeOdigos hook for better data handling

--- frontend/webapp/components/overview/all-drawers/describe-drawer.tsx
+++ frontend/webapp/components/overview/all-drawers/describe-drawer.tsx
@@ -16,37 +16,8 @@ const DataContainer = styled.div`
   gap: 12px;
 `;
 
-// This function is used to restructure the data, so that it reflects the output given by "odigos describe" command in the CLI.
-// This is not really needed, but it's a nice-to-have feature to make the data more readable.
-const restructureForPrettyMode = (code?: DescribeOdigos['describeOdigos']) => {
-  if (!code) return {};
-
-  const payload = {
-    [code.odigosVersion.name]: code.odigosVersion.value,
-    'Number Of Sources': code.numberOfSources,
-    'Number Of Destinations': code.numberOfDestinations,
-    'Cluster Collector': {},
-    'Node Collector': {},
-  };
-
-  const mapObjects = (obj: any, objectName: string) => {
-    if (typeof obj === 'object' && !!obj?.name) {
-      let key = obj.name;
-      let val = obj.value;
-      if (obj.explain) key += `#tooltip=${obj.explain}`;
-      if (!payload[objectName]) payload[objectName] = {};
-      payload[objectName][key] = val;
-    }
-  };
-
-  Object.values(code.clusterCollector).forEach((val) => mapObjects(val, 'Cluster Collector'));
-  Object.values(code.nodeCollector).forEach((val) => mapObjects(val, 'Node Collector'));
-
-  return payload;
-};
-
 export const DescribeDrawer: React.FC<Props> = () => {
-  const { data: describe } = useDescribeOdigos();
+  const { data: describe, restructureForPrettyMode } = useDescribeOdigos();
   const [isCodeMode, setIsCodeMode] = useState(false);
 
   return (
--- frontend/webapp/hooks/describe/useDescribeOdigos.ts
+++ frontend/webapp/hooks/describe/useDescribeOdigos.ts
@@ -7,9 +7,39 @@ export const useDescribeOdigos = () => {
     pollInterval: 5000,
   });
 
+  // This function is used to restructure the data, so that it reflects the output given by "odigos describe" command in the CLI.
+  // This is not really needed, but it's a nice-to-have feature to make the data more readable.
+  const restructureForPrettyMode = (code?: DescribeOdigos['describeOdigos']) => {
+    if (!code) return {};
+
+    const payload = {
+      [code.odigosVersion.name]: code.odigosVersion.value,
+      'Number Of Sources': code.numberOfSources,
+      'Number Of Destinations': code.numberOfDestinations,
+      'Cluster Collector': {},
+      'Node Collector': {},
+    };
+
+    const mapObjects = (obj: any, objectName: string) => {
+      if (typeof obj === 'object' && !!obj?.name) {
+        let key = obj.name;
+        let val = obj.value;
+        if (obj.explain) key += `#tooltip=${obj.explain}`;
+        if (!payload[objectName]) payload[objectName] = {};
+        payload[objectName][key] = val;
+      }
+    };
+
+    Object.values(code.clusterCollector).forEach((val) => mapObjects(val, 'Cluster Collector'));
+    Object.values(code.nodeCollector).forEach((val) => mapObjects(val, 'Node Collector'));
+
+    return payload;
+  };
+
   return {
-    data: data?.describeOdigos,
     loading,
     error,
+    data: data?.describeOdigos,
+    restructureForPrettyMode,
   };
 };
