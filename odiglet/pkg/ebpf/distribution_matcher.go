e3ddf8fc2e5c7475458f61cf374e6a888d13d801
Ron Federman <73110295+RonFed@users.noreply.github.com>
2025-01-08 22:00:54 +0200

    Verify the language of a process before instrumenting (#2164)
    Since our language detection is done in the container level but
    instrumentation is done in the process level it is possible that we'll
    try to instrument a process as being in certain language even though it
    is written in a different one - this can happen when multiple processes
    exist in the same container.
    To handle this, the distribution matcher is updated to call the language
    inspector of the language written in the device - and verify the
    language match.
    
    In addition, the manager is updated to call the reporter `OnInit` method
    regardless of an init error. Also tracking the PID in case of an init
    error - this allows the reporter to clean the instrumentation instance
    once the PID is exiting.

--- odiglet/pkg/ebpf/distribution_matcher.go
+++ odiglet/pkg/ebpf/distribution_matcher.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/odigos-io/odigos/instrumentation"
 	odgiosK8s "github.com/odigos-io/odigos/k8sutils/pkg/container"
+	"github.com/odigos-io/odigos/procdiscovery/pkg/inspectors"
+	"github.com/odigos-io/odigos/procdiscovery/pkg/process"
 )
 
 type podDeviceDistributionMatcher struct{}
@@ -13,11 +15,22 @@ type podDeviceDistributionMatcher struct{}
 func (dm *podDeviceDistributionMatcher) Distribution(ctx context.Context, e K8sProcessDetails) (instrumentation.OtelDistribution, error) {
 	// get the language and sdk for this process event
 	// based on the pod spec and the container name from the process event
-	// TODO: We should have all the required information in the process event
-	// to determine the language - hence in the future we can improve this
 	lang, sdk, err := odgiosK8s.LanguageSdkFromPodContainer(e.pod, e.containerName)
 	if err != nil {
 		return instrumentation.OtelDistribution{}, fmt.Errorf("failed to get language and sdk: %w", err)
 	}
-	return instrumentation.OtelDistribution{Language: lang, OtelSdk: sdk}, nil
+	// verify the language of the process event
+	if ok := inspectors.VerifyLanguage(process.Details{
+		ProcessID: e.procEvent.PID,
+		ExeName:   e.procEvent.ExecDetails.ExeName,
+		CmdLine:   e.procEvent.ExecDetails.CmdLine,
+		Environments: process.ProcessEnvs{
+			DetailedEnvs:  e.procEvent.ExecDetails.Environments,
+		},
+	}, lang); ok {
+		return instrumentation.OtelDistribution{Language: lang, OtelSdk: sdk}, nil
+	}
+
+	return instrumentation.OtelDistribution{},
+	 	fmt.Errorf("process language does not match the detected language (%s) for container: %s. exe name: %s", lang, e.containerName, e.procEvent.ExecDetails.ExeName)
 }
