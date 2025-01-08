a9fd8bfa9b159614c3c4d5222551984f758460f3
alonkeyval <alonbraymok007@gmail.com>
2025-01-08 11:03:49 +0200

    chore: wip

--- common/config/axiom.go
+++ common/config/axiom.go
@@ -10,11 +10,14 @@ const (
 
 type Axiom struct{}
 
+// compile time checks
+var _ Configer = (*Axiom)(nil)
+
 func (a *Axiom) DestType() common.DestinationType {
 	return common.AxiomDestinationType
 }
 
-func (a *Axiom) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) error {
+func (a *Axiom) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) ([]string, error) {
 	dataset, exists := dest.GetConfig()[axiomDatasetKey]
 	if !exists {
 		dataset = "default"
@@ -31,11 +34,14 @@ func (a *Axiom) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) err
 		},
 	}
 
+	pipelineNames := []string{}
+
 	if isTracingEnabled(dest) {
 		tracesPipelineName := "traces/axiom-" + dest.GetID()
 		currentConfig.Service.Pipelines[tracesPipelineName] = Pipeline{
 			Exporters: []string{axiomExporterName},
 		}
+		pipelineNames = append(pipelineNames, tracesPipelineName)
 	}
 
 	if isLoggingEnabled(dest) {
@@ -43,7 +49,8 @@ func (a *Axiom) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) err
 		currentConfig.Service.Pipelines[logsPipelineName] = Pipeline{
 			Exporters: []string{axiomExporterName},
 		}
+		pipelineNames = append(pipelineNames, logsPipelineName)
 	}
 
-	return nil
+	return pipelineNames, nil
 }
