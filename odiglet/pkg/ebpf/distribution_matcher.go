f22d7c7a2fe1e6f04a6982cb178ec056604b45f8
Ron Federman <ron@keyval.dev>
2025-01-09 10:44:50 +0200

    Rename exe name to exe path

--- odiglet/pkg/ebpf/distribution_matcher.go
+++ odiglet/pkg/ebpf/distribution_matcher.go
@@ -22,15 +22,15 @@ func (dm *podDeviceDistributionMatcher) Distribution(ctx context.Context, e K8sP
 	// verify the language of the process event
 	if ok := inspectors.VerifyLanguage(process.Details{
 		ProcessID: e.procEvent.PID,
-		ExeName:   e.procEvent.ExecDetails.ExePath,
+		ExePath:   e.procEvent.ExecDetails.ExePath,
 		CmdLine:   e.procEvent.ExecDetails.CmdLine,
 		Environments: process.ProcessEnvs{
-			DetailedEnvs:  e.procEvent.ExecDetails.Environments,
+			DetailedEnvs: e.procEvent.ExecDetails.Environments,
 		},
 	}, lang); ok {
 		return instrumentation.OtelDistribution{Language: lang, OtelSdk: sdk}, nil
 	}
 
 	return instrumentation.OtelDistribution{},
-	 	fmt.Errorf("process language does not match the detected language (%s) for container: %s. exe name: %s", lang, e.containerName, e.procEvent.ExecDetails.ExePath)
+		fmt.Errorf("process language does not match the detected language (%s) for container: %s. exe path: %s", lang, e.containerName, e.procEvent.ExecDetails.ExePath)
 }
--- procdiscovery/pkg/inspectors/java/java.go
+++ procdiscovery/pkg/inspectors/java/java.go
@@ -1,9 +1,10 @@
 package java
 
 import (
-	"github.com/hashicorp/go-version"
 	"regexp"
 	"strings"
+
+	"github.com/hashicorp/go-version"
 
 	"github.com/odigos-io/odigos/common"
 	"github.com/odigos-io/odigos/procdiscovery/pkg/process"
@@ -17,7 +18,7 @@ const JavaVersionRegex = `\d+\.\d+\.\d+\+\d+`
 var re = regexp.MustCompile(JavaVersionRegex)
 
 func (j *JavaInspector) Inspect(proc *process.Details) (common.ProgrammingLanguage, bool) {
-	if strings.Contains(proc.ExeName, processName) || strings.Contains(proc.CmdLine, processName) {
+	if strings.Contains(proc.ExePath, processName) || strings.Contains(proc.CmdLine, processName) {
 		return common.JavaProgrammingLanguage, true
 	}
 
