d96d13ca4c396a737aaf353221c9cd63007b7484
Ben Elferink <ben.elferink@icloud.com>
2025-01-11 16:40:09 +0200

    fix: cleanup

--- frontend/webapp/reuseable-components/dropdown/index.tsx
+++ frontend/webapp/reuseable-components/dropdown/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { useOnClickOutside } from '@/hooks';
 import type { DropdownOption } from '@/types';
 import styled, { css } from 'styled-components';
