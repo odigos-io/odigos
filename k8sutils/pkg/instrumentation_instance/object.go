c4193ab0e9ee1efa1294522b68757fd699e57f4f
tamirdavid1 <tamir@odigos.io>
2025-01-13 13:31:42 +0200

    chore: change imports

--- k8sutils/pkg/instrumentation_instance/object.go
+++ k8sutils/pkg/instrumentation_instance/object.go
@@ -3,15 +3,15 @@ package instrumentation_instance
 import (
 	"context"
 
-	odigosv1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/odigos-io/odigos/api/odigos/v1alpha1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func DeleteInstrumentationInstance(ctx context.Context, owner client.Object, containerName string, kubeClient client.Client, pid int) error {
 	instrumentationInstanceName := InstrumentationInstanceName(owner.GetName(), pid)
-	err := kubeClient.Delete(ctx, &odigosv1.InstrumentationInstance{
-		ObjectMeta: metav1.ObjectMeta{
+	err := kubeClient.Delete(ctx, &v1alpha1.InstrumentationInstance{
+		ObjectMeta: v1.ObjectMeta{
 			Name:      instrumentationInstanceName,
 			Namespace: owner.GetNamespace(),
 		},
