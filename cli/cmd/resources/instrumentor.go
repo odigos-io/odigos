3dd09fe197af21a1aa20f83cab9960f8504b8784
tamirdavid1 <tamir@odigos.io>
2025-01-15 13:24:34 +0200

    chore: spread instrumentor across nodes

--- cli/cmd/resources/instrumentor.go
+++ cli/cmd/resources/instrumentor.go
@@ -516,6 +516,23 @@ func NewInstrumentorDeployment(ns string, version string, telemetryEnabled bool,
 					},
 				},
 				Spec: corev1.PodSpec{
+					Affinity: &corev1.Affinity{
+						PodAntiAffinity: &corev1.PodAntiAffinity{
+							PreferredDuringSchedulingIgnoredDuringExecution: []corev1.WeightedPodAffinityTerm{
+								{
+									Weight: 100,
+									PodAffinityTerm: corev1.PodAffinityTerm{
+										LabelSelector: &metav1.LabelSelector{
+											MatchLabels: map[string]string{
+												"app.kubernetes.io/name": InstrumentorAppLabelValue,
+											},
+										},
+										TopologyKey: "kubernetes.io/hostname",
+									},
+								},
+							},
+						},
+					},
 					Containers: []corev1.Container{
 						{
 							Name:  InstrumentorContainerName,
@@ -558,7 +575,6 @@ func NewInstrumentorDeployment(ns string, version string, telemetryEnabled bool,
 									},
 								},
 							},
-
 							Ports: []corev1.ContainerPort{
 								{
 									Name:          "webhook-server",
