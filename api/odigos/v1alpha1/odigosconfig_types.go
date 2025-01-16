dd36d9fa6dd07133293e59a3e4f5bbfbb3107607
Mike Dame <mike@odigos.io>
2025-01-16 15:28:10 -0500

    Generate CRD API docs (#2248)
    This adds a `make docgen` that uses
    [genref](https://github.com/kubernetes-sigs/reference-docs/tree/master/genref)
    to generate API docs based on our CRD types.
    
    This also updates some API type comments that broke the generator by
    appearing to be html
    
    Sample screenshot:
    <img width="1415" alt="Screenshot 2025-01-16 at 1 26 10 PM"
    src="https://github.com/user-attachments/assets/af31625a-d12f-4b8c-a8fe-edf02cd9afca"
    />
    
    ---------
    
    Co-authored-by: Ben Elferink <ben.elferink@icloud.com>

--- api/odigos/v1alpha1/odigosconfig_types.go
+++ api/odigos/v1alpha1/odigosconfig_types.go
@@ -10,7 +10,7 @@ import (
 // Deprecated: Use common.OdigosConfiguration instead
 type CollectorGatewayConfiguration struct {
 	// RequestMemoryMiB is the memory request for the cluster gateway collector deployment.
-	// it will be embedded in the deployment as a resource request of the form "memory: <value>Mi"
+	// it will be embedded in the deployment as a resource request of the form `memory: <value>Mi`
 	// default value is 500Mi
 	RequestMemoryMiB int `json:"requestMemoryMiB,omitempty"`
 
@@ -34,18 +34,18 @@ type CollectorGatewayConfiguration struct {
 //
 // Deprecated: Use common.OdigosConfiguration instead
 type OdigosConfigurationSpec struct {
-	OdigosVersion     string                                          `json:"odigosVersion"`
-	ConfigVersion     int                                             `json:"configVersion"`
-	TelemetryEnabled  bool                                            `json:"telemetryEnabled,omitempty"`
-	OpenshiftEnabled  bool                                            `json:"openshiftEnabled,omitempty"`
-	IgnoredNamespaces []string                                        `json:"ignoredNamespaces,omitempty"`
-	IgnoredContainers []string                                        `json:"ignoredContainers,omitempty"`
-	Psp               bool                                            `json:"psp,omitempty"`
-	ImagePrefix       string                                          `json:"imagePrefix,omitempty"`
-	OdigletImage      string                                          `json:"odigletImage,omitempty"`
-	InstrumentorImage string                                          `json:"instrumentorImage,omitempty"`
-	AutoscalerImage   string                                          `json:"autoscalerImage,omitempty"`
-	CollectorGateway  *CollectorGatewayConfiguration                  `json:"collectorGateway,omitempty"`
+	OdigosVersion     string                         `json:"odigosVersion"`
+	ConfigVersion     int                            `json:"configVersion"`
+	TelemetryEnabled  bool                           `json:"telemetryEnabled,omitempty"`
+	OpenshiftEnabled  bool                           `json:"openshiftEnabled,omitempty"`
+	IgnoredNamespaces []string                       `json:"ignoredNamespaces,omitempty"`
+	IgnoredContainers []string                       `json:"ignoredContainers,omitempty"`
+	Psp               bool                           `json:"psp,omitempty"`
+	ImagePrefix       string                         `json:"imagePrefix,omitempty"`
+	OdigletImage      string                         `json:"odigletImage,omitempty"`
+	InstrumentorImage string                         `json:"instrumentorImage,omitempty"`
+	AutoscalerImage   string                         `json:"autoscalerImage,omitempty"`
+	CollectorGateway  *CollectorGatewayConfiguration `json:"collectorGateway,omitempty"`
 
 	// this is internal currently, and is not exposed on the CLI / helm
 	// used for odigos enterprise
