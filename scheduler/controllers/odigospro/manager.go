f61f6f437b3a04f91b2f509adfcf7c4398e3b5d2
Avihu Hanya <avihuhenya@gmail.com>
2025-01-20 09:58:59 +0200

    chore: add controller for case that the secret deleted ahen the main controller was down

--- scheduler/controllers/odigospro/manager.go
+++ scheduler/controllers/odigospro/manager.go
@@ -4,6 +4,7 @@ import (
 	odigospredicates "github.com/odigos-io/odigos/k8sutils/pkg/predicate"
 	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
 func SetupWithManager(mgr ctrl.Manager) error {
@@ -12,6 +13,19 @@ func SetupWithManager(mgr ctrl.Manager) error {
 		For(&corev1.Secret{}).
 		Named("odigospro-odigospro").
 		WithEventFilter(&odigospredicates.OdigosProSecretPredicate).
+		Complete(&odigossecretController{
+			Client: mgr.GetClient(),
+		})
+	if err != nil {
+		return err
+	}
+
+	// it is possbile that the secret was deleted when the controller was down.
+	// we want to sync the odigos deployment config map with the secret on startup to reconcile any deleted pro info.
+	err = ctrl.NewControllerManagedBy(mgr).
+		For(&corev1.ConfigMap{}).
+		Named("odigospro-odigosdeployment").
+		WithEventFilter(predicate.And(&odigospredicates.OdigosDeploymentConfigMapPredicate, &odigospredicates.CreationPredicate{})).
 		Complete(&odigossecretController{
 			Client: mgr.GetClient(),
 		})
