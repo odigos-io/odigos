7e775206658ce163a02851ffd6067468ed533dbc
Ben Elferink <ben.elferink@icloud.com>
2025-01-26 13:48:02 +0200

    fix(): config should not use istrumentation labels

--- frontend/services/config.go
+++ frontend/services/config.go
@@ -2,10 +2,8 @@ package services
 
 import (
 	"context"
-	"fmt"
 	"log"
 
-	"github.com/odigos-io/odigos/common/consts"
 	"github.com/odigos-io/odigos/frontend/kube"
 	"github.com/odigos-io/odigos/k8sutils/pkg/env"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -23,84 +21,50 @@ type GetConfigResponse struct {
 	Installation InstallationStatus `json:"installation"`
 }
 
-func GetConfig(c context.Context) GetConfigResponse {
+func GetConfig(ctx context.Context) GetConfigResponse {
 	var response GetConfigResponse
-	if !isSomethingLabeled(c) {
+
+	if !isSourceCreated(ctx) {
 		response.Installation = NewInstallation
-	} else if !isDestinationChosen(c) {
+	} else if !isDestinationConnected(ctx) {
 		response.Installation = AppsSelected
 	} else {
 		response.Installation = Finished
 	}
-	return response
 
+	return response
 }
 
-func isDestinationChosen(ctx context.Context) bool {
-	odigosNamespace := env.GetCurrentNamespace()
+func isSourceCreated(ctx context.Context) bool {
+	nsList, err := kube.DefaultClient.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		log.Printf("Error listing namespaces: %v\n", err)
+		return false
+	}
 
-	dests, err := kube.DefaultClient.OdigosClient.Destinations(odigosNamespace).List(ctx, metav1.ListOptions{})
+	for _, ns := range nsList.Items {
+		sourceList, err := kube.DefaultClient.OdigosClient.Sources(ns.Namespace).List(ctx, metav1.ListOptions{})
+		if err != nil {
+			log.Printf("Error listing sources: %v\n", err)
+			return false
+		}
+
+		if len(sourceList.Items) > 0 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isDestinationConnected(ctx context.Context) bool {
+	ns := env.GetCurrentNamespace()
+
+	dests, err := kube.DefaultClient.OdigosClient.Destinations(ns).List(ctx, metav1.ListOptions{})
 	if err != nil {
 		log.Printf("Error listing destinations: %v\n", err)
 		return false
 	}
 
 	return len(dests.Items) > 0
-}
-
-func isSomethingLabeled(ctx context.Context) bool {
-	labelSelector := fmt.Sprintf("%s=%s", consts.OdigosInstrumentationLabel, consts.InstrumentationEnabled)
-	ns, err := kube.DefaultClient.CoreV1().Namespaces().List(ctx, metav1.ListOptions{
-		LabelSelector: labelSelector,
-	})
-
-	if err != nil {
-		log.Printf("Error listing namespaces: %v\n", err)
-		return false
-	}
-
-	if len(ns.Items) > 0 {
-		return true
-	}
-
-	deps, err := kube.DefaultClient.AppsV1().Deployments("").List(ctx, metav1.ListOptions{
-		LabelSelector: labelSelector,
-	})
-
-	if err != nil {
-		log.Printf("Error listing deployments: %v\n", err)
-		return false
-	}
-
-	if len(deps.Items) > 0 {
-		return true
-	}
-
-	ss, err := kube.DefaultClient.AppsV1().StatefulSets("").List(ctx, metav1.ListOptions{
-		LabelSelector: labelSelector,
-	})
-
-	if err != nil {
-		log.Printf("Error listing statefulsets: %v\n", err)
-		return false
-	}
-
-	if len(ss.Items) > 0 {
-		return true
-	}
-
-	ds, err := kube.DefaultClient.AppsV1().DaemonSets("").List(ctx, metav1.ListOptions{
-		LabelSelector: labelSelector,
-	})
-
-	if err != nil {
-		log.Printf("Error listing daemonsets: %v\n", err)
-		return false
-	}
-
-	if len(ds.Items) > 0 {
-		return true
-	}
-
-	return false
 }
