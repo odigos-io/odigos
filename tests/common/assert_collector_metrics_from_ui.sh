5519c68002344c757e3edfc47524a501b363b409
Ron Federman <ron@keyval.dev>
2025-01-31 20:00:34 +0200

    reduce flakiness in check for collector metrics report from ui

--- tests/common/assert_collector_metrics_from_ui.sh
+++ tests/common/assert_collector_metrics_from_ui.sh
@@ -7,36 +7,25 @@ if ! command -v jq &> /dev/null; then
 fi
 
 print_help() {
-    echo "Usage: $0 [namespace] <valid_sources> <valid_destinations>"
+    echo "Usage: $0 [namespace]"
     echo ""
     echo "Arguments:"
     echo "  namespace       (optional) The Kubernetes namespace of the UI service (default: 'odigos-system')"
-    echo "  valid_sources   (required) The minimum number of valid sources expected"
-    echo "  valid_destinations (required) The minimum number of valid destinations expected"
-    echo ""
-    echo "Example:"
-    echo "  $0 odigos-system 5 2   # Uses 'odigos-system' as the namespace, expects 5 valid sources & 2 valid destinations"
-    echo "  $0 5 2                 # Uses default namespace 'odigos-system', expects 5 valid sources & 2 valid destinations"
     exit 1
 }
 
-if [[ $# -lt 2 || $# -gt 3 ]]; then
+if [[ $# -gt 1 ]]; then
     echo "❌ Error: Invalid number of arguments."
     print_help
 fi
 
-if [[ $# -eq 3 ]]; then
+if [[ $# -eq 1 ]]; then
     NAMESPACE=$1
-    VALID_SOURCES=$2
-    VALID_DESTINATIONS=$3
 else
     NAMESPACE="odigos-system"
-    VALID_SOURCES=$1
-    VALID_DESTINATIONS=$2
 fi
 
 echo "ℹ️ Using namespace: $NAMESPACE"
-echo "ℹ️ Expecting at least $VALID_SOURCES valid sources and $VALID_DESTINATIONS valid destinations"
 
 find_free_port() {
     while :; do
@@ -109,16 +98,18 @@ if [[ $(echo "$response" | jq '.data.getOverviewMetrics') == "null" ]]; then
     exit 1
 fi
 
+# check that we have at least one valid source and destination
+# trying to count the number of sources appeared in the response and asserting that number can lead to flaky tests
 valid_sources_count=$(echo "$response" | jq '.data.getOverviewMetrics.sources | map(select(.totalDataSent > 0)) | length')
 valid_destinations_count=$(echo "$response" | jq '.data.getOverviewMetrics.destinations | map(select(.totalDataSent > 0)) | length')
 
-if [[ "$valid_sources_count" -lt "$VALID_SOURCES" ]]; then
-    echo "❌ Error: Expected at least $VALID_SOURCES valid sources, but found only $valid_sources_count."
+if [[ "$valid_sources_count" -lt "1" ]]; then
+    echo "❌ Error: Expected at least 1 valid source metrics, but found only $valid_sources_count."
     exit 1
 fi
 
-if [[ "$valid_destinations_count" -lt "$VALID_DESTINATIONS" ]]; then
-    echo "❌ Error: Expected at least $VALID_DESTINATIONS valid destinations, but found only $valid_destinations_count."
+if [[ "$valid_destinations_count" -lt "1" ]]; then
+    echo "❌ Error: Expected at least 1 valid destinations metrics, but found only $valid_destinations_count."
     exit 1
 fi
 
