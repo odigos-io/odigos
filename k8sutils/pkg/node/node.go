cab2563f6a16ddfc74f88fce500ef3317d3373fb
tamirdavid1 <tamir@odigos.io>
2025-02-04 11:18:34 +0200

    fix: golangci-lint

--- k8sutils/pkg/node/node.go
+++ k8sutils/pkg/node/node.go
@@ -3,13 +3,14 @@ package node
 import (
 	"context"
 
-	"github.com/odigos-io/odigos/api/k8sconsts"
-	"github.com/odigos-io/odigos/common"
-	"github.com/odigos-io/odigos/k8sutils/pkg/env"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+
+	"github.com/odigos-io/odigos/api/k8sconsts"
+	"github.com/odigos-io/odigos/common"
+	"github.com/odigos-io/odigos/k8sutils/pkg/env"
 )
 
 func AddLabelToNode(nodeName string, labelKey string, labelValue string) error {
