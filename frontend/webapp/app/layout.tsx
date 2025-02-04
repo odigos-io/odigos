bce97aca6249cb99c1c7099826c4ef45a467b464
Ben Elferink <ben.elferink@icloud.com>
2025-02-04 15:45:34 +0200

    fix: hydration errors

--- frontend/webapp/app/layout.tsx
+++ frontend/webapp/app/layout.tsx
@@ -1,35 +1,45 @@
 'use client';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import { METADATA } from '@/utils';
 import { ApolloWrapper } from '@/lib';
 import { ThemeProvider } from '@/styles';
 import { useDarkModeStore } from '@/store';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+function RootLayout({ children }: PropsWithChildren) {
   const { darkMode } = useDarkModeStore();
 
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning={true}>
       <head>
         <meta name='description' content={METADATA.title} />
         <link rel='icon' type='image/svg+xml' href={`/${METADATA.icon}`} />
         <link rel='manifest' href='/manifest.json' />
         <title>{METADATA.title}</title>
       </head>
-      <ApolloWrapper>
-        <ThemeProvider darkMode={darkMode}>
-          <body
-            style={{
-              width: '100vw',
-              height: '100vh',
-              margin: 0,
-              backgroundColor: darkMode ? '#111111' : '#EEEEEE',
-            }}
-          >
-            {children}
-          </body>
-        </ThemeProvider>
-      </ApolloWrapper>
+
+      {/*
+        The "!window" check is to prevent hydration errors,
+        previously we had "suppressHydrationWarning={true}" on the "body" tag, but it was causing issues and had to be handled better.
+      */}
+
+      {!window ? null : (
+        <ApolloWrapper>
+          <ThemeProvider darkMode={darkMode}>
+            <body
+              style={{
+                width: '100vw',
+                height: '100vh',
+                margin: 0,
+                backgroundColor: darkMode ? '#111111' : '#EEEEEE',
+              }}
+            >
+              {children}
+            </body>
+          </ThemeProvider>
+        </ApolloWrapper>
+      )}
     </html>
   );
 }
+
+export default RootLayout;
