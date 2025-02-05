44fcd024152e61cbc0ab3a602e82fa7921ef87cf
Amir Blum <amirgiraffe@gmail.com>
2025-02-05 16:53:18 +0200

    fix: remove pod label from workload manifest (#2377)
    Co-authored-by: Ron Federman <ron@keyval.dev>
    Co-authored-by: Ben Elferink <ben.elferink@icloud.com>
    Co-authored-by: tamirdavid1 <tamir@odigos.io>

---
+++ instrumentor/controllers/agentenabled/manager.go
@@ -0,0 +1,76 @@
+package agentenabled
+
+import (
+	odigosv1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
+	instrumentorpredicate "github.com/odigos-io/odigos/instrumentor/controllers/utils/predicates"
+	odigospredicate "github.com/odigos-io/odigos/k8sutils/pkg/predicate"
+	corev1 "k8s.io/api/core/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+func SetupWithManager(mgr ctrl.Manager) error {
+	err := builder.
+		ControllerManagedBy(mgr).
+		Named("agentenabled-collectorsgroup").
+		For(&odigosv1.CollectorsGroup{}).
+		WithEventFilter(predicate.And(&odigospredicate.OdigosCollectorsGroupNodePredicate, &odigospredicate.CgBecomesReadyPredicate{})).
+		Complete(&CollectorsGroupReconciler{
+			Client: mgr.GetClient(),
+		})
+	if err != nil {
+		return err
+	}
+
+	err = builder.
+		ControllerManagedBy(mgr).
+		Named("agentenabled-instrumentationconfig").
+		For(&odigosv1.InstrumentationConfig{}).
+		// When the runtime details change we need to potentially update the instrumentation config and roll out the workload.
+		// When the instrumentation config is deleted, we need to roll out the workload to un-instrument it.
+		WithEventFilter(predicate.Or(&instrumentorpredicate.RuntimeDetailsChangedPredicate{}, odigospredicate.DeletionPredicate{})).
+		Complete(&InstrumentationConfigReconciler{
+			Client: mgr.GetClient(),
+		})
+	if err != nil {
+		return err
+	}
+
+	err = builder.
+		ControllerManagedBy(mgr).
+		Named("agentenabled-instrumentationrules").
+		For(&odigosv1.InstrumentationRule{}).
+		WithEventFilter(&instrumentorpredicate.OtelSdkInstrumentationRulePredicate{}).
+		Complete(&InstrumentationRuleReconciler{
+			Client: mgr.GetClient(),
+		})
+	if err != nil {
+		return err
+	}
+
+	err = builder.
+		ControllerManagedBy(mgr).
+		Named("agentenabled-effectiveconfig").
+		For(&corev1.ConfigMap{}).
+		WithEventFilter(odigospredicate.OdigosEffectiveConfigMapPredicate).
+		Complete(&EffectiveConfigReconciler{
+			Client: mgr.GetClient(),
+		})
+	if err != nil {
+		return err
+	}
+
+	err = builder.
+		WebhookManagedBy(mgr).
+		For(&corev1.Pod{}).
+		WithDefaulter(&PodsWebhook{
+			Client: mgr.GetClient(),
+		}).
+		Complete()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
