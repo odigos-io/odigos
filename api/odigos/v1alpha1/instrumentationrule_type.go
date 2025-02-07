odigos-io/odigos

commit fbfa7918f0c37adc4a4a7ed28defcb1ca92013c7

feat: instrumentation rule for otel distros (#2396)
This is to move away from the Deprecated `OtelSdk` and towards using the
more generic OtelDistribution

---------

Co-authored-by: Ron Federman <73110295+RonFed@users.noreply.github.com>

api/config/crd/bases/odigos.io_instrumentationrules.yaml
@@ -128,11 +128,23 @@ spec:
                   regarding the rule for convenience. For example: why it was added.
                   Odigos does not use or assume any meaning from this field.'
                 type: string
+              otelDistros:
+                description: Set the otel distros to use instead of the defaults.
+                properties:
+                  otelDistroNames:
+                    description: |-
+                      Set a list of distribution names that take priority over the default distributions.
+                      if a language is not in this list, the default distribution will be used.
+                      it multiple distributions are specified for the same language, in one or many rules,
+                      the behavior is undefined.
+                    items:
+                      type: string
+                    type: array
+                required:
+                - otelDistroNames
+                type: object
               otelSdks:
-                description: |-
-                  Set the OtelSdk to use for the workloads in this rule.
-                  instrumentation libraries will be ignored if set.
-                  the rule will be used only for languages which are specified, and ignored otherwise.
+                description: 'Deprecated: use OrelDistros instead.'
                 properties:
                   otelSdkByLanguage:
                     additionalProperties:

api/odigos/v1alpha1/instrumentationrule_type.go
@@ -61,10 +61,11 @@ type InstrumentationRuleSpec struct {
 	// Allows to configure payload collection aspects for different types of payloads.
 	PayloadCollection *instrumentationrules.PayloadCollection `json:"payloadCollection,omitempty"`
 
-	// Set the OtelSdk to use for the workloads in this rule.
-	// instrumentation libraries will be ignored if set.
-	// the rule will be used only for languages which are specified, and ignored otherwise.
+	// Deprecated: use OrelDistros instead.
 	OtelSdks *instrumentationrules.OtelSdks `json:"otelSdks,omitempty"`
+
+	// Set the otel distros to use instead of the defaults.
+	OtelDistros *instrumentationrules.OtelDistros `json:"otelDistros,omitempty"`
 
 	// Configure which code attributes should be recorded as span attributes.
 	CodeAttributes *instrumentationrules.CodeAttributes `json:"codeAttributes,omitempty"`

api/odigos/v1alpha1/instrumentationrules/otel-sdk.go
@@ -4,6 +4,18 @@ import "github.com/odigos-io/odigos/common"
 
 // +kubebuilder:object:generate=true
 // +kubebuilder:deepcopy-gen=true
+// Deprecated: Use OtelDistro instead
 type OtelSdks struct {
 	OtelSdkByLanguage map[common.ProgrammingLanguage]common.OtelSdk `json:"otelSdkByLanguage"`
+}
+
+// +kubebuilder:object:generate=true
+// +kubebuilder:deepcopy-gen=true
+type OtelDistros struct {
+
+	// Set a list of distribution names that take priority over the default distributions.
+	// if a language is not in this list, the default distribution will be used.
+	// it multiple distributions are specified for the same language, in one or many rules,
+	// the behavior is undefined.
+	OtelDistroNames []string `json:"otelDistroNames"`
 }

docs/api-reference/odigos.io.v1alpha1.mdx
@@ -1869,9 +1869,18 @@ If empty, no instrumentation libraries will be used.</p>
 <a href="https://pkg.go.dev/github.com/odigos-io/odigos/api/odigos/v1alpha1/instrumentationrules#OtelSdks"><code>instrumentationrules.OtelSdks</code></a>
 </td>
 <td>
-   <p>Set the OtelSdk to use for the workloads in this rule.
-instrumentation libraries will be ignored if set.
-the rule will be used only for languages which are specified, and ignored otherwise.</p>
+   <p>Deprecated: use OrelDistros instead.</p>
+</td>
+</tr>
+<tr>
+<td>
+<code>otelDistros</code> <B>[Required]</B>
+</td>
+<td>
+<a href="https://pkg.go.dev/github.com/odigos-io/odigos/api/odigos/v1alpha1/instrumentationrules#OtelDistros"><code>instrumentationrules.OtelDistros</code></a>
+</td>
+<td>
+   <p>Set the otel distros to use instead of the defaults.</p>
 </td>
 </tr>
 <tr>

helm/odigos/templates/crds/odigos.io_instrumentationrules.yaml
@@ -128,11 +128,23 @@ spec:
                   regarding the rule for convenience. For example: why it was added.
                   Odigos does not use or assume any meaning from this field.'
                 type: string
+              otelDistros:
+                description: Set the otel distros to use instead of the defaults.
+                properties:
+                  otelDistroNames:
+                    description: |-
+                      Set a list of distribution names that take priority over the default distributions.
+                      if a language is not in this list, the default distribution will be used.
+                      it multiple distributions are specified for the same language, in one or many rules,
+                      the behavior is undefined.
+                    items:
+                      type: string
+                    type: array
+                required:
+                - otelDistroNames
+                type: object
               otelSdks:
-                description: |-
-                  Set the OtelSdk to use for the workloads in this rule.
-                  instrumentation libraries will be ignored if set.
-                  the rule will be used only for languages which are specified, and ignored otherwise.
+                description: 'Deprecated: use OrelDistros instead.'
                 properties:
                   otelSdkByLanguage:
                     additionalProperties:

api/generated/odigos/applyconfiguration/odigos/v1alpha1/instrumentationrulespec.go
@@ -32,6 +32,7 @@ type InstrumentationRuleSpecApplyConfiguration struct {
 	InstrumentationLibraries *[]InstrumentationLibraryGlobalIdApplyConfiguration `json:"instrumentationLibraries,omitempty"`
 	PayloadCollection        *instrumentationrules.PayloadCollection             `json:"payloadCollection,omitempty"`
 	OtelSdks                 *instrumentationrules.OtelSdks                      `json:"otelSdks,omitempty"`
+	OtelDistros              *instrumentationrules.OtelDistros                   `json:"otelDistros,omitempty"`
 	CodeAttributes           *instrumentationrules.CodeAttributes                `json:"codeAttributes,omitempty"`
 }
 
@@ -106,6 +107,14 @@ func (b *InstrumentationRuleSpecApplyConfiguration) WithPayloadCollection(value 
 // If called multiple times, the OtelSdks field is set to the value of the last call.
 func (b *InstrumentationRuleSpecApplyConfiguration) WithOtelSdks(value instrumentationrules.OtelSdks) *InstrumentationRuleSpecApplyConfiguration {
 	b.OtelSdks = &value
+	return b
+}
+
+// WithOtelDistros sets the OtelDistros field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the OtelDistros field is set to the value of the last call.
+func (b *InstrumentationRuleSpecApplyConfiguration) WithOtelDistros(value instrumentationrules.OtelDistros) *InstrumentationRuleSpecApplyConfiguration {
+	b.OtelDistros = &value
 	return b
 }
 

api/odigos/v1alpha1/instrumentationrules/zz_generated.deepcopy.go
@@ -154,6 +154,26 @@ func (in *MessagingPayloadCollection) DeepCopy() *MessagingPayloadCollection {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *OtelDistros) DeepCopyInto(out *OtelDistros) {
+	*out = *in
+	if in.OtelDistroNames != nil {
+		in, out := &in.OtelDistroNames, &out.OtelDistroNames
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new OtelDistros.
+func (in *OtelDistros) DeepCopy() *OtelDistros {
+	if in == nil {
+		return nil
+	}
+	out := new(OtelDistros)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *OtelSdks) DeepCopyInto(out *OtelSdks) {
 	*out = *in
 	if in.OtelSdkByLanguage != nil {

api/odigos/v1alpha1/zz_generated.deepcopy.go
@@ -864,6 +864,11 @@ func (in *InstrumentationRuleSpec) DeepCopyInto(out *InstrumentationRuleSpec) {
 		*out = new(instrumentationrules.OtelSdks)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.OtelDistros != nil {
+		in, out := &in.OtelDistros, &out.OtelDistros
+		*out = new(instrumentationrules.OtelDistros)
+		(*in).DeepCopyInto(*out)
+	}
 	if in.CodeAttributes != nil {
 		in, out := &in.CodeAttributes, &out.CodeAttributes
 		*out = new(instrumentationrules.CodeAttributes)

commit 3c5a428f9d6d490c57f8902536f4cef96f8fd02a

Fix typos (#2403)
Fixed 2 typos introduced in #2396

api/config/crd/bases/odigos.io_instrumentationrules.yaml
@@ -135,8 +135,7 @@ spec:
                     description: |-
                       Set a list of distribution names that take priority over the default distributions.
                       if a language is not in this list, the default distribution will be used.
-                      it multiple distributions are specified for the same language, in one or many rules,
-                      the behavior is undefined.
+                      if multiple distributions are specified for the same language, in one or many rules, the behavior is undefined.
                     items:
                       type: string
                     type: array
@@ -144,7 +143,7 @@ spec:
                 - otelDistroNames
                 type: object
               otelSdks:
-                description: 'Deprecated: use OrelDistros instead.'
+                description: 'Deprecated: use OtelDistros instead.'
                 properties:
                   otelSdkByLanguage:
                     additionalProperties:

api/odigos/v1alpha1/instrumentationrule_type.go
@@ -61,7 +61,7 @@ type InstrumentationRuleSpec struct {
 	// Allows to configure payload collection aspects for different types of payloads.
 	PayloadCollection *instrumentationrules.PayloadCollection `json:"payloadCollection,omitempty"`
 
-	// Deprecated: use OrelDistros instead.
+	// Deprecated: use OtelDistros instead.
 	OtelSdks *instrumentationrules.OtelSdks `json:"otelSdks,omitempty"`
 
 	// Set the otel distros to use instead of the defaults.

api/odigos/v1alpha1/instrumentationrules/otel-sdk.go
@@ -15,7 +15,6 @@ type OtelDistros struct {
 
 	// Set a list of distribution names that take priority over the default distributions.
 	// if a language is not in this list, the default distribution will be used.
-	// it multiple distributions are specified for the same language, in one or many rules,
-	// the behavior is undefined.
+	// if multiple distributions are specified for the same language, in one or many rules, the behavior is undefined.
 	OtelDistroNames []string `json:"otelDistroNames"`
 }

docs/api-reference/odigos.io.v1alpha1.mdx
@@ -1869,7 +1869,7 @@ If empty, no instrumentation libraries will be used.</p>
 <a href="https://pkg.go.dev/github.com/odigos-io/odigos/api/odigos/v1alpha1/instrumentationrules#OtelSdks"><code>instrumentationrules.OtelSdks</code></a>
 </td>
 <td>
-   <p>Deprecated: use OrelDistros instead.</p>
+   <p>Deprecated: use OtelDistros instead.</p>
 </td>
 </tr>
 <tr>

helm/odigos/templates/crds/odigos.io_instrumentationrules.yaml
@@ -135,8 +135,7 @@ spec:
                     description: |-
                       Set a list of distribution names that take priority over the default distributions.
                       if a language is not in this list, the default distribution will be used.
-                      it multiple distributions are specified for the same language, in one or many rules,
-                      the behavior is undefined.
+                      if multiple distributions are specified for the same language, in one or many rules, the behavior is undefined.
                     items:
                       type: string
                     type: array
@@ -144,7 +143,7 @@ spec:
                 - otelDistroNames
                 type: object
               otelSdks:
-                description: 'Deprecated: use OrelDistros instead.'
+                description: 'Deprecated: use OtelDistros instead.'
                 properties:
                   otelSdkByLanguage:
                     additionalProperties: