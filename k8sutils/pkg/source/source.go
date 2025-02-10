41156d6a02de8aef93bf646de9ad65a133324f99
Amir Blum <amirgiraffe@gmail.com>
2025-02-10 10:53:23 +0200

    feat: add 'runtime detection' condition to ic status

--- k8sutils/pkg/source/source.go
+++ k8sutils/pkg/source/source.go
@@ -6,6 +6,7 @@ import (
 
 	odigosv1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
 	k8sutils "github.com/odigos-io/odigos/k8sutils/pkg/utils"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -17,26 +18,41 @@ import (
 // 4) False
 func IsObjectInstrumentedBySource(ctx context.Context,
 	k8sClient client.Client,
-	obj client.Object) (bool, odigosv1.MarkedForInstrumentationReason, string, error) {
+	obj client.Object) (bool, metav1.Condition, error) {
 	// Check if a Source object exists for this object
 	sources, err := odigosv1.GetSources(ctx, k8sClient, obj)
 	if err != nil {
-		reason := odigosv1.MarkedForInstrumentationReasonError
-		return false, reason, "cannot determine if workload is marked for instrumentation due to error", err
+		condition := metav1.Condition{
+			Type:    odigosv1.MarkedForInstrumentationStatusConditionType,
+			Status:  metav1.ConditionUnknown,
+			Reason:  string(odigosv1.MarkedForInstrumentationReasonError),
+			Message: "cannot determine if workload is marked for instrumentation due to error",
+		}
+		return false, condition, err
 	}
 
 	if sources.Workload != nil {
 		if !odigosv1.IsDisabledSource(sources.Workload) && !k8sutils.IsTerminating(sources.Workload) {
-			reason := odigosv1.MarkedForInstrumentationReasonWorkloadSource
 			message := fmt.Sprintf("workload marked for automatic instrumentation by workload source CR '%s' in namespace '%s'",
 				sources.Workload.Name, sources.Workload.Namespace)
-			return true, reason, message, nil
+			condition := metav1.Condition{
+				Type:    odigosv1.MarkedForInstrumentationStatusConditionType,
+				Status:  metav1.ConditionTrue,
+				Reason:  string(odigosv1.MarkedForInstrumentationReasonWorkloadSource),
+				Message: message,
+			}
+			return true, condition, nil
 		}
 		if odigosv1.IsDisabledSource(sources.Workload) && !k8sutils.IsTerminating(sources.Workload) {
-			reason := odigosv1.MarkedForInstrumentationReasonWorkloadSourceDisabled
 			message := fmt.Sprintf("workload marked to disable instrumentation by workload source CR '%s' in namespace '%s'",
 				sources.Workload.Name, sources.Workload.Namespace)
-			return false, reason, message, nil
+			condition := metav1.Condition{
+				Type:    odigosv1.MarkedForInstrumentationStatusConditionType,
+				Status:  metav1.ConditionFalse,
+				Reason:  string(odigosv1.MarkedForInstrumentationReasonWorkloadSourceDisabled),
+				Message: message,
+			}
+			return false, condition, nil
 		}
 	}
 
@@ -44,12 +60,22 @@ func IsObjectInstrumentedBySource(ctx context.Context,
 		reason := odigosv1.MarkedForInstrumentationReasonNamespaceSource
 		message := fmt.Sprintf("workload marked for automatic instrumentation by namespace source CR '%s' in namespace '%s'",
 			sources.Namespace.Name, sources.Namespace.Namespace)
-		return true, reason, message, nil
+		condition := metav1.Condition{
+			Type:    odigosv1.MarkedForInstrumentationStatusConditionType,
+			Status:  metav1.ConditionTrue,
+			Reason:  string(reason),
+			Message: message,
+		}
+		return true, condition, nil
 	}
 
-	reason := odigosv1.MarkedForInstrumentationReasonNoSource
-	message := "workload not marked for automatic instrumentation by any source CR"
-	return false, reason, message, nil
+	condition := metav1.Condition{
+		Type:    odigosv1.MarkedForInstrumentationStatusConditionType,
+		Status:  metav1.ConditionFalse,
+		Reason:  string(odigosv1.MarkedForInstrumentationReasonNoSource),
+		Message: "workload not marked for automatic instrumentation by any source CR",
+	}
+	return false, condition, nil
 }
 
 // IsSourceRelevant returns true if a Source:
