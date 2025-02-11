b92f3806c500b96e20c64460da4c31cef3151f76
Amir Blum <amirgiraffe@gmail.com>
2025-02-11 21:14:18 +0200

    fix: avoid adding the mount paths twice

--- instrumentor/controllers/agentenabled/podswebhook/mount.go
+++ instrumentor/controllers/agentenabled/podswebhook/mount.go
@@ -8,10 +8,24 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+func checkIfMountDirectoryExists(containerSpec *corev1.Container, dir string) bool {
+	for _, volumeMount := range containerSpec.VolumeMounts {
+		if volumeMount.SubPath == dir {
+			return true
+		}
+	}
+	return false
+}
+
 func MountDirectory(containerSpec *corev1.Container, dir string) {
 	// TODO: assuming the directory always starts with {{ODIGOS_AGENTS_DIR}}. This should be validated.
 	// Should we return errors here to validate static values?
 	relativePath := strings.TrimPrefix(dir, distro.AgentPlaceholderDirectory+"/")
+	if checkIfMountDirectoryExists(containerSpec, dir) {
+		// avoid adding the directory volume twice to the container
+		return
+	}
+
 	absolutePath := strings.ReplaceAll(dir, distro.AgentPlaceholderDirectory, k8sconsts.OdigosAgentsDirectory)
 	containerSpec.VolumeMounts = append(containerSpec.VolumeMounts, corev1.VolumeMount{
 		Name:      k8sconsts.OdigosAgentMountVolumeName,
@@ -21,7 +35,22 @@ func MountDirectory(containerSpec *corev1.Container, dir string) {
 	})
 }
 
+func checkIfVolumExists(pod *corev1.Pod) bool {
+	for _, volume := range pod.Spec.Volumes {
+		if volume.Name == k8sconsts.OdigosAgentMountVolumeName {
+			return true
+		}
+	}
+	return false
+}
+
 func MountPodVolume(pod *corev1.Pod) {
+
+	if checkIfVolumExists(pod) {
+		// avoid adding the volume twice to the pod
+		return
+	}
+
 	pod.Spec.Volumes = append(pod.Spec.Volumes, corev1.Volume{
 		Name: k8sconsts.OdigosAgentMountVolumeName,
 		VolumeSource: corev1.VolumeSource{
