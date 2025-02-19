411d65f30c00c587806777f2dfa1e8af6d98647b
Ron Federman <ron@keyval.dev>
2025-02-19 11:17:00 +0200

    pass image name to instrumentor deployment creator

--- cli/cmd/resources/instrumentor.go
+++ cli/cmd/resources/instrumentor.go
@@ -716,7 +716,7 @@ func (a *instrumentorResourceManager) InstallFromScratch(ctx context.Context) er
 		NewInstrumentorRoleBinding(a.ns),
 		NewInstrumentorClusterRole(a.config.OpenshiftEnabled),
 		NewInstrumentorClusterRoleBinding(a.ns),
-		NewInstrumentorDeployment(a.ns, a.odigosVersion, a.config.TelemetryEnabled, a.config.ImagePrefix, a.config.InstrumentorImage, a.tier),
+		NewInstrumentorDeployment(a.ns, a.odigosVersion, a.config.TelemetryEnabled, a.config.ImagePrefix, imageName, a.tier),
 		NewInstrumentorService(a.ns),
 	}
 
