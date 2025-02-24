cd370a6537e4cc192ec12106194548f9169c1a25
Erik Nazuyev <erik@Eriks-MacBook-Pro-2.local>
2025-02-24 11:46:29 +0200

    Added eks execution to cross cloud tests

---
+++ tests-infrastructure/terraform/eks/variables.tf
@@ -0,0 +1,24 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  default     = "tests-eks"
+}
+
+variable "region" {
+  description = "AWS region to deploy the cluster in"
+  default     = "us-east-1"
+}
+
+variable "node_count" {
+  description = "Number of nodes in the cluster"
+  default     = 2
+}
+
+variable "test_scenario" {
+  description = "Test scenario to differentiate state files"
+  type        = string
+}
+
+variable "run_id" {
+  description = "GitHub run ID for uniquely identifying state files"
+  type        = string
+}
