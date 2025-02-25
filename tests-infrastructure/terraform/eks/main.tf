96e4b0dca21eec86d018cb56003de3d85594551f
Erik Naz <erik@odigos.io>
2025-02-25 13:56:38 +0200

    finilize pipeline

--- tests-infrastructure/terraform/eks/main.tf
+++ tests-infrastructure/terraform/eks/main.tf
@@ -44,7 +44,6 @@ module "eks" {
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
 
-  # Ensure that the cluster endpoint is only accessible privately.
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
