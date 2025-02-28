512e1d2bff0560759b41a2c2d227123dea9659ce
Mike Dame <mike@odigos.io>
2025-02-28 11:41:06 -0500

    Add Operator (#2388)
    This adds a very basic operator for installing, uninstalling, and
    upgrading Odigos. It is based on the same functions used by the CLI,
    which should be refactored into a common directory at some point in the
    future.
    
    ## Important files:
    
    * `operator/api/v1alpha1/odigos_types.go`: This creates the
    `operator.odigos.io` self-contained API. The CRD in this file represents
    an installation of Odigos, and supports most of the current config
    fields. This API can be versioned separately from the main Odigos API
    fairly easily because it is self-contained, allowing us to support
    future breaking changes with conversion webhooks.
    * `operator/internal/controller/odigos_controller.go`: This is the main
    reconciler for `Odigos` CRs. It handles new installations (by creating a
    CR), modifying an existing installation (modifying the CR), uninstalling
    (deleting the CR), or upgrades (by upgrading the operator, see below)
    
    Almost everything else is generated by operator-sdk and shouldn't need
    much, if any review.
    
    ## Usage
    
    The Operator is installed in any namespace the user wants (such as
    `default` or `odigos-operator-system`). The Operator then watches only
    its own namespace for `Odigos` objects.
    
    When an `Odigos` object is created, the Operator will install Odigos in
    its own namespace. So the operator and Odigos run in the same namespace.
    Only one `Odigos` object can exist.
    
    If the `Odigos` object is modified, the Operator will re-run the
    install, patching any changes.
    
    If the `Odigos` object is deleted, the Operator will uninstall Odigos.
    The `Odigos` object uses a finalizer to allow resources to be cleaned
    up.
    
    OpenShift's Operator Lifecycle Manager will handle upgrading the
    Operator pod from one version to the next. When the new operator pod
    starts, it will pick up the existing Odigos config and attempt an
    upgrade.
    
    ## Important notes
    
    The version of Odigos is pinned by the `VERSION` variable, defined in
    `operator/Makefile`. This uses kustomize to define a configmap that the
    operator uses to know what version of Odigos to pull.
    
    Because the operator code depends on the current CLI code, the operator
    should stay pinned to the same version as Odigos for now. This means
    that upgrades can be done by simply upgrading the operator, which will
    find the existing Odigos config, and perform the same upgrade logic as
    the `upgrade` cli command.
    
    The operator does depend on a broad range of RBAC rules. It requires
    every permission that Odigos has (to be able to grant Odigos those
    permissions), plus the ability to create and delete all of Odigos's
    resources (deployments, serviceaccounts, etc). We might be able to scope
    the latter permissions to just the operator's namespace.
    
    ## Screenshots
    
    Here's how it looks to install with OpenShift:
    
    Odigos listed in Operators view
    
    <img width="1228" alt="Screenshot 2025-02-07 at 3 39 29 PM"
    src="https://github.com/user-attachments/assets/a7c85289-4264-4fd5-b388-d63c759ab9f5"
    />
    
    Odigos Operator config page
    
    <img width="1228" alt="Screenshot 2025-02-07 at 3 39 39 PM"
    src="https://github.com/user-attachments/assets/76510d43-b242-42fc-9179-51deadc27848"
    />
    
    Creating an `Odigos` object (installing Odigos)
    
    <img width="1231" alt="Screenshot 2025-02-07 at 3 40 05 PM"
    src="https://github.com/user-attachments/assets/b88c6363-99a5-4c4a-838a-f2b780a4e4c3"
    />
    
    Odigos object status post-install/upgrade
    
    <img width="1592" alt="Screenshot 2025-02-07 at 3 40 30 PM"
    src="https://github.com/user-attachments/assets/8ab03ba5-ec90-4088-9a73-50eb2bf168de"
    />

--- k8sutils/pkg/installationmethod/installationmethod.go
+++ k8sutils/pkg/installationmethod/installationmethod.go
@@ -3,6 +3,7 @@ package installationmethod
 type K8sInstallationMethod string
 
 const (
-	K8sInstallationMethodOdigosCli K8sInstallationMethod = "odigos-cli"
-	K8sInstallationMethodHelm      K8sInstallationMethod = "helm"
+	K8sInstallationMethodOdigosCli      K8sInstallationMethod = "odigos-cli"
+	K8sInstallationMethodHelm           K8sInstallationMethod = "helm"
+	K8sInstallationMethodOdigosOperator K8sInstallationMethod = "odigos-operator"
 )
