097e08a24f21907ddd8a0142260bc862865e6d8e
Ben Elferink <ben.elferink@icloud.com>
2025-03-06 17:17:03 +0200

    chore: cleanup unused imports

--- frontend/webapp/hooks/sources/useSourceCRUD.ts
+++ frontend/webapp/hooks/sources/useSourceCRUD.ts
@@ -4,7 +4,7 @@ import { useNamespace } from '../compute-platform';
 import { useLazyQuery, useMutation } from '@apollo/client';
 import { GET_INSTANCES, GET_SOURCE, GET_SOURCES, PERSIST_SOURCE, UPDATE_K8S_ACTUAL_SOURCE } from '@/graphql';
 import type { FetchedSource, NamespaceInstrumentInput, PaginatedData, SourceInstrumentInput, SourceUpdateInput } from '@/@types';
-import { Condition, CRUD, DISPLAY_TITLES, ENTITY_TYPES, FORM_ALERTS, getSseTargetFromId, NOTIFICATION_TYPE, sleep, type Source, type WorkloadId } from '@odigos/ui-utils';
+import { Condition, CRUD, DISPLAY_TITLES, ENTITY_TYPES, FORM_ALERTS, getSseTargetFromId, NOTIFICATION_TYPE, type Source, type WorkloadId } from '@odigos/ui-utils';
 import {
   type NamespaceSelectionFormData,
   type SourceFormData,
