odigos-io/odigos

commit 37f01e591286af6dd45826056ac238204b52ced9

fix: use vpid in e2e test assert after agent merge (#2538)
Updating the tests after merging
https://github.com/odigos-io/opentelemetry-node/pull/18

tests/common/assert/simple-demo-instrumented.yaml
@@ -252,7 +252,7 @@ status:
       (value != null): true
     - key: telemetry.distro.version
       (value != null): true
-    - key: process.pid
+    - key: process.vpid
       (value != null): true
     - key: k8s.namespace.name
       (value != null): true

tests/e2e/workload-lifecycle/01-assert-instrumented.yaml
@@ -85,7 +85,7 @@ status:
       value: "14.0.0"
     - key: telemetry.distro.version
       value: e2e-test
-    - key: process.pid
+    - key: process.vpid
       (value != null): true
     - key: k8s.namespace.name
       (value != null): true
@@ -135,7 +135,7 @@ status:
       (value != null): true
     - key: telemetry.distro.version
       value: e2e-test
-    - key: process.pid
+    - key: process.vpid
       (value != null): true
     - key: k8s.namespace.name
       (value != null): true
@@ -185,7 +185,7 @@ status:
       value: "20.17.0"
     - key: telemetry.distro.version
       value: e2e-test
-    - key: process.pid
+    - key: process.vpid
       (value != null): true
     - key: k8s.namespace.name
       (value != null): true
@@ -235,7 +235,7 @@ status:
       value: "20.17.0"
     - key: telemetry.distro.version
       value: e2e-test
-    - key: process.pid
+    - key: process.vpid
       (value != null): true
     - key: k8s.namespace.name
       (value != null): true

commit 69a515af097fc69bd48881f8d612d2a100aa197c

test: assert less attributes with cli-upgrade before upgrading (#2541)
## Description

Attempt to fix the cli-upgrade test failure introduced with #2538 .

This PR makes it so that we don't assert instrumentation attributes when
installing odigos latest version as the first step for upgrading.

When we have breaking changes in agents or the attributes which we are
asserting, the test that uses the same assert file, will always break on
the latest published version.

I separated the files, so one yaml only checks if the applications are
instrumented, and the second one also assert the full attributes and
internal data which might break. cli-upgrade uses the first one for
"light" assertions only.

It will require that we apply any future updates to both files.

## How Has This Been Tested?

Attempting to make the cli-upgrade tests pass.

- [x] Updated e2e Tests

## Kubernetes Checklist

Only Tests

## User Facing Changes

Only Tests

---------

Co-authored-by: Ron Federman <73110295+RonFed@users.noreply.github.com>

tests/common/assert/simple-demo-instrumented-full.yaml
@@ -164,6 +164,8 @@ metadata:
 status:
   runtimeDetailsByContainer:
     - containerName: coupon
+      language: javascript
+      runtimeVersion: 18.3.0
 ---
 apiVersion: odigos.io/v1alpha1
 kind: InstrumentationConfig
@@ -179,6 +181,8 @@ metadata:
 status:
   runtimeDetailsByContainer:
     - containerName: frontend
+      language: java
+      runtimeVersion: 17.0.14+7
 ---
 apiVersion: odigos.io/v1alpha1
 kind: InstrumentationConfig
@@ -194,6 +198,8 @@ metadata:
 status:
   runtimeDetailsByContainer:
     - containerName: inventory
+      language: python
+      runtimeVersion: 3.11.11
 ---
 apiVersion: odigos.io/v1alpha1
 kind: InstrumentationConfig
@@ -209,6 +215,8 @@ metadata:
 status:
   runtimeDetailsByContainer:
     - containerName: membership
+      language: go
+      runtimeVersion: 1.21.4
 ---
 apiVersion: odigos.io/v1alpha1
 kind: InstrumentationConfig
@@ -224,6 +232,7 @@ metadata:
 status:
   runtimeDetailsByContainer:
     - containerName: pricing
+      language: dotnet
 ---
 ### Instrumentation Instances ###
 apiVersion: odigos.io/v1alpha1
@@ -234,6 +243,23 @@ metadata:
     instrumented-app: deployment-coupon
 status:
   healthy: true
+  identifyingAttributes:
+    - key: service.instance.id
+      (value != null): true
+    - key: telemetry.sdk.language
+      value: nodejs
+    - key: process.runtime.version
+      (value != null): true
+    - key: telemetry.distro.version
+      (value != null): true
+    - key: process.vpid
+      (value != null): true
+    - key: k8s.namespace.name
+      (value != null): true
+    - key: k8s.container.name
+      (value != null): true
+    - key: k8s.pod.name
+      (value != null): true      
 ---
 apiVersion: odigos.io/v1alpha1
 kind: InstrumentationInstance
@@ -243,6 +269,19 @@ metadata:
     instrumented-app: deployment-inventory
 status:
   healthy: true
+  identifyingAttributes:
+    - key: service.instance.id
+      (value != null): true
+    - key: process.pid
+      (value != null): true
+    - key: telemetry.sdk.language
+      value: python
+    - key: k8s.namespace.name
+      (value != null): true
+    - key: k8s.container.name
+      (value != null): true
+    - key: k8s.pod.name
+      (value != null): true        
 ---
 apiVersion: odigos.io/v1alpha1
 kind: InstrumentationInstance
@@ -252,3 +291,4 @@ metadata:
     instrumented-app: deployment-membership
 status:
   healthy: true
+  reason: LoadedSuccessfully

tests/e2e/cli-upgrade/chainsaw-test.yaml
@@ -76,12 +76,12 @@ spec:
               docker pull registry.odigos.io/odigos-demo-inventory:v0.1
               docker pull registry.odigos.io/odigos-demo-membership:v0.1
               docker pull registry.odigos.io/odigos-demo-coupon:v0.1
-              docker pull registry.odigos.io/odigos-demo-inventory:v0.1
+              docker pull registry.odigos.io/odigos-demo-pricing:v0.1
               docker pull registry.odigos.io/odigos-demo-frontend:v0.2
               kind load docker-image registry.odigos.io/odigos-demo-inventory:v0.1
               kind load docker-image registry.odigos.io/odigos-demo-membership:v0.1
               kind load docker-image registry.odigos.io/odigos-demo-coupon:v0.1
-              kind load docker-image registry.odigos.io/odigos-demo-inventory:v0.1
+              kind load docker-image registry.odigos.io/odigos-demo-pricing:v0.1
               kind load docker-image registry.odigos.io/odigos-demo-frontend:v0.2
         - apply:
             file: ../../common/apply/install-simple-demo.yaml
@@ -90,12 +90,8 @@ spec:
 
     - name: Instrument Namespace
       try:
-      # Using the deprecated way of instrumenting a namespace,
-      # after v1.0.144, use ../../common/apply/instrument-default-ns.yaml
-        - script:
-            content: kubectl label namespace default odigos-instrumentation=enabled
-        # - apply:
-        #     file: ../../common/apply/instrument-default-ns.yaml
+        - apply:
+            file: ../../common/apply/instrument-default-ns.yaml
 
     - name: Assert Runtime Detected
       try:

tests/e2e/helm-chart/chainsaw-test.yaml
@@ -76,10 +76,12 @@ spec:
                 docker pull registry.odigos.io/odigos-demo-inventory:v0.1
                 docker pull registry.odigos.io/odigos-demo-membership:v0.1
                 docker pull registry.odigos.io/odigos-demo-coupon:v0.1
+                docker pull registry.odigos.io/odigos-demo-pricing:v0.1
                 docker pull registry.odigos.io/odigos-demo-frontend:v0.2
                 kind load docker-image registry.odigos.io/odigos-demo-inventory:v0.1
                 kind load docker-image registry.odigos.io/odigos-demo-membership:v0.1
                 kind load docker-image registry.odigos.io/odigos-demo-coupon:v0.1
+                kind load docker-image registry.odigos.io/odigos-demo-pricing:v0.1
                 kind load docker-image registry.odigos.io/odigos-demo-frontend:v0.2
               else
                 echo "Skipping docker pull and kind load for cross-cloud-tests mode"
@@ -122,7 +124,7 @@ spec:
     - name: Simple-demo instrumented after destination added
       try:
         - assert:
-            file: ../../common/assert/simple-demo-instrumented.yaml
+            file: ../../common/assert/simple-demo-instrumented-full.yaml
 
     - name: Generate Traffic
       try:

tests/e2e/multi-apps/chainsaw-test.yaml
@@ -96,7 +96,7 @@ spec:
     - name: Simple-demo instrumented after destination added
       try:
         - assert:
-            file: ../../common/assert/simple-demo-instrumented.yaml
+            file: ../../common/assert/simple-demo-instrumented-full.yaml
 
     - name: Generate Traffic
       try:

tests/e2e/source/chainsaw-test.yaml
@@ -46,12 +46,12 @@ spec:
                 docker pull registry.odigos.io/odigos-demo-inventory:v0.1
                 docker pull registry.odigos.io/odigos-demo-membership:v0.1
                 docker pull registry.odigos.io/odigos-demo-coupon:v0.1
-                docker pull registry.odigos.io/odigos-demo-inventory:v0.1
+                docker pull registry.odigos.io/odigos-demo-pricing:v0.1
                 docker pull registry.odigos.io/odigos-demo-frontend:v0.2
                 kind load docker-image registry.odigos.io/odigos-demo-inventory:v0.1
                 kind load docker-image registry.odigos.io/odigos-demo-membership:v0.1
                 kind load docker-image registry.odigos.io/odigos-demo-coupon:v0.1
-                kind load docker-image registry.odigos.io/odigos-demo-inventory:v0.1
+                kind load docker-image registry.odigos.io/odigos-demo-pricing:v0.1
                 kind load docker-image registry.odigos.io/odigos-demo-frontend:v0.2
               else
                 echo "Skipping docker pull and kind load for cross-cloud-tests mode"
@@ -93,7 +93,7 @@ spec:
     - name: Simple-demo instrumented after destination added
       try:
         - assert:
-            file: ../../common/assert/simple-demo-instrumented.yaml
+            file: ../../common/assert/simple-demo-instrumented-full.yaml
         - assert:
             timeout: 2m
             file: 01-workloads.yaml
@@ -208,7 +208,7 @@ spec:
     - name: Simple demo instrumented after runtime detection
       try:
         - assert:
-            file: ../../common/assert/simple-demo-instrumented.yaml
+            file: ../../common/assert/simple-demo-instrumented-full.yaml
         - script:
             timeout: 70s
             content: ./wait_for_rollout.sh