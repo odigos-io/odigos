28e9c80d88bd24346b00ebb665322c15d7c06cd9
ErikNaz-odigos <erik@odigos.io>
2025-03-11 16:50:18 +0200

    Bugfix/odigos instrumentor leader election error (#2557)
    ## Description
    On large and heavy cluster we've noticed some timeout and general
    instability with the Instrumentor leader election mechanism.
    
    The new parameters that are chosen are closed to what real HA
    applications use in production environments
    
    ---------
    
    Co-authored-by: Mike Dame <mike@odigos.io>
    Co-authored-by: Amir Blum <amirgiraffe@gmail.com>

--- instrumentor/controllers/manager.go
+++ instrumentor/controllers/manager.go
@@ -92,28 +92,30 @@ func CreateManager(opts KubeManagerOptions) (ctrl.Manager, error) {
 		/*
 			Leader Election Parameters:
 
-			LeaseDuration (5s):
+			LeaseDuration (30s):
 			- Maximum time a pod can remain the leader after its last successful renewal.
 			- If the leader pod dies, failover can take up to the LeaseDuration from the last renewal.
 			  The actual failover time depends on how recently the leader renewed the lease.
 			- Controls when the lease is fully expired and failover can occur.
 
-			RenewDeadline (4s):
+			RenewDeadline (20s):
 			- The maximum time the leader pod has to successfully renew its lease before it is
 			  considered unhealthy. Relevant only while the leader is alive and renewing.
 			- Controls how long the current leader will keep retrying to refresh the lease.
 
-			RetryPeriod (1s):
+			RetryPeriod (5s):
 			- How often non-leader pods check and attempt to acquire leadership when the lease is available.
 			- Lower value means faster failover but adds more load on the Kubernetes API server.
 
 			Relationship:
 			- RetryPeriod < RenewDeadline < LeaseDuration
 			- This ensures proper failover timing and system stability.
+
+			Setting the leader election params to 30s/20s/5s should provide a good balance between stability and quick failover.
 		*/
-		LeaseDuration: durationPointer(5 * time.Second),
-		RenewDeadline: durationPointer(4 * time.Second),
-		RetryPeriod:   durationPointer(1 * time.Second),
+		LeaseDuration: durationPointer(30 * time.Second),
+		RenewDeadline: durationPointer(20 * time.Second),
+		RetryPeriod:   durationPointer(5 * time.Second),
 		Cache: cache.Options{
 			DefaultTransform: cache.TransformStripManagedFields(),
 			ByObject: map[client.Object]cache.ByObject{
