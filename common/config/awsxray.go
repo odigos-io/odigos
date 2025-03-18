a372c027422f1c1c41d2a128873c83cce728e328
Ben Elferink <ben.elferink@icloud.com>
2025-03-18 15:30:37 +0200

    feat: add more form fields to aws x-ray

--- common/config/awsxray.go
+++ common/config/awsxray.go
@@ -1,15 +1,24 @@
 package config
 
 import (
+	"encoding/json"
+	"errors"
+
 	"github.com/odigos-io/odigos/common"
 )
 
 const (
-	AWS_XRAY_REGION        = "AWS_XRAY_REGION"
-	AWS_XRAY_ENDPOINT      = "AWS_XRAY_ENDPOINT"
-	AWS_XRAY_PROXY_ADDRESS = "AWS_XRAY_PROXY_ADDRESS"
-	AWS_XRAY_DISABLE_SSL   = "AWS_XRAY_DISABLE_SSL"
-	AWS_XRAY_LOCAL_MODE    = "AWS_XRAY_LOCAL_MODE"
+	AWS_XRAY_REGION               = "AWS_XRAY_REGION"
+	AWS_XRAY_ENDPOINT             = "AWS_XRAY_ENDPOINT"
+	AWS_XRAY_PROXY_ADDRESS        = "AWS_XRAY_PROXY_ADDRESS"
+	AWS_XRAY_DISABLE_SSL          = "AWS_XRAY_DISABLE_SSL"
+	AWS_XRAY_LOCAL_MODE           = "AWS_XRAY_LOCAL_MODE"
+	AWS_XRAY_RESOURCE_ARN         = "AWS_XRAY_RESOURCE_ARN"
+	AWS_XRAY_ROLE_ARN             = "AWS_XRAY_ROLE_ARN"
+	AWS_XRAY_EXTERNAL_ID          = "AWS_XRAY_EXTERNAL_ID"
+	AWS_XRAY_INDEX_ALL_ATTRIBUTES = "AWS_XRAY_INDEX_ALL_ATTRIBUTES"
+	AWS_XRAY_INDEXED_ATTRIBUTES   = "AWS_XRAY_INDEXED_ATTRIBUTES"
+	AWS_XRAY_LOG_GROUPS           = "AWS_XRAY_LOG_GROUPS"
 )
 
 type AWSXRay struct{}
@@ -49,6 +58,54 @@ func (m *AWSXRay) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) (
 	localMode, exists := config[AWS_XRAY_LOCAL_MODE]
 	if exists {
 		exporterConfig["local_mode"] = parseBool(localMode)
+	}
+
+	resourceArn, exists := config[AWS_XRAY_RESOURCE_ARN]
+	if exists {
+		exporterConfig["resource_arn"] = resourceArn
+	}
+
+	roleArn, exists := config[AWS_XRAY_ROLE_ARN]
+	if exists {
+		exporterConfig["role_arn"] = roleArn
+	}
+
+	externalId, exists := config[AWS_XRAY_EXTERNAL_ID]
+	if exists {
+		exporterConfig["external_id"] = externalId
+	}
+
+	indexAllAttributes, exists := config[AWS_XRAY_INDEX_ALL_ATTRIBUTES]
+	if exists {
+		exporterConfig["index_all_attributes"] = parseBool(indexAllAttributes)
+	}
+
+	indexedAttributes, exists := config[AWS_XRAY_INDEXED_ATTRIBUTES]
+	if exists {
+		var list []string
+
+		err := json.Unmarshal([]byte(indexedAttributes), &list)
+		if err != nil {
+			return nil, errors.Join(err, errors.New(
+				"failed to parse AWS X-Ray destination parameter \"AWS_XRAY_INDEXED_ATTRIBUTES\" as JSON string in the format: string[]",
+			))
+		}
+
+		exporterConfig["indexed_attributes"] = indexedAttributes
+	}
+
+	logGroups, exists := config[AWS_XRAY_LOG_GROUPS]
+	if exists {
+		var list []string
+
+		err := json.Unmarshal([]byte(logGroups), &list)
+		if err != nil {
+			return nil, errors.Join(err, errors.New(
+				"failed to parse AWS X-Ray destination parameter \"AWS_XRAY_LOG_GROUPS\" as JSON string in the format: string[]",
+			))
+		}
+
+		exporterConfig["aws_log_groups"] = list
 	}
 
 	currentConfig.Exporters[exporterName] = exporterConfig
