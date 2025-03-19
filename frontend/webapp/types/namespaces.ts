40af48e6319dbdd1ca70ee1fe15064be79f7303c
Ben Elferink <ben.elferink@icloud.com>
2025-03-19 15:32:48 +0200

    fix: cleanup unused

--- frontend/webapp/types/namespaces.ts
+++ frontend/webapp/types/namespaces.ts
@@ -1,11 +1,3 @@
-import type { Source } from '@odigos/ui-kit/types';
-
-export interface FetchedNamespace {
-  name: string;
-  selected: boolean;
-  sources?: Source[];
-}
-
 export interface NamespaceInstrumentInput {
   name: string;
   futureSelected: boolean;
