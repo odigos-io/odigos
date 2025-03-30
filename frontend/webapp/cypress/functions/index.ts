4b45445bd3c5a82d7036659c7953ccfdeccdf533
Ben Elferink <ben.elferink@icloud.com>
2025-03-30 14:03:36 +0300

    ci: attempt to improve UI tests

--- frontend/webapp/cypress/functions/index.ts
+++ frontend/webapp/cypress/functions/index.ts
@@ -81,13 +81,17 @@ export const updateEntity = ({ nodeId, nodeContains, fieldKey, fieldValue }: Upd
   cy.get(DATA_IDS.DRAWER).should('exist');
   cy.get(DATA_IDS.DRAWER_EDIT).click();
 
-  cy.get(fieldKey).click().focused().clear().type(fieldValue);
-  cy.get(fieldKey).should('have.value', fieldValue);
+  cy.wait(500).then(() => {
+    cy.get(fieldKey).click().focused().clear().type(fieldValue);
+    cy.get(fieldKey).should('have.value', fieldValue);
 
-  cy.get(DATA_IDS.DRAWER_SAVE).click();
-  cy.get(DATA_IDS.DRAWER_CLOSE).click();
+    cy.wait(500).then(() => {
+      cy.get(DATA_IDS.DRAWER_SAVE).click();
+      cy.get(DATA_IDS.DRAWER_CLOSE).click();
 
-  if (!!callback) callback();
+      if (!!callback) callback();
+    });
+  });
 };
 
 interface DeleteEntityOptions {
