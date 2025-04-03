499ec5a5ea6c9fdb7929857d40cf697d0a5143c5
Ben Elferink <ben.elferink@icloud.com>
2025-04-03 14:16:07 +0300

    feat: add VictoriaMetrics destination support

---
+++ common/config/victoriametrics.go
@@ -0,0 +1,64 @@
+package config
+
+import (
+	"github.com/odigos-io/odigos/common"
+)
+
+const (
+	VICTORIA_METRICS_ENDPOINT = "VICTORIA_METRICS_ENDPOINT"
+	VICTORIA_METRICS_API_KEY  = "VICTORIA_METRICS_API_KEY"
+)
+
+type VictoriaMetrics struct{}
+
+func (j *VictoriaMetrics) DestType() common.DestinationType {
+	return common.VictoriaMetricsDestinationType
+}
+
+func (j *VictoriaMetrics) ModifyConfig(dest ExporterConfigurer, cfg *Config) ([]string, error) {
+	config := dest.GetConfig()
+	uniqueUri := "victoriametrics-" + dest.GetID()
+	var pipelineNames []string
+
+	endpoint, exists := config[VICTORIA_METRICS_ENDPOINT]
+	if !exists {
+		return nil, errorMissingKey(VICTORIA_METRICS_ENDPOINT)
+	}
+	endpoint, err := parseOtlpHttpEndpoint(endpoint, "", "/opentelemetry")
+	if err != nil {
+		return nil, err
+	}
+
+	authExtensionName := "bearertokenauth/" + uniqueUri
+	cfg.Extensions[authExtensionName] = GenericMap{
+		"token": "${VICTORIA_METRICS_API_KEY}",
+	}
+
+	exporterName := "otlphttp/" + uniqueUri
+	cfg.Exporters[exporterName] = GenericMap{
+		"endpoint": endpoint,
+		"auth": GenericMap{
+			"authenticator": authExtensionName,
+		},
+	}
+
+	cfg.Service.Extensions = append(cfg.Service.Extensions, authExtensionName)
+
+	if isMetricsEnabled(dest) {
+		pipeName := "metrics/" + uniqueUri
+		cfg.Service.Pipelines[pipeName] = Pipeline{
+			Exporters: []string{exporterName},
+		}
+		pipelineNames = append(pipelineNames, pipeName)
+	}
+
+	if isLoggingEnabled(dest) {
+		pipeName := "logs/" + uniqueUri
+		cfg.Service.Pipelines[pipeName] = Pipeline{
+			Exporters: []string{exporterName},
+		}
+		pipelineNames = append(pipelineNames, pipeName)
+	}
+
+	return pipelineNames, nil
+}
