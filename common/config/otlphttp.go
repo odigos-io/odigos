cbded55b5a62802eeac3a81ca40de7e33a13fd19
Avihu Hanya <avihuhenya@gmail.com>
2025-04-20 12:55:32 +0300

    add optional headers field

--- common/config/otlphttp.go
+++ common/config/otlphttp.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 
@@ -11,6 +12,7 @@ const (
 	otlpHttpEndpointKey          = "OTLP_HTTP_ENDPOINT"
 	otlpHttpBasicAuthUsernameKey = "OTLP_HTTP_BASIC_AUTH_USERNAME"
 	otlpHttpBasicAuthPasswordKey = "OTLP_HTTP_BASIC_AUTH_PASSWORD"
+	otlpHttpHeaders              = "OTLP_HTTP_HEADERS"
 )
 
 type OTLPHttp struct{}
@@ -20,7 +22,9 @@ func (g *OTLPHttp) DestType() common.DestinationType {
 }
 
 func (g *OTLPHttp) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) ([]string, error) {
-	url, exists := dest.GetConfig()[otlpHttpEndpointKey]
+	config := dest.GetConfig()
+
+	url, exists := config[otlpHttpEndpointKey]
 	if !exists {
 		return nil, errors.New("OTLP http endpoint not specified, gateway will not be configured for otlp http")
 	}
@@ -47,7 +51,26 @@ func (g *OTLPHttp) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) 
 			"authenticator": basicAuthExtensionName,
 		}
 	}
+	headers, exists := config[otlpHttpHeaders]
+	if exists {
+		var headersList []struct {
+			Key   string `json:"key"`
+			Value string `json:"value"`
+		}
+		err := json.Unmarshal([]byte(headers), &headersList)
+		if err != nil {
+			return nil, errors.Join(err, errors.New(
+				"failed to parse otlphttp destination OTLP_HTTP_HEADERS parameter as json string in the form {key: string, value: string}[]",
+			))
+		}
+		mappedHeaders := map[string]string{}
+		for _, header := range headersList {
+			mappedHeaders[header.Key] = header.Value
+		}
+		exporterConf["headers"] = mappedHeaders
+	}
 	currentConfig.Exporters[otlpHttpExporterName] = exporterConf
+
 	var pipelineNames []string
 	if isTracingEnabled(dest) {
 		tracesPipelineName := "traces/otlphttp-" + dest.GetID()
