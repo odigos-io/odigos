b2ce87108ecd980776ccc7d39968d5545dd133ac
Ben Elferink <ben.elferink@icloud.com>
2025-04-23 13:42:13 +0300

    ci: generate traffic with verbose=true in cli-upgrade

--- e2e-test.sh
+++ e2e-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-kind create cluster || { echo "Error: Failed to create Kind cluster"; exit 1; }
+kind create cluster || {
+  echo "Error: Failed to create Kind cluster"
+  exit 1
+}
 
 # Build CLI
 go build -tags=embed_manifests -o odigos-e2e-test cli/main.go
@@ -14,8 +17,8 @@ TAG=e2e-test make build-images load-to-kind
 # Install Collector - Add Dependencies
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 
-if [ ! -d "opentelemetry-helm-charts" ]; then \
-    git clone https://github.com/open-telemetry/opentelemetry-helm-charts.git; \
+if [ ! -d "opentelemetry-helm-charts" ]; then
+  git clone https://github.com/open-telemetry/opentelemetry-helm-charts.git
 fi
 
 # Install Collector
@@ -40,28 +43,25 @@ kubectl label namespace kvshop odigos-instrumentation=enabled
 kubectl create -f .github/workflows/e2e/jaeger-dest.yaml
 
 # Wait for Odigos to bring up collectors
-while [[ $(kubectl get daemonset odigos-data-collection -n odigos-system -o jsonpath='{.status.numberReady}') != 1 ]];
-do
-  echo "Waiting for odigos-data-collection daemonset to be created" && sleep 3;
+while [[ $(kubectl get daemonset odigos-data-collection -n odigos-system -o jsonpath='{.status.numberReady}') != 1 ]]; do
+  echo "Waiting for odigos-data-collection daemonset to be created" && sleep 3
 done
-while [[ $(kubectl get deployment odigos-gateway -n odigos-system -o jsonpath='{.status.readyReplicas}') != 1 ]];
-do
-  echo "Waiting for odigos-data-collection deployment to be created" && sleep 3;
+while [[ $(kubectl get deployment odigos-gateway -n odigos-system -o jsonpath='{.status.readyReplicas}') != 1 ]]; do
+  echo "Waiting for odigos-data-collection deployment to be created" && sleep 3
 done
-while [[ $(kubectl get pods -n kvshop | grep Running | wc -l) -ne 5 ]];
-do
-  echo "Waiting for kvshop pods to be running" && sleep 3;
+while [[ $(kubectl get pods -n kvshop | grep Running | wc -l) -ne 5 ]]; do
+  echo "Waiting for kvshop pods to be running" && sleep 3
 done
 sleep 10
 kubectl get pods -A
 kubectl get svc -A
 
 # Start bot job
-kubectl create -f .github/workflows/e2e/buybot-job.yaml -n kvshop
+kubectl create -f .github/workflows/e2e/traffic-bot.yaml -n kvshop
 
 # Wait for bot job to complete
 echo "Waiting for bot job to complete..."
-kubectl wait --for=condition=Complete --timeout=60s job/buybot-job -n kvshop
+kubectl wait --for=condition=Complete --timeout=60s job/traffic-bot -n kvshop
 
 # Copy trace output
 echo "Sleeping for 10 seconds to allow traces to be collected"
