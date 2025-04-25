0355bd2385c2172329a6a22231cdb28dbbb08591
Ron Federman <ron@keyval.dev>
2025-04-25 15:34:28 +0300

    lint and docs update

--- common/odigos_config.go
+++ common/odigos_config.go
@@ -129,6 +129,6 @@ type OdigosConfiguration struct {
 	// to only using the JAVA_TOOL_OPTIONS env var.
 	// When this is true, we will not inject the JAVA_OPTS env var into the container.
 	// when false or not set the original behavior will be used and the JAVA_OPTS env var will be injected.
-	AvoidInjectingJavaOptsEnvVar     *bool                          `json:"avoidInjectingJavaOptsEnvVar,omitempty"`
-	UserInstrumentationEnvs          *UserInstrumentationEnvs       `json:"UserInstrumentationEnvs,omitempty"`
+	AvoidInjectingJavaOptsEnvVar *bool                    `json:"avoidInjectingJavaOptsEnvVar,omitempty"`
+	UserInstrumentationEnvs      *UserInstrumentationEnvs `json:"UserInstrumentationEnvs,omitempty"`
 }
