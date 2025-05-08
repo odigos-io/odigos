264e7a7d6aea6f4a0ae496907bb1ebdb2bde0b88
Mike Dame <mike@odigos.io>
2025-05-08 11:28:16 -0400

    destination: Add Splunk OTLPHTTP exporter

--- common/config/splunk.go
+++ common/config/splunk.go
@@ -8,16 +8,22 @@ import (
 )
 
 const (
-	splunkRealm = "SPLUNK_REALM"
+	splunkRealm                  = "SPLUNK_REALM"
+	splunkOtlpTlsKey             = "SPLUNK_OTLP_TLS_ENABLED"
+	splunkOtlpCaPemKey           = "SPLUNK_OTLP_CA_PEM"
+	splunkOtlpInsecureSkipVerify = "SPLUNK_OTLP_INSECURE_SKIP_VERIFY"
+	splunkOtlpCompression        = "SPLUNK_OTLP_COMPRESSION"
 )
 
-type Splunk struct{}
+// SplunkSAPM configures the SAPM collector exporter.
+// Deprecated: This exporter is deprecated upstream in favor of the OTLPHTTP exporter.
+type SplunkSAPM struct{}
 
-func (s *Splunk) DestType() common.DestinationType {
-	return common.SplunkDestinationType
+func (s *SplunkSAPM) DestType() common.DestinationType {
+	return common.SplunkSAPMDestinationType
 }
 
-func (s *Splunk) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) ([]string, error) {
+func (s *SplunkSAPM) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) ([]string, error) {
 	realm, exists := dest.GetConfig()[splunkRealm]
 	if !exists {
 		return nil, errors.New("Splunk realm not specified, gateway will not be configured for Splunk")
@@ -31,6 +37,63 @@ func (s *Splunk) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) ([
 		}
 
 		tracesPipelineName := "traces/splunk-" + dest.GetID()
+		currentConfig.Service.Pipelines[tracesPipelineName] = Pipeline{
+			Exporters: []string{exporterName},
+		}
+		pipelineNames = append(pipelineNames, tracesPipelineName)
+	}
+
+	return pipelineNames, nil
+}
+
+// SplunkOTLP configures an OTLPHTTP exporter configured for Splunk ingestion.
+type SplunkOTLP struct{}
+
+func (s *SplunkOTLP) DestType() common.DestinationType {
+	return common.SplunkOTLPDestinationType
+}
+
+func (s *SplunkOTLP) ModifyConfig(dest ExporterConfigurer, currentConfig *Config) ([]string, error) {
+	config := dest.GetConfig()
+
+	realm, exists := config[splunkRealm]
+	if !exists {
+		return nil, errors.New("Splunk realm not specified, gateway will not be configured for Splunk")
+	}
+
+	tls := config[splunkOtlpTlsKey]
+	tlsEnabled := tls == "true"
+	tlsConfig := GenericMap{
+		"insecure": !tlsEnabled,
+	}
+	caPem, caExists := config[splunkOtlpCaPemKey]
+	if caExists && caPem != "" {
+		tlsConfig["ca_pem"] = caPem
+	}
+	insecureSkipVerify, skipExists := config[splunkOtlpInsecureSkipVerify]
+	if skipExists && insecureSkipVerify != "" {
+		tlsConfig["insecure_skip_verify"] = parseBool(insecureSkipVerify)
+	}
+
+	var pipelineNames []string
+	if isTracingEnabled(dest) {
+		exporterName := "otlphttp/" + dest.GetID()
+		exporterConf := GenericMap{
+			"headers": map[string]string{
+				"X-SF-Token": "${SPLUNK_ACCESS_TOKEN}",
+			},
+			"traces_endpoint": fmt.Sprintf("https://ingest.%s.signalfx.com/v2/trace/otlp", realm),
+		}
+		if compression, ok := config[splunkOtlpCompression]; ok {
+			exporterConf["compression"] = compression
+		}
+		if tlsEnabled {
+			exporterConf["tls"] = tlsConfig
+		}
+
+		currentConfig.Exporters[exporterName] = exporterConf
+
+		tracesPipelineName := "traces/splunkotlp-" + dest.GetID()
 		currentConfig.Service.Pipelines[tracesPipelineName] = Pipeline{
 			Exporters: []string{exporterName},
 		}
