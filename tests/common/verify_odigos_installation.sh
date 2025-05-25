0f5884c3e1a45e6279088bbc3e645c9558b37c9b
Ron Federman <ron@odigos.io>
2025-05-25 14:48:14 +0300

    adjust uninstall test

--- tests/common/verify_odigos_installation.sh
+++ tests/common/verify_odigos_installation.sh
@@ -31,6 +31,12 @@ check_command "Odigos UI deployment created"
 kubectl wait --for=create daemonset/odiglet -n $NAMESPACE --timeout=$TIMEOUT
 check_command "Odiglet DaemonSet created"
 
+# Wait for pods to be created first
+until kubectl get pods -n $NAMESPACE 2>/dev/null | grep -q .; do
+    sleep 2
+done
+
+# Now wait for pods to be ready
 kubectl wait --for=condition=ready pods --all -n $NAMESPACE --timeout=$TIMEOUT
 check_command "All pods are ready"
 
