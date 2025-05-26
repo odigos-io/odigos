bba908297d06faf4e41979a1d7f5f3751c48f98d
tamirdavid1 <tamir@odigos.io>
2025-05-26 15:09:46 +0300

    chore: remove re-declaration

--- common/consts/consts.go
+++ common/consts/consts.go
@@ -28,9 +28,6 @@ const (
 	// Deprecated: Sources are used to mark workloads for instrumentation.
 	InstrumentationDisabled = "disabled"
 
-	// DefaultDataStream is the default data stream name used for telemetry data.
-	DefaultDataStream = "default"
-
 	// Deprecated: reported name is set via the Source CR.
 	OdigosReportedNameAnnotation = "odigos.io/reported-name"
 	RolloutTriggerAnnotation     = "rollout-trigger"
