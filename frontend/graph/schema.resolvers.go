81a9b26876eccbc54da41aff6a3ecaaaa5bc51f3
Ben Elferink <ben.elferink@icloud.com>
2025-06-11 15:54:41 +0300

    chore: cleanup

--- frontend/graph/schema.resolvers.go
+++ frontend/graph/schema.resolvers.go
@@ -1225,15 +1225,3 @@ type computePlatformResolver struct{ *Resolver }
 type k8sActualNamespaceResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func (r *mutationResolver) RestartWorkload(ctx context.Context, sourceID model.K8sSourceID) (bool, error) {
-
-}
-*/
