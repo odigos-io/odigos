d258bae8905decaf14b7f3043c42c92e817ad7bd
Mike Dame <mike@odigos.io>
2025-06-25 10:05:27 -0400

    Add disable-gin profile (#3063)
    ## Description
    
    This adds a profile to disable `gin-gonic/gin` instrumentation for its
    apparent redundance with some net/http spans. It adds this profile to
    the `kratos` aggregator
    
    ## How Has This Been Tested?
    
    <!-- Describe the tests you ran and how you verified your changes. -->
    
    - [ ] Added Unit Tests
    - [ ] Updated e2e Tests
    - [x] Manual Testing
    - [ ] Manual Load Test
    
    ## Kubernetes Checklist
    
    <!-- If this PR affects how Odigos interacts with Kubernetes, check the
    relevant boxes below and provide more details -->
    
    - [ ] Changes how Odigos interacts with Kubernetes
    - [ ] Introduces additional calls to the API Server (potential
    performance impact)
    - [ ] New Query/feature supported in all the k8s versions supported by
    Odigos
    - [ ] Modifies Odigos manifests (addressed in both CLI and Helm)
    - [ ] Changes RBAC permissions
    
    ## User Facing Changes
    
    <!-- Any changes that users will notice or need to be aware of -->
    
    - [ ] Users need to take action before upgrading
    - [ ] Automatic migration will modify existing objects (backward
    compatible)
    - [ ] Changes UI, CLI, or K8s Manifests aspects in a way that users need
    to be aware of
    - [ ] Documentation updated accordingly

---
+++ api/odigos/v1alpha1/instrumentationrules/trace_config.go
@@ -0,0 +1,8 @@
+package instrumentationrules
+
+// +kubebuilder:object:generate=true
+// +kubebuilder:deepcopy-gen=true
+type TraceConfig struct {
+	// Disabled will disable tracing for the rule.
+	Disabled *bool `json:"disabled,omitempty"`
+}
