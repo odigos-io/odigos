2fcdcaee1adb640bc37f517526445a489e55e580
Amir Blum <amirgiraffe@gmail.com>
2025-06-25 05:53:23 +0300

    chore: doc comment improvment

--- distros/distro/appendenvs.go
+++ distros/distro/appendenvs.go
@@ -8,7 +8,9 @@ import "github.com/odigos-io/odigos/common/consts"
 // for further patching downstream.
 func GetAppendEnvVarNames(distros []*OtelDistro) map[string]struct{} {
 	appendEnvVarNames := map[string]struct{}{
-		consts.LdPreloadEnvVarName: {}, // LD_PRELOAD is special, and is always collected for any distro with special handling
+		// LD_PRELOAD is special, and is always collected.
+		// It has special handling that does not require it to be set in the "AppendOdigosVariables" list.
+		consts.LdPreloadEnvVarName: {},
 	}
 	for _, distro := range distros {
 		for _, envVar := range distro.EnvironmentVariables.AppendOdigosVariables {
