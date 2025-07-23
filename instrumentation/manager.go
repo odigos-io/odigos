5120a5e7f4ffda41d7915b01d9d85b6084abd8df
david <david@odigos.io>
2025-07-23 11:44:55 +0300

    debug

--- instrumentation/manager.go
+++ instrumentation/manager.go
@@ -424,7 +424,7 @@ func (m *manager[ProcessDetails, ConfigGroup]) applyInstrumentationConfiguration
 			continue
 		}
 		m.logger.Info("applying configuration to instrumentation", "process group details", instDetails.pd, "configGroup", configGroup)
-		fmt.Printf("@@ applying configuration to instrumentation %T for process group details %v with config group %v\n", instDetails.inst, instDetails.pd, configGroup)
+		fmt.Printf("@@ applying configuration to instrumentation %T for process group details %v with config group %v config[%v]\n", instDetails.inst, instDetails.pd, configGroup, config)
 		applyErr := instDetails.inst.ApplyConfig(ctx, config)
 		err = errors.Join(err, applyErr)
 	}
