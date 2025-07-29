c85fbc3253b06743fa94296d9217c5a5bf10d93d
tamirdavid1 <tamir@odigos.io>
2025-07-29 08:09:34 +0300

    chore: remove comments

--- tests/e2e/workload-lifecycle/services/python-gunicorn-server/script.sh
+++ tests/e2e/workload-lifecycle/services/python-gunicorn-server/script.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# export ENV_IN_SCRIPT=ENV_IN_SCRIPT_VALUE
-
-# echo "ENV_IN_SCRIPT is: $ENV_IN_SCRIPT"
 python3 script.py
 
 exec  /app/venv/bin/gunicorn -w 5 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:8000
-
-
-# /app/venv/bin/newrelic-admin run-program
