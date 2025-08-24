a3123442963f956d917acbf62945374e88464ea5
Ben Elferink <ben@odigos.io>
2025-08-24 17:39:22 +0300

    refactor: remove debug logging from useTraces in trace view
    - Eliminated console.log statement for traces in the Page component to clean up the code and improve performance.
    - This change enhances the overall readability and maintainability of the trace view component.

--- frontend/webapp/app/(overview)/trace-view/page.tsx
+++ frontend/webapp/app/(overview)/trace-view/page.tsx
@@ -7,7 +7,6 @@ import { HEADER_HEIGHT, MENU_BAR_HEIGHT } from '@/utils';
 
 export default function Page() {
   const { traces } = useTraces({ serviceName: undefined });
-  console.log('useTraces', traces);
 
   return (
     <>
