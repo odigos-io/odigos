ccc487a51c4287a7319edb5ea1c7a6004d9e7a3f
Ben Elferink <ben@odigos.io>
2025-09-02 13:28:22 +0300

    fix: update input value check in onboarding test
    - Changed the assertion for the input field in the onboarding test to check for the default value instead of just existence. This improves the accuracy of the test by ensuring the input has the expected initial value.

--- frontend/webapp/cypress/e2e/02-onboarding.cy.ts
+++ frontend/webapp/cypress/e2e/02-onboarding.cy.ts
@@ -32,8 +32,7 @@ describe('Onboarding', () => {
 
   it('Should allow the user to pass every step, and end-up on the "overview" page.', () => {
     visitPage(ROUTES.CHOOSE_STREAM, () => {
-      cy.get('input').should('exist').type('default');
-      cy.contains('button', BUTTONS.BACK).should('exist');
+      cy.get('input').should('exist').should('have.value', 'default');
       cy.contains('button', BUTTONS.NEXT).should('exist').click();
 
       cy.location('pathname').should('eq', ROUTES.CHOOSE_SOURCES);
