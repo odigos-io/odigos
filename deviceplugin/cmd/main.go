e60d9d7f9c3a16c52b04bfd8de1fbfd8f1b7abc6
Dan Sela <dansela@Dans-MacBook-Pro---Odigos-4.local>
2025-09-04 16:04:19 +0300

    removing dotnet legacy device

--- deviceplugin/cmd/main.go
+++ deviceplugin/cmd/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"os"
 
-	"github.com/odigos-io/odigos/common"
 	"github.com/odigos-io/odigos/deviceplugin/pkg"
-	"github.com/odigos-io/odigos/deviceplugin/pkg/instrumentation"
 	"github.com/odigos-io/odigos/deviceplugin/pkg/log"
 	"github.com/odigos-io/odigos/k8sutils/pkg/env"
 )
@@ -21,17 +19,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	dp := pkg.New(pkg.Options{
-		DeviceInjectionCallbacks: deviceInjectionCallbacks(),
-	})
+	dp := pkg.New()
 
 	if err := dp.Run(); err != nil {
 		log.Logger.Error(err, "Device plugin exited with error")
 	}
-}
-
-func deviceInjectionCallbacks() instrumentation.OtelSdksLsf {
-	// still being used in enterprise.
-	// will be removed once dotnet-legacy is removed and we stay with only "generic" device.
-	return map[common.ProgrammingLanguage]map[common.OtelSdk]instrumentation.LangSpecificFunc{}
 }
