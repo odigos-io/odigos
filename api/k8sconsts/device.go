fc690fd546e57e2b4afea9a4559efe8b8b9a34ae
Amir Blum <amirgiraffe@gmail.com>
2025-09-12 10:18:53 +0300

    chore: remove unused consts

--- api/k8sconsts/device.go
+++ api/k8sconsts/device.go
@@ -8,12 +8,4 @@ const (
 	// GetResourceNamespace must return namespace (vendor ID) of implemented Lister. e.g. for
 	// resources in format "color.example.com/<color>" that would be "color.example.com".
 	OdigosResourceNamespace = "instrumentation.odigos.io"
-
-	OdigosGenericDeviceName = "generic"
-
-	// the name of the device that only mounts the odigos agents root directory,
-	// allowing any agent to be access it's files.
-	// it would be more ideal to only mount what is needed,
-	// but it's not desirable to have tons of different devices for each case.
-	OdigosGenericDeviceNameFull = OdigosResourceNamespace + "/" + OdigosGenericDeviceName
 )
