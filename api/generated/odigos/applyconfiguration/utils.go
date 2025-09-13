765548752a4445faa52e2c9ecc40bc01fbb5d2fc
Amir Blum <amirgiraffe@gmail.com>
2025-09-13 21:47:10 +0300

    chore: regenerate the api

--- api/generated/odigos/applyconfiguration/utils.go
+++ api/generated/odigos/applyconfiguration/utils.go
@@ -23,7 +23,7 @@ import (
 	v1alpha1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
-	testing "k8s.io/client-go/testing"
+	managedfields "k8s.io/apimachinery/pkg/util/managedfields"
 )
 
 // ForKind returns an apply configuration type for the given GroupVersionKind, or nil if no
@@ -116,6 +116,6 @@ func ForKind(kind schema.GroupVersionKind) interface{} {
 	return nil
 }
 
-func NewTypeConverter(scheme *runtime.Scheme) *testing.TypeConverter {
-	return &testing.TypeConverter{Scheme: scheme, TypeResolver: internal.Parser()}
+func NewTypeConverter(scheme *runtime.Scheme) managedfields.TypeConverter {
+	return managedfields.NewSchemeTypeConverter(scheme, internal.Parser())
 }
