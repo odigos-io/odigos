a8b1db0f80b2af4522bc0fd895cba60175f03a8e
Ben Elferink <ben@odigos.io>
2025-09-15 15:03:00 +0300

    refactor: simplify HTTP origin configuration in routes
    This commit updates the HTTP origin configuration in the routes file by removing environment-based conditions and directly setting the backend port to 8085. This change streamlines the code and ensures a consistent local development setup.

--- frontend/webapp/utils/constants/routes.tsx
+++ frontend/webapp/utils/constants/routes.tsx
@@ -14,13 +14,10 @@ export const ROUTES = {
 
 export const SKIP_TO_SUMMERY_QUERY_PARAM = 'skipToSummary';
 
-const HAS_WINDOW = typeof window !== 'undefined';
-const IS_PROD = process.env.NODE_ENV === 'production';
-const PORT = process.env.PORT || 8085;
-
-const HTTP_ORIGIN = IS_PROD && HAS_WINDOW ? `http://${window.location.hostname}` : `http://localhost:${PORT}`;
+const BACKEND_PORT = 8085;
+const BACKEND_HTTP_ORIGIN = `http://localhost:${BACKEND_PORT}`;
 
 export const API = {
-  GRAPHQL: `${HTTP_ORIGIN}/graphql`,
-  EVENTS: `${HTTP_ORIGIN}/api/events`,
+  GRAPHQL: `${BACKEND_HTTP_ORIGIN}/graphql`,
+  EVENTS: `${BACKEND_HTTP_ORIGIN}/api/events`,
 };
