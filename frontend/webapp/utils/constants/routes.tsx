a53eaeda111b730794f58c8675cef5475b40e7e1
Ben Elferink <ben@odigos.io>
2025-09-16 13:52:33 +0300

    refactor: enhance backend HTTP origin configuration to use dynamic protocol
    This commit updates the backend HTTP origin setup in the routes file to dynamically include the protocol based on the current window location. This change improves the flexibility of the routing logic, ensuring that the application correctly configures the backend URL for both production and local development environments.

--- frontend/webapp/utils/constants/routes.tsx
+++ frontend/webapp/utils/constants/routes.tsx
@@ -14,11 +14,12 @@ export const ROUTES = {
 
 export const SKIP_TO_SUMMERY_QUERY_PARAM = 'skipToSummary';
 
+const PROTOCOL = typeof window !== 'undefined' ? window.location.protocol : 'http';
 const HOSTNAME = typeof window !== 'undefined' ? window.location.hostname : '';
 const IS_INGRESSED_DOMAIN = HOSTNAME && HOSTNAME !== 'localhost';
 
 const BACKEND_PORT = process.env.PORT || 8085;
-const BACKEND_HTTP_ORIGIN = IS_INGRESSED_DOMAIN ? `http://${HOSTNAME}` : `http://localhost:${BACKEND_PORT}`;
+const BACKEND_HTTP_ORIGIN = IS_INGRESSED_DOMAIN ? `${PROTOCOL}//${HOSTNAME}` : `http://localhost:${BACKEND_PORT}`;
 
 export const API = {
   GRAPHQL: `${BACKEND_HTTP_ORIGIN}/graphql`,
