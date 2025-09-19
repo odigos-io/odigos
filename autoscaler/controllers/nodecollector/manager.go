66d8aeff530b01d9f4b268d44f715750a5543ebc
Ron Federman <ron@odigos.io>
2025-09-19 18:23:18 +0300

    add autoscaler-deployment controller to react to one create event and own the node collector config map

--- autoscaler/controllers/nodecollector/manager.go
+++ autoscaler/controllers/nodecollector/manager.go
@@ -1,8 +1,10 @@
 package nodecollector
 
 import (
+	"github.com/odigos-io/odigos/api/k8sconsts"
 	odigosv1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
 	odigospredicate "github.com/odigos-io/odigos/k8sutils/pkg/predicate"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
@@ -14,7 +16,6 @@ func SetupWithManager(mgr ctrl.Manager) error {
 		ControllerManagedBy(mgr).
 		Named("nodecollector-collectorsgroup").
 		For(&odigosv1.CollectorsGroup{}).
-		Owns(&corev1.ConfigMap{}). // in case the configmap of the node-collector is deleted or modified for any reason, this will reconcile and recreate it
 		// we assume everything in the collectorsgroup spec is the configuration for the collectors to generate.
 		// thus, we need to monitor any change to the spec which is what the generation field is for.
 		WithEventFilter(
@@ -24,8 +25,34 @@ func SetupWithManager(mgr ctrl.Manager) error {
 			)).
 		Complete(&CollectorsGroupReconciler{
 			nodeCollectorBaseReconciler: nodeCollectorBaseReconciler{
-				Client:           mgr.GetClient(),
-				scheme:           mgr.GetScheme(),
+				Client: mgr.GetClient(),
+				scheme: mgr.GetScheme(),
+			},
+		})
+	if err != nil {
+		return err
+	}
+
+	err = builder.
+		ControllerManagedBy(mgr).
+		Named("nodecollector-autosacler-deployment").
+		For(&appsv1.Deployment{}).
+		Owns(&corev1.ConfigMap{}). // in case the configmap of the node-collector is deleted or modified for any reason, this will reconcile it
+		// this controller is supposed to react to:
+		// 1. 	creation of the autoscaler deployment -
+		// 	 	single event in the lifecycle of this pod which is part of the autoscaler deployment (one event on start up).
+		//      if no sources or destinations are defined this will create a no-op config map upon a fresh install.
+		// 2. 	changes to the node collector config map - which is owned by the autoscaler deployment -
+		// 		if for any reason the config map is modified or deleted, this will reconcile it.
+		WithEventFilter(
+			predicate.Or(
+				predicate.And(&odigospredicate.ObjectNamePredicate{AllowedObjectName: k8sconsts.AutoScalerDeploymentName}, &odigospredicate.CreationPredicate{}),
+				predicate.And(&odigospredicate.ObjectNamePredicate{AllowedObjectName: k8sconsts.OdigosNodeCollectorConfigMapName}, &odigospredicate.OnlyUpdatesPredicate{}),
+			)).
+		Complete(&AutoscalerDeploymentReconciler{
+			nodeCollectorBaseReconciler: nodeCollectorBaseReconciler{
+				Client: mgr.GetClient(),
+				scheme: mgr.GetScheme(),
 			},
 		})
 	if err != nil {
@@ -41,8 +68,8 @@ func SetupWithManager(mgr ctrl.Manager) error {
 		WithEventFilter(&odigospredicate.ExistencePredicate{}).
 		Complete(&InstrumentationConfigReconciler{
 			nodeCollectorBaseReconciler: nodeCollectorBaseReconciler{
-				Client:           mgr.GetClient(),
-				scheme:           mgr.GetScheme(),
+				Client: mgr.GetClient(),
+				scheme: mgr.GetScheme(),
 			},
 		})
 	if err != nil {
@@ -58,8 +85,8 @@ func SetupWithManager(mgr ctrl.Manager) error {
 		WithEventFilter(&predicate.GenerationChangedPredicate{}).
 		Complete(&ProcessorReconciler{
 			nodeCollectorBaseReconciler: nodeCollectorBaseReconciler{
-				Client:           mgr.GetClient(),
-				scheme:           mgr.GetScheme(),
+				Client: mgr.GetClient(),
+				scheme: mgr.GetScheme(),
 			},
 		})
 	if err != nil {
