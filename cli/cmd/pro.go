e8146adfab842009c6e2c5214463a8a459ac3245
Alon Braymok <138359965+alonkeyval@users.noreply.github.com>
2025-09-28 16:07:38 +0300

    fix: setting the backend url and cluster name in the odigos UI or CLI (#3531)
    Restart central-proxy when central-backend-url or cluster-name is
    updated from the UI or CLI
    
    ---------
    
    Co-authored-by: alonbraymok <alonbraymok007@gmail.com>

--- cli/cmd/pro.go
+++ cli/cmd/pro.go
@@ -11,7 +11,6 @@ import (
 	"os/signal"
 	"sync"
 	"syscall"
-	"time"
 
 	"github.com/odigos-io/odigos/api/k8sconsts"
 	"github.com/odigos-io/odigos/cli/cmd/resources"
@@ -25,12 +24,12 @@ import (
 	"github.com/odigos-io/odigos/common/consts"
 	"github.com/odigos-io/odigos/k8sutils/pkg/installationmethod"
 	"github.com/odigos-io/odigos/k8sutils/pkg/pro"
+	"github.com/odigos-io/odigos/k8sutils/pkg/restart"
 
 	"github.com/spf13/cobra"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 )
 
 var (
@@ -533,19 +532,7 @@ func findPodWithAppLabel(ctx context.Context, client *kube.Client, ns, appLabel 
 }
 
 func restartOdiglet(ctx context.Context, client *kube.Client, ns string) error {
-	// Create patch to add/update the restartedAt annotation
-	patch := fmt.Sprintf(`{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt":"%s"}}}}}`,
-		time.Now().Format(time.RFC3339))
-
-	// Patch the Odiglet daemonset
-	_, err := client.AppsV1().DaemonSets(ns).Patch(
-		ctx,
-		k8sconsts.OdigletDaemonSetName,
-		types.StrategicMergePatchType,
-		[]byte(patch),
-		metav1.PatchOptions{},
-	)
-	return err
+	return restart.RestartDaemonSet(ctx, client.Interface, ns, k8sconsts.OdigletDaemonSetName)
 }
 
 func init() {
