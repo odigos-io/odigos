209e73570e4af526e06a7c91d87d695367d7e2fb
Mike Dame <mike@odigos.io>
2025-10-01 12:29:56 -0400

    CORE-190: Remove memory-intensive k8sattributes processor config

--- instrumentor/controllers/agentenabled/podswebhook/otelresource.go
+++ instrumentor/controllers/agentenabled/podswebhook/otelresource.go
@@ -8,6 +8,7 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const otelServiceNameEnvVarName = "OTEL_SERVICE_NAME"
@@ -18,9 +19,9 @@ type resourceAttribute struct {
 	Value string
 }
 
-func getResourceAttributes(podWorkload k8sconsts.PodWorkload, containerName string) []resourceAttribute {
+func getResourceAttributes(podWorkload k8sconsts.PodWorkload, containerName string, ownerReferences []metav1.OwnerReference) []resourceAttribute {
 	workloadKindKey := getWorkloadKindAttributeKey(podWorkload.Kind)
-	return []resourceAttribute{
+	resourceAttributes := []resourceAttribute{
 		{
 			Key:   semconv.K8SPodNameKey,
 			Value: "$(ODIGOS_POD_NAME)",
@@ -38,6 +39,74 @@ func getResourceAttributes(podWorkload k8sconsts.PodWorkload, containerName stri
 			Value: podWorkload.Name,
 		},
 	}
+	resourceAttributes = append(resourceAttributes, getOwnerReferenceAttributes(ownerReferences)...)
+	return resourceAttributes
+}
+
+func getOwnerReferenceAttributes(ownerReferences []metav1.OwnerReference) []resourceAttribute {
+	resourceAttributes := []resourceAttribute{}
+	for _, ownerReference := range ownerReferences {
+		switch ownerReference.Kind {
+		case "Deployment":
+			resourceAttributes = append(resourceAttributes,
+				resourceAttribute{
+					Key:   semconv.K8SDeploymentNameKey,
+					Value: ownerReference.Name,
+				},
+				resourceAttribute{
+					Key:   semconv.K8SDeploymentUIDKey,
+					Value: string(ownerReference.UID),
+				},
+			)
+		case "StatefulSet":
+			resourceAttributes = append(resourceAttributes,
+				resourceAttribute{
+					Key:   semconv.K8SStatefulSetNameKey,
+					Value: ownerReference.Name,
+				},
+				resourceAttribute{
+					Key:   semconv.K8SStatefulSetUIDKey,
+					Value: string(ownerReference.UID),
+				},
+			)
+		case "DaemonSet":
+			resourceAttributes = append(resourceAttributes,
+				resourceAttribute{
+					Key:   semconv.K8SDaemonSetNameKey,
+					Value: ownerReference.Name,
+				},
+				resourceAttribute{
+					Key:   semconv.K8SDaemonSetUIDKey,
+					Value: string(ownerReference.UID),
+				},
+			)
+		case "Job":
+			resourceAttributes = append(resourceAttributes,
+				resourceAttribute{
+					Key:   semconv.K8SJobNameKey,
+					Value: ownerReference.Name,
+				},
+				resourceAttribute{
+					Key:   semconv.K8SJobUIDKey,
+					Value: string(ownerReference.UID),
+				},
+			)
+		case "ReplicaSet":
+			resourceAttributes = append(resourceAttributes,
+				resourceAttribute{
+					Key:   semconv.K8SReplicaSetNameKey,
+					Value: ownerReference.Name,
+				},
+			)
+			resourceAttributes = append(resourceAttributes,
+				resourceAttribute{
+					Key:   semconv.K8SReplicaSetUIDKey,
+					Value: string(ownerReference.UID),
+				},
+			)
+		}
+	}
+	return resourceAttributes
 }
 
 func getWorkloadKindAttributeKey(workloadKind k8sconsts.WorkloadKind) attribute.Key {
@@ -64,13 +133,13 @@ func getResourceAttributesEnvVarValue(ra []resourceAttribute) string {
 	return strings.Join(attrs, ",")
 }
 
-func InjectOtelResourceAndServiceNameEnvVars(existingEnvNames EnvVarNamesMap, container *corev1.Container, distroName string, pw k8sconsts.PodWorkload, serviceName string) EnvVarNamesMap {
+func InjectOtelResourceAndServiceNameEnvVars(existingEnvNames EnvVarNamesMap, container *corev1.Container, distroName string, pw k8sconsts.PodWorkload, serviceName string, ownerReferences []metav1.OwnerReference) EnvVarNamesMap {
 
 	// OTEL_SERVICE_NAME
 	existingEnvNames = InjectConstEnvVarToPodContainer(existingEnvNames, container, otelServiceNameEnvVarName, serviceName)
 
 	// OTEL_RESOURCE_ATTRIBUTES
-	resourceAttributes := getResourceAttributes(pw, container.Name)
+	resourceAttributes := getResourceAttributes(pw, container.Name, ownerReferences)
 	resourceAttributesEnvValue := getResourceAttributesEnvVarValue(resourceAttributes)
 	existingEnvNames = InjectConstEnvVarToPodContainer(existingEnvNames, container, otelResourceAttributesEnvVarName, resourceAttributesEnvValue)
 	return existingEnvNames
