9759d54a3ea08216a9ea4b41708754e64d72b184
Mike Dame <mike@odigos.io>
2025-10-17 11:55:46 -0400

    Add components preflight check

--- cli/pkg/preflight/checks.go
+++ cli/pkg/preflight/checks.go
@@ -7,6 +7,7 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/odigos-io/odigos/api/k8sconsts"
 	"github.com/odigos-io/odigos/cli/cmd/resources"
 	"github.com/odigos-io/odigos/cli/pkg/kube"
 	"github.com/odigos-io/odigos/cli/pkg/remote"
@@ -26,6 +27,35 @@ func (c *isOdigosInstalled) Execute(client *kube.Client, ctx context.Context, re
 		} else {
 			return fmt.Errorf("Error detecting Odigos namespace in the current cluster: %s", err)
 		}
+	}
+
+	return nil
+}
+
+type isOdigosReady struct{}
+
+func (c *isOdigosReady) Description() string {
+	return "Checking if Odigos components are ready"
+}
+
+func (c *isOdigosReady) Execute(client *kube.Client, ctx context.Context, isRemote bool) error {
+	ns, err := resources.GetOdigosNamespace(client, ctx)
+	odigletDs, err := client.Clientset.AppsV1().DaemonSets(ns).Get(ctx, k8sconsts.OdigletDaemonSetName, metav1.GetOptions{})
+	if err != nil {
+		return fmt.Errorf("Error getting Odigos odiglet daemonset: %s", err)
+	}
+
+	if odigletDs.Status.DesiredNumberScheduled != odigletDs.Status.NumberReady {
+		return errors.New("Odigos odiglet daemonset is not ready")
+	}
+
+	instrumentor, err := client.Clientset.AppsV1().Deployments(ns).Get(ctx, k8sconsts.InstrumentorDeploymentName, metav1.GetOptions{})
+	if err != nil {
+		return fmt.Errorf("Error getting Odigos instrumentor deployment: %s", err)
+	}
+
+	if instrumentor.Status.Replicas != instrumentor.Status.ReadyReplicas {
+		return errors.New("Odigos instrumentor deployment is not ready")
 	}
 
 	return nil
