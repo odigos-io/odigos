d97f249d0f06fc72ae223e1794e89e751b96dd6e
Aviad Hahami <aviad@odigos.io>
2025-10-20 00:36:23 +0300

    added helm

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -14,6 +14,15 @@ if [ -f /home/vscode/.kube-host/config ]; then
   log "Copied host kubeconfig."
 fi
 
+# Check for helm, if not present, install it
+if ! command -v helm &> /dev/null; then
+  log "Helm not found, installing..."
+  # Installing helm at tag 3.19.0
+  curl -s https://raw.githubusercontent.com/helm/helm/3a5805ea7e8aa385cce278e0026656baa68fa83d/scripts/get-helm-3 | bash
+  log "Helm installed."
+else
+  log "Helm found"
+fi
 # Permissions
 chown -R vscode:vscode /home/vscode/.kube
 
