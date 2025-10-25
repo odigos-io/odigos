15dd7c75f20c3d3944ac818e46467c07ce7460fd
Ron Federman <ron@odigos.io>
2025-10-25 13:39:06 +0300

    test: fix configgrpc tests since we hae extra option added

--- collector/config/configgrpc/configgrpc_test.go
+++ collector/config/configgrpc/configgrpc_test.go
@@ -306,7 +306,7 @@ func TestDefaultGrpcServerSettings(t *testing.T) {
 	}
 	opts, err := gss.getGrpcServerOptions(context.Background(), componenttest.NewNopHost(), componenttest.NewNopTelemetrySettings(), []ToServerOption{})
 	require.NoError(t, err)
-	assert.Len(t, opts, 3)
+	assert.Len(t, opts, 4)
 }
 
 func TestGrpcServerExtraOption(t *testing.T) {
@@ -323,8 +323,8 @@ func TestGrpcServerExtraOption(t *testing.T) {
 		[]ToServerOption{WithGrpcServerOption(extraOpt)},
 	)
 	require.NoError(t, err)
-	assert.Len(t, opts, 4)
-	assert.Equal(t, opts[3], extraOpt)
+	assert.Len(t, opts, 5)
+	assert.Equal(t, opts[4], extraOpt)
 }
 
 func TestGrpcServerValidate(t *testing.T) {
@@ -409,7 +409,7 @@ func TestAllGrpcServerSettingsExceptAuth(t *testing.T) {
 	}
 	opts, err := gss.getGrpcServerOptions(context.Background(), componenttest.NewNopHost(), componenttest.NewNopTelemetrySettings(), []ToServerOption{})
 	require.NoError(t, err)
-	assert.Len(t, opts, 10)
+	assert.Len(t, opts, 11)
 }
 
 func TestGrpcServerAuthSettings(t *testing.T) {
