945bcbf20f5cb814ab3750a63b70a0343c517850
Amir Blum <amirgiraffe@gmail.com>
2025-11-02 14:44:28 +0200

    feat: map custom processor with high hint to happen after metrics calc (#3719)
    Fixes: CORE-345
    
    Allow users to add custom processor with order hint >= 20. those will
    take affect on exporting traces, but will not affect span-metrics
    calculations.
    
    User can do dumb sampling in data-collection container, but still keep
    accurate metrics, for example:
    
    ```yaml
    apiVersion: odigos.io/v1alpha1
    kind: Processor
    metadata:
      name: sample-service-name
      namespace: odigos-system
    spec:
      type: filter
      processorConfig:
        traces:
          span:
            - 'resource.attributes["service.name"] == "frontend" and (FNV(span.trace_id.string) - (FNV(span.trace_id.string) / 100) * 100) < 90'
      orderHint: 20
      signals:
      - TRACES
      collectorRoles:
      - NODE_COLLECTOR
     ```
    
    Will drop 90% of spans from service "frontend"

--- common/config/processor.go
+++ common/config/processor.go
@@ -4,40 +4,61 @@ import (
 	"fmt"
 )
 
-func CrdProcessorToConfig(processors []ProcessorConfigurer) (cfg Config,
-	tracesProcessors []string, metricsProcessors []string, logsProcessors []string, errs map[string]error) {
-	errs = make(map[string]error)
-	processorsMap := GenericMap{}
+type CrdProcessorResults struct {
+	ProcessorsConfig                Config
+	TracesProcessors                []string
+	TracesProcessorsPostSpanMetrics []string
+	MetricsProcessors               []string
+	LogsProcessors                  []string
+	Errs                            map[string]error
+}
+
+func CrdProcessorToConfig(processors []ProcessorConfigurer) CrdProcessorResults {
+	results := CrdProcessorResults{
+		ProcessorsConfig: Config{
+			Processors: GenericMap{},
+		},
+		TracesProcessorsPostSpanMetrics: []string{},
+		MetricsProcessors:               []string{},
+		LogsProcessors:                  []string{},
+		Errs:                            make(map[string]error),
+	}
+
 	for _, processor := range processors {
 		processorKey := fmt.Sprintf("%s/%s", processor.GetType(), processor.GetID())
 		processorsConfig, err := processor.GetConfig()
 		if err != nil {
 			// TODO: write the error to the status of the processor
 			// consider how to handle this error
-			errs[processor.GetID()] = fmt.Errorf("failed to convert processor %q to collector config: %w", processor.GetID(), err)
+			results.Errs[processor.GetID()] = fmt.Errorf("failed to convert processor %q to collector config: %w", processor.GetID(), err)
 			continue
 		}
 		if processorKey == "" || processorsConfig == nil {
 			continue
 		}
-		processorsMap[processorKey] = processorsConfig
+		results.ProcessorsConfig.Processors[processorKey] = processorsConfig
 
 		if isTracingEnabled(processor) {
-			tracesProcessors = append(tracesProcessors, processorKey)
+			// for traces processors, we differentiate between 2:
+			// - regular ones with order hint < 10
+			// - those that have order hint >= 10, which are applied for exporting, but after spanmetrics is calculated.
+			// it can be used to add simple sampling (not tail) in node-collector, which will happen after the span metrics are calculated.
+			if processor.GetOrderHint() < 10 {
+				results.TracesProcessors = append(results.TracesProcessors, processorKey)
+			} else {
+				results.TracesProcessorsPostSpanMetrics = append(results.TracesProcessorsPostSpanMetrics, processorKey)
+			}
 		}
 		if isMetricsEnabled(processor) {
-			metricsProcessors = append(metricsProcessors, processorKey)
+			results.MetricsProcessors = append(results.MetricsProcessors, processorKey)
 		}
 		if isLoggingEnabled(processor) {
-			logsProcessors = append(logsProcessors, processorKey)
+			results.LogsProcessors = append(results.LogsProcessors, processorKey)
 		}
 	}
-	cfg = Config{
-		Processors: processorsMap,
-	}
-	if len(errs) != 0 {
-		return cfg, tracesProcessors, metricsProcessors, logsProcessors, errs
+	if len(results.Errs) != 0 {
+		return results
 	}
 
-	return cfg, tracesProcessors, metricsProcessors, logsProcessors, errs
+	return results
 }
