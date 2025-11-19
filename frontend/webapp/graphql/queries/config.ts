2f7891ed068edc71e6bbcbd62b0ba4ae675542be
Ben Elferink <ben@odigos.io>
2025-11-19 10:35:37 +0200

    refactor: remove system-setting from GraphQL and UI

--- frontend/webapp/graphql/queries/config.ts
+++ frontend/webapp/graphql/queries/config.ts
@@ -1,6 +1,5 @@
 import { gql } from '@apollo/client';
 
-// Define the GraphQL query
 export const GET_CONFIG = gql`
   query GetConfig {
     config {
@@ -8,59 +7,6 @@ export const GET_CONFIG = gql`
       tier
       installationMethod
       installationStatus
-    }
-  }
-`;
-
-export const GET_ODIGOS_CONFIG = gql`
-  query GetOdigosConfig {
-    odigosConfig {
-      karpenterEnabled
-      allowConcurrentAgents
-      uiPaginationLimit
-      centralBackendURL
-      oidc {
-        tenantUrl
-        clientId
-        clientSecret
-      }
-      clusterName
-      imagePrefix
-      ignoredNamespaces
-      ignoredContainers
-      mountMethod
-      agentEnvVarsInjectionMethod
-      customContainerRuntimeSocketPath
-      odigletHealthProbeBindPort
-      rollbackDisabled
-      rollbackGraceTime
-      rollbackStabilityWindow
-      nodeSelector
-      rollout {
-        automaticRolloutDisabled
-      }
-      collectorNode {
-        collectorOwnMetricsPort
-        requestMemoryMiB
-        limitMemoryMiB
-        requestCPUm
-        limitCPUm
-        memoryLimiterLimitMiB
-        memoryLimiterSpikeLimitMiB
-        goMemLimitMiB
-        k8sNodeLogsDirectory
-      }
-      collectorGateway {
-        requestMemoryMiB
-        limitMemoryMiB
-        requestCPUm
-        limitCPUm
-        memoryLimiterLimitMiB
-        memoryLimiterSpikeLimitMiB
-        goMemLimitMiB
-        minReplicas
-        maxReplicas
-      }
     }
   }
 `;
