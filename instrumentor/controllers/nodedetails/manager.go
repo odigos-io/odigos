5d48cd8d68b27a94b762d0e4b1b1a85e88fddf54
tamirdavid1 <tamir@odigos.io>
2025-12-10 12:53:50 +0200

    chore: adding event filter for creation predicate

--- instrumentor/controllers/nodedetails/manager.go
+++ instrumentor/controllers/nodedetails/manager.go
@@ -2,6 +2,7 @@ package nodedetails
 
 import (
 	odigosv1alpha1 "github.com/odigos-io/odigos/api/odigos/v1alpha1"
+	utilpredicate "github.com/odigos-io/odigos/k8sutils/pkg/predicate"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 )
@@ -11,6 +12,7 @@ func SetupWithManager(mgr ctrl.Manager) error {
 	err := builder.
 		ControllerManagedBy(mgr).
 		Named("nodedetails-nodedetails").
+		WithEventFilter(utilpredicate.CreationPredicate{}).
 		For(&odigosv1alpha1.NodeDetails{}).
 		Complete(&NodeDetailsReconciler{
 			Client: mgr.GetClient(),
