a5e9acd83572609acda9c4599244509a023d102b
dan505512 <dan@odigos.io>
2026-01-01 15:53:34 +0200

    Adding JAVA_HOME support for runtime version

--- procdiscovery/pkg/inspectors/java/java.go
+++ procdiscovery/pkg/inspectors/java/java.go
@@ -17,7 +17,11 @@ var (
 	binaries = []string{
 		"/libjvm.so", // Ensures "libjvm.so" appears within a path (because of the "/" prefix)
 	}
+	// versionRegex matches JAVA_VERSION format like "11.0.2+9" or "17.0.1.1+12"
 	versionRegex = regexp.MustCompile(`\d+\.\d+\.\d+(?:\.\d+)?\+\d+`)
+	// javaHomeVersionRegex extracts version from JAVA_HOME paths like "/usr/lib/jvm/zulu7.56.0.11-ca-jdk7.0.352-linux_x64"
+	// It looks for "jdk" followed by a version number
+	javaHomeVersionRegex = regexp.MustCompile(`jdk(\d+(?:\.\d+)*)`)
 )
 
 func (j *JavaInspector) QuickScan(pcx *process.ProcessContext) (common.ProgrammingLanguage, bool) {
@@ -42,9 +46,26 @@ func (j *JavaInspector) DeepScan(pcx *process.ProcessContext) (common.Programmin
 }
 
 func (j *JavaInspector) GetRuntimeVersion(pcx *process.ProcessContext, containerURL string) string {
+	javaVersion := ""
 	if value, exists := pcx.GetDetailedEnvsValue(process.JavaVersionConst); exists {
-		javaVersion := versionRegex.FindString(value)
-		return javaVersion
+		javaVersion = versionRegex.FindString(value)
+	}
+
+	// Prefer JAVA_VERSION to have backward compatibility
+	javaHome := j.GetJavaHome(pcx)
+	if javaHome != "" {
+		subMatch := javaHomeVersionRegex.FindStringSubmatch(javaHome)
+		if len(subMatch) > 1 {
+			javaVersion = subMatch[1]
+		}
+	}
+
+	return javaVersion
+}
+
+func (j *JavaInspector) GetJavaHome(pcx *process.ProcessContext) string {
+	if value, exists := pcx.GetDetailedEnvsValue(process.JavaHomeConst); exists {
+		return value
 	}
 
 	return ""
