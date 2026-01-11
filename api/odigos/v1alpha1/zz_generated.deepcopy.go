8b14ce23149bff350c89cac247c29552778c5ecd
tamirdavid1 <tamir@odigos.io>
2026-01-11 14:32:38 +0200

    chore run cd api; make generate && make manifests

--- api/odigos/v1alpha1/zz_generated.deepcopy.go
+++ api/odigos/v1alpha1/zz_generated.deepcopy.go
@@ -27,7 +27,7 @@ import (
 	"github.com/odigos-io/odigos/api/odigos/v1alpha1/instrumentationrules"
 	"github.com/odigos-io/odigos/common"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
