80e618918c180a09665b5c69745f83adf59e461c
Ben Elferink <ben@odigos.io>
2026-01-22 14:22:46 +0200

    feat: implement file download functionality in useDiagnose hook
    - Added a new `downloadFile` function to handle the downloading of diagnostic files.
    - Integrated the file download logic into the `downloadDiagnose` method, triggering the download when diagnostic data is available.
    - Improved error handling for download failures and added notifications for users.

--- frontend/webapp/hooks/diagnose/useDiagnose.ts
+++ frontend/webapp/hooks/diagnose/useDiagnose.ts
@@ -1,4 +1,5 @@
 import { DOWNLOAD_DIAGNOSE } from '@/graphql';
+import { API } from '@/utils';
 import { useLazyQuery } from '@apollo/client';
 import { useNotificationStore } from '@odigos/ui-kit/store';
 import { Crud, StatusType, type DiagnoseFormData } from '@odigos/ui-kit/types';
@@ -18,9 +19,29 @@ export const useDiagnose = () => {
     onError: (error) => addNotification({ type: StatusType.Error, title: error.name || Crud.Read, message: error.cause?.message || error.message }),
   });
 
+  const downloadFile = async () => {
+    try {
+      const response = await fetch(`${API.BACKEND_HTTP_ORIGIN}/diagnose/download`);
+      if (!response.ok) throw new Error('Failed to download diagnose file');
+
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      window.open(url, '_blank');
+    } catch (error) {
+      console.error(error);
+      addNotification({ type: StatusType.Error, title: 'Error', message: 'Failed to download diagnose file' });
+    }
+  };
+
   return {
     downloadDiagnose: async (payload: DiagnoseFormData) => {
-      await downloadDiagnose({ variables: { input: payload } });
+      const { data } = await downloadDiagnose({ variables: { input: payload } });
+
+      if (data?.diagnose?.stats?.fileCount) {
+        downloadFile();
+      } else {
+        addNotification({ type: StatusType.Error, title: 'Error', message: 'No diagnose data available' });
+      }
     },
   };
 };
