3664120996141115d3b2723b6af95c4e33b8815c
Naty Harary <naty@odigos.io>
2026-01-25 15:12:53 +0200

    feat: add the rollout rate limiter to relevant controllers

--- instrumentor/controllers/agentenabled/action_controller.go
+++ instrumentor/controllers/agentenabled/action_controller.go
@@ -4,13 +4,15 @@ import (
 	"context"
 
 	"github.com/odigos-io/odigos/distros"
+	"github.com/odigos-io/odigos/instrumentor/controllers/agentenabled/rollout"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type ActionReconciler struct {
 	client.Client
-	DistrosProvider *distros.Provider
+	DistrosProvider    *distros.Provider
+	RolloutRateLimiter *rollout.RolloutRateLimiter
 }
 
 func (r *ActionReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
@@ -18,5 +20,5 @@ func (r *ActionReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 	// is created, updated or deleted.
 	// When URLTemplatization actions change, we need to reconcile all workloads
 	// to ensure the correct instrumentation configuration is applied.
-	return reconcileAll(ctx, r.Client, r.DistrosProvider)
+	return reconcileAll(ctx, r.Client, r.DistrosProvider, r.RolloutRateLimiter)
 }
