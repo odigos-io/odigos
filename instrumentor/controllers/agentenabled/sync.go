2433e958a6e40b535582a58c613a448180cac682
tamirdavid1 <tamir@odigos.io>
2026-02-17 13:58:00 +0200

    chore: move the filterTailSamplingRulesForContainer to tailsampling file

--- instrumentor/controllers/agentenabled/sync.go
+++ instrumentor/controllers/agentenabled/sync.go
@@ -524,7 +524,8 @@ func calculateContainerCollectorConfig(containerName string,
 	if err != nil {
 		return nil
 	}
-	noisyOps, relevantOps, costRules := filterSamplingRulesForContainer(samplingRules, runtimeDetails.Language, pw, containerName, containerDistro)
+
+	noisyOps, relevantOps, costRules := sampling.FilterTailSamplingRulesForContainer(samplingRules, runtimeDetails.Language, pw, containerName, containerDistro)
 
 	return &odigosv1.ContainerCollectorConfig{
 		ContainerName: containerName,
@@ -534,55 +535,6 @@ func calculateContainerCollectorConfig(containerName string,
 			CostReductionRules:       costRules,
 		},
 	}
-}
-
-func filterSamplingRulesForContainer(samplingRules *[]odigosv1.Sampling, language common.ProgrammingLanguage,
-	pw k8sconsts.PodWorkload, containerName string, distro *distro.OtelDistro) ([]odigosv1.NoisyOperations, []odigosv1.HighlyRelevantOperation, []odigosv1.CostReductionRule) {
-
-	var filteredNoisyOps []odigosv1.NoisyOperations
-	var filteredRelevantOps []odigosv1.HighlyRelevantOperation
-	var filteredCostRules []odigosv1.CostReductionRule
-
-	for _, samplingRule := range *samplingRules {
-		// Filter and convert NoisyOperations, HighlyRelevantOperations, CostReductionRules.
-		// Exclude SourceScopes and Notes from the rules because we want the instrumentationConfig to be more lightweight.
-
-		// If the distro not supports head sampling, we need the NoisyOperations to be applied at the collector level (tailsampling).
-		if distro.Traces == nil || distro.Traces.HeadSampling == nil || !distro.Traces.HeadSampling.Supported {
-			for _, noisyOp := range samplingRule.Spec.NoisyOperations {
-				if sampling.IsServiceInRuleScope(noisyOp.SourceScopes, pw, containerName, language) {
-					filteredNoisyOps = append(filteredNoisyOps, odigosv1.NoisyOperations{
-						HttpServer:       noisyOp.HttpServer,
-						HttpClient:       noisyOp.HttpClient,
-						PercentageAtMost: noisyOp.PercentageAtMost,
-					})
-				}
-			}
-		}
-
-		// Filter and convert HighlyRelevantOperations - exclude SourceScopes and Notes
-		for _, relevantOp := range samplingRule.Spec.HighlyRelevantOperations {
-			if sampling.IsServiceInRuleScope(relevantOp.SourceScopes, pw, containerName, language) {
-				filteredRelevantOps = append(filteredRelevantOps, odigosv1.HighlyRelevantOperation{
-					Error:             relevantOp.Error,
-					DurationAtLeastMs: relevantOp.DurationAtLeastMs,
-					Operation:         relevantOp.Operation,
-					PercentageAtLeast: relevantOp.PercentageAtLeast,
-				})
-			}
-		}
-
-		for _, costRule := range samplingRule.Spec.CostReductionRules {
-			if sampling.IsServiceInRuleScope(costRule.SourceScopes, pw, containerName, language) {
-				filteredCostRules = append(filteredCostRules, odigosv1.CostReductionRule{
-					Operation:        costRule.Operation,
-					PercentageAtMost: costRule.PercentageAtMost,
-				})
-			}
-		}
-	}
-
-	return filteredNoisyOps, filteredRelevantOps, filteredCostRules
 }
 
 func calculateContainerInstrumentationConfig(containerName string,
