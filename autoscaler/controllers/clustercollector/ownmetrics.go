b2484b4a4c20fc049f90bf10c8e54f72ec9ec022
tamirdavid1 <tamir@odigos.io>
2026-03-01 17:17:04 +0200

    feat: send gateway data to metricstore

--- autoscaler/controllers/clustercollector/ownmetrics.go
+++ autoscaler/controllers/clustercollector/ownmetrics.go
@@ -10,8 +10,10 @@ import (
 
 const (
 	odigosOwnTelemetryOtlpReceiverName = "otlp/odigos-own-metrics-in"
+	gatewayPrometheusReceiverName      = "prometheus/gateway-own-metrics"
 	ownMetricsStorePipelineName        = "metrics/own-metrics"
 	odigosVictoriametricsExporterName  = "otlphttp/odigos-victoriametrics"
+	defaultOwnMetricsScrapeInterval    = "10s"
 )
 
 func receiversConfigForOwnMetricsPrometheus() config.GenericMap {
@@ -19,6 +21,27 @@ func receiversConfigForOwnMetricsPrometheus() config.GenericMap {
 		"protocols": config.GenericMap{
 			"http": config.GenericMap{
 				"endpoint": "0.0.0.0:44318",
+			},
+		},
+	}
+}
+
+func gatewayPrometheusReceiverConfig(ownTelemetryPort int32, interval string) config.GenericMap {
+	if interval == "" {
+		interval = defaultOwnMetricsScrapeInterval
+	}
+	return config.GenericMap{
+		"config": config.GenericMap{
+			"scrape_configs": []config.GenericMap{
+				{
+					"job_name":        "otelcol-gateway",
+					"scrape_interval": interval,
+					"static_configs": []config.GenericMap{
+						{
+							"targets": []string{fmt.Sprintf("127.0.0.1:%d", ownTelemetryPort)},
+						},
+					},
+				},
 			},
 		},
 	}
@@ -41,12 +64,17 @@ func victoriaMetricsExporter(odigosNamespace string) config.GenericMap {
 func addOwnMetricsPipeline(c *config.Config, ownMetricsConfig *odigosv1.OdigosOwnMetricsSettings, odigosNamespace string, ownTelemetryPort int32, destinationPipelineNames []string) error {
 	c.Receivers[odigosOwnTelemetryOtlpReceiverName] = receiversConfigForOwnMetricsPrometheus()
 
+	receivers := []string{odigosOwnTelemetryOtlpReceiverName}
 	exporters := []string{}
 
 	if ownMetricsConfig.SendToOdigosMetricsStore {
 		c.Exporters[odigosVictoriametricsExporterName] = victoriaMetricsExporter(odigosNamespace)
-
 		exporters = append(exporters, odigosVictoriametricsExporterName)
+
+		// Scrape the gateway's own prometheus endpoint so that gateway metrics
+		// are also forwarded to the metrics store, using the configured interval.
+		c.Receivers[gatewayPrometheusReceiverName] = gatewayPrometheusReceiverConfig(ownTelemetryPort, ownMetricsConfig.Interval)
+		receivers = append(receivers, gatewayPrometheusReceiverName)
 	}
 
 	if ownMetricsConfig.SendToMetricsDestinations {
@@ -60,7 +88,7 @@ func addOwnMetricsPipeline(c *config.Config, ownMetricsConfig *odigosv1.OdigosOw
 	}
 
 	c.Service.Pipelines[ownMetricsStorePipelineName] = config.Pipeline{
-		Receivers: []string{odigosOwnTelemetryOtlpReceiverName},
+		Receivers: receivers,
 		Exporters: exporters,
 	}
 
