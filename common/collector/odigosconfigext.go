c7cde6a2b286ba023c4e588b5ffb76db0cef28c7
Amir Blum <amirgiraffe@gmail.com>
2026-03-02 14:34:22 +0200

    chore: lint fix

--- common/collector/odigosconfigext.go
+++ common/collector/odigosconfigext.go
@@ -1,10 +1,15 @@
 package collector
 
 import (
-	commonapi "github.com/odigos-io/odigos/common/api"
 	"go.opentelemetry.io/otel/sdk/resource"
+
+	commonapi "github.com/odigos-io/odigos/common/api"
 )
 
+// OdigosConfigExtension is the interface that must be implemented by an extension that wants to provide Odigos configuration.
+// Every platform (k8s, vm) can implement this interface to provide it's own processor extension to fetch the config from where it is stored.
 type OdigosConfigExtension interface {
+
+	// givin a specific resource, return it's collector config if exists.
 	GetConfigFromResourceAttributes(res resource.Resource) (*commonapi.ContainerCollectorConfig, bool)
 }
