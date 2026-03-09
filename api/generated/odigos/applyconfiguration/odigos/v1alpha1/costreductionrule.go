6b6368c8999b372ea11dc5ac00e230df500beb70
Amir Blum <amirgiraffe@gmail.com>
2026-03-09 19:53:44 +0200

    feat: tail sampling API and types (#4384)
    #### What this PR does / why we need it:
    
    Fixes: CORE-734
    
    Updates to the sampling api.
    
    ## Updated to common (used by both the k8s types and collector config in
    future PR):
    
    - for each rule - added name and disabled fields to allow control in the
    rule level.
    - for http client matcher in head sampling, use route and route prefix
    instead of url path for consistency and usability
    - move all sampling types into a package under `common/api` e.g.
    `common/api/sampling`
    - split it's types into 3 parts: head, tail, and general types in
    sampling.go
    - rename some type from instrumentationconfig_types and moved them into
    this directory.
    
    ## Instrumentor
    
    - apply the changes from common (url path -> route / route prefix)
    
    
    ## api
    
    - add the name and disabled properties per rule
    - 
    
    #### Changelog entry: Does this PR introduce a user-facing bug fix,
    feature, dependency update, or breaking change??
    <!--
    This section will go in the release notes for this version. Is this
    something users should be able to find easily?
    If no, just write "NONE" in the release-note block below.
    If yes, please add a release note in the block below describing this in
    1-2 sentences for our changelog.
    -->
    
    Not yet released, no one is using it so we can make any breaking changes
    we like
    
    ```release-note
    NONE
    ```

--- api/generated/odigos/applyconfiguration/odigos/v1alpha1/costreductionrule.go
+++ api/generated/odigos/applyconfiguration/odigos/v1alpha1/costreductionrule.go
@@ -19,22 +19,40 @@ package v1alpha1
 
 import (
 	k8sconsts "github.com/odigos-io/odigos/api/k8sconsts"
-	api "github.com/odigos-io/odigos/common/api"
+	sampling "github.com/odigos-io/odigos/common/api/sampling"
 )
 
 // CostReductionRuleApplyConfiguration represents a declarative configuration of the CostReductionRule type for use
 // with apply.
 type CostReductionRuleApplyConfiguration struct {
-	SourceScopes     []k8sconsts.SourcesScope          `json:"sourceScopes,omitempty"`
-	Operation        *api.TailSamplingOperationMatcher `json:"operation,omitempty"`
-	PercentageAtMost *float64                          `json:"percentageAtMost,omitempty"`
-	Notes            *string                           `json:"notes,omitempty"`
+	Name             *string                                `json:"name,omitempty"`
+	Disabled         *bool                                  `json:"disabled,omitempty"`
+	SourceScopes     []k8sconsts.SourcesScope               `json:"sourceScopes,omitempty"`
+	Operation        *sampling.TailSamplingOperationMatcher `json:"operation,omitempty"`
+	PercentageAtMost *float64                               `json:"percentageAtMost,omitempty"`
+	Notes            *string                                `json:"notes,omitempty"`
 }
 
 // CostReductionRuleApplyConfiguration constructs a declarative configuration of the CostReductionRule type for use with
 // apply.
 func CostReductionRule() *CostReductionRuleApplyConfiguration {
 	return &CostReductionRuleApplyConfiguration{}
+}
+
+// WithName sets the Name field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the Name field is set to the value of the last call.
+func (b *CostReductionRuleApplyConfiguration) WithName(value string) *CostReductionRuleApplyConfiguration {
+	b.Name = &value
+	return b
+}
+
+// WithDisabled sets the Disabled field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the Disabled field is set to the value of the last call.
+func (b *CostReductionRuleApplyConfiguration) WithDisabled(value bool) *CostReductionRuleApplyConfiguration {
+	b.Disabled = &value
+	return b
 }
 
 // WithSourceScopes adds the given value to the SourceScopes field in the declarative configuration
@@ -50,7 +68,7 @@ func (b *CostReductionRuleApplyConfiguration) WithSourceScopes(values ...k8scons
 // WithOperation sets the Operation field in the declarative configuration to the given value
 // and returns the receiver, so that objects can be built by chaining "With" function invocations.
 // If called multiple times, the Operation field is set to the value of the last call.
-func (b *CostReductionRuleApplyConfiguration) WithOperation(value api.TailSamplingOperationMatcher) *CostReductionRuleApplyConfiguration {
+func (b *CostReductionRuleApplyConfiguration) WithOperation(value sampling.TailSamplingOperationMatcher) *CostReductionRuleApplyConfiguration {
 	b.Operation = &value
 	return b
 }
