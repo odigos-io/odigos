5915be3008491bd86ff686d72299dd1016c3c6c7
daemon1024 <barun1024@gmail.com>
2026-03-12 13:03:08 +0530

    chore(ebpfreceiver): use consts for max attr value
    Signed-off-by: daemon1024 <barun1024@gmail.com>

--- collector/receivers/odigosebpfreceiver/types.go
+++ collector/receivers/odigosebpfreceiver/types.go
@@ -1,7 +1,8 @@
 package odigosebpfreceiver
 
 const (
-	numOfPages = 2048
+	numOfPages       = 2048
+	attrValueMaxSize = 1024
 )
 
 // ReceiverType defines the type of receiver (traces, metrics, or logs)
