de767e3cb2ee2341f655b3033fed7686f7b8bf8d
Ben Elferink <ben@odigos.io>
2026-03-23 15:52:46 +0200

    feat(config): add TypeScript interfaces for configuration YAMLs
    - Introduced interfaces for ConfigYaml and ConfigYamlField to define the structure of configuration YAMLs.
    - Updated the useConfigYamls hook to utilize the new interfaces, enhancing type safety and clarity.
    
    ```release-note
    feat: enhance type safety for configuration management by adding TypeScript interfaces for YAML structures
    ```

--- frontend/webapp/hooks/config/useConfigYamls.ts
+++ frontend/webapp/hooks/config/useConfigYamls.ts
@@ -1,6 +1,26 @@
 import { useQuery } from '@apollo/client';
 import { GET_CONFIG_YAMLS } from '@/graphql';
-import { type FetchedConfigYamls } from '@odigos/ui-kit/types';
+
+// TODO: once we released the kit with the types, we can remove these interfaces and import `FetchedConfigYamls` from the kit
+interface ConfigYamlField {
+  displayName: string;
+  componentType: string;
+  isHelmOnly: boolean;
+  description: string;
+  helmValuePath: string;
+  docsLink?: string | null;
+  componentProps?: string | null;
+}
+interface ConfigYaml {
+  name: string;
+  displayName: string;
+  fields: ConfigYamlField[];
+}
+interface FetchedConfigYamls {
+  configYamls: {
+    configs: ConfigYaml[];
+  };
+}
 
 export const useConfigYamls = () => {
   const { data } = useQuery<FetchedConfigYamls>(GET_CONFIG_YAMLS);
