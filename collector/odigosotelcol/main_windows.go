38caf331ddf575afd287b8d94863ab610f2f18fd
Ray Mileo <3686815+rayray221@users.noreply.github.com>
2026-04-22 17:47:22 -0400

    Add files from make genodigoscol

--- collector/odigosotelcol/main_windows.go
+++ collector/odigosotelcol/main_windows.go
@@ -7,10 +7,9 @@ package main
 import (
 	"errors"
 	"fmt"
-
-	"go.opentelemetry.io/collector/otelcol"
 	"golang.org/x/sys/windows"
 	"golang.org/x/sys/windows/svc"
+	"go.opentelemetry.io/collector/otelcol"
 )
 
 func run(params otelcol.CollectorSettings) error {
