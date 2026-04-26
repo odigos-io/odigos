29d87247a69545680345ceddf48d3de51d3ff1b3
ErikNaz <erik@odigos.io>
2026-04-26 14:01:19 +0300

    Revamp the auto restart for configuration changes

--- collector/distribution/odigos-otelcol/postinstall.sh
+++ collector/distribution/odigos-otelcol/postinstall.sh
@@ -1,4 +1,11 @@
 if command -v systemctl >/dev/null 2>&1; then
+    systemctl daemon-reload
+
     systemctl enable odigos-otelcol.service
     systemctl start odigos-otelcol.service
+
+    systemctl enable odigos-otelcol-config.path
+    systemctl start odigos-otelcol-config.path
+
+    systemctl reload odigos-otelcol.service
 fi
