0a1580e7693e7035bf83c5f812d78e71baf867b6
Tamir David <tamir@odigos.io>
2026-05-04 11:03:22 +0300

    fix: set agent container logs config only when needed (#4787)

--- instrumentor/controllers/agentenabled/dynamicconfig/config.go
+++ instrumentor/controllers/agentenabled/dynamicconfig/config.go
@@ -179,6 +179,7 @@ func CalculateDynamicContainerConfig(
 	d *distro.OtelDistro,
 	enabledSignals signals.EnabledSignals,
 	nodeCollectorsGroup *odigosv1.CollectorsGroup,
+	clusterCollectorsGroup *odigosv1.CollectorsGroup,
 ) (*DynamicContainerConfigs, *odigosv1.AgentDisabledInfo) {
 
 	var collectorConfig *commonapi.ContainerCollectorConfig
@@ -202,14 +203,15 @@ func CalculateDynamicContainerConfig(
 		metricsConfig = agentMetricsConfig
 	}
 
+	// To determine if logs are enabled, we check the gateway collector group receiver signals
+	// because logs won't be present in the data collection (node) collector group.
+	logsEnabled := clusterCollectorsGroup != nil && slices.Contains(clusterCollectorsGroup.Status.ReceiverSignals, common.LogsObservabilitySignal)
 	var logsConfig *odigosv1.AgentLogsConfig
-	// Currently if ebpf log capture is enabled, we write it for all containers.
-	// In the future, we will support scopes it need to be changed.
 	ebpfLogCaptureConfig := logs.CalculateEbpfLogCaptureConfig(d, irls)
-	if ebpfLogCaptureConfig != nil {
-		logsConfig = &odigosv1.AgentLogsConfig{
-			EbpfLogCapture: ebpfLogCaptureConfig,
-		}
+
+	if logsEnabled && ebpfLogCaptureConfig != nil {
+		logsConfig = &odigosv1.AgentLogsConfig{}
+		logsConfig.EbpfLogCapture = ebpfLogCaptureConfig
 	}
 
 	return &DynamicContainerConfigs{
