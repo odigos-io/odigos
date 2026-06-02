8e27e9638c6b2e01b16e5b4fcc9fe80d0801c437
Ben Elferink <ben@odigos.io>
2026-06-02 17:51:09 +0300

    refactor: simplify useK8sManifest hook by removing notification handling
    - Removed the notification handling logic from the useK8sManifest hook, streamlining the fetching process for Kubernetes manifests.
    - Updated the useLazyQuery to focus solely on the query execution without additional error notifications.

--- frontend/webapp/hooks/k8s-manifest/useK8sManifest.ts
+++ frontend/webapp/hooks/k8s-manifest/useK8sManifest.ts
@@ -1,19 +1,14 @@
 import { useCallback } from 'react';
 import { GET_K8S_MANIFEST } from '@/graphql';
 import { useLazyQuery } from '@apollo/client';
-import { useNotificationStore } from '@odigos/ui-kit/store';
-import { Crud, K8sResourceKind, StatusType } from '@odigos/ui-kit/types';
+import { K8sResourceKind } from '@odigos/ui-kit/types';
 
 interface UseK8sManifest {
   fetchK8sManifest: (namespace: string, kind: K8sResourceKind, name: string) => Promise<string | undefined>;
 }
 
 export const useK8sManifest = (): UseK8sManifest => {
-  const { addNotification } = useNotificationStore();
-
-  const [queryK8sManifest] = useLazyQuery<{ k8sManifest: string }>(GET_K8S_MANIFEST, {
-    onError: (error) => addNotification({ type: StatusType.Error, title: error.name || Crud.Read, message: error.cause?.message || error.message }),
-  });
+  const [queryK8sManifest] = useLazyQuery<{ k8sManifest: string }>(GET_K8S_MANIFEST);
 
   const fetchK8sManifest = useCallback(
     async (namespace: string, kind: K8sResourceKind, name: string) => {
