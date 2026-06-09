3b28a03fac5f283ccca15b822d61a9d646c90a55
Mike Dame <mike@odigos.io>
2026-06-09 11:02:49 -0400

    CORE-1030: Support GKE Autopilot (#4991)
    #### What this PR does / why we need it:
    This adds support for GKE Autopilot based on our approval as an
    Autopilot partner
    https://docs.cloud.google.com/kubernetes-engine/docs/how-to/run-autopilot-partner-workloads
    and
    https://docs.cloud.google.com/kubernetes-engine/docs/resources/autopilot-partners
    
    It adds a `gke.autopilot` flag that installs an AllowlistSynchronizer
    (GKE CRD) that will automatically install our Autopilot allowlist to
    support Odiglet's elevated permissions.
    
    It also disables the odiglet's "node label" init phase, since we can't
    modify nodes on GKE Autopilot. This means hostpath mount method won't
    necessarily always work (if an injected pod gets scheduled on a
    different Autopilot node than the odiglet -- though not sure this is
    even possible with how Autopilot scales nodes).
    
    Keeping a downstream copy of our git-on-borg (googlesource git repo)
    here: https://github.com/odigos-io/gke-ap-allowlist, which we could
    automate to submit new allowlists upstream. We will need a new allowlist
    if the odiglet daemonset definition changes
    
    #### Changelog entry: Does this PR introduce a user-facing bug fix,
    feature, dependency update, or breaking change??
    <!--
    This section will go in the release notes for this version. Is this
    something users should be able to find easily?
    If no, just write "NONE" in the release-note block below.
    If yes, please add a release note in the block below describing this in
    1-2 sentences for our changelog.
    -->
    
    ```release-note
    Add GKE Autopilot which disables odiglet node labeling and gke.autopilot flag to install AllowlistSynchronizer
    ```
    
    cherry-pick:v1.28

--- api/k8sconsts/node.go
+++ api/k8sconsts/node.go
@@ -3,4 +3,9 @@ package k8sconsts
 const (
 	NodeNameEnvVar = "NODE_NAME"
 	NodeIPEnvVar   = "NODE_IP"
+
+	GKEAutopilotEnvVar = "GKE_AUTOPILOT"
+
+	// GKEManagedNodeNamePrefix is the prefix for node names on GKE Autopilot clusters.
+	GKEManagedNodeNamePrefix = "gk3-"
 )
