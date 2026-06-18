5906b4921b5fd4af83ef03d552f66b6026752d7f
Ben Elferink <ben@odigos.io>
2026-06-18 10:41:48 +0300

    chore: update @odigos/ui-kit to version 0.0.254 and refactor useConfig hook
    - Updated the dependency version of `@odigos/ui-kit` in `package.json` and `yarn.lock`.
    - Refactored the `useConfig` hook to remove unused properties, simplifying its return value.

--- frontend/webapp/hooks/config/useConfig.ts
+++ frontend/webapp/hooks/config/useConfig.ts
@@ -1,14 +1,12 @@
 'use client';
 
+import type { FetchedConfig } from '@odigos/ui-kit/types';
 import { useApiQuery } from '@odigos/ui-kit/contexts/odigos-api';
-import { FetchedConfig, InstallationStatus, Tier } from '@odigos/ui-kit/types';
 
-export const useConfig = (): { config: FetchedConfig; isReadonly: boolean; isEnterprise: boolean; installationStatus: InstallationStatus } => {
+export const useConfig = (): { config: FetchedConfig; isReadonly: boolean } => {
   const { data: config } = useApiQuery('GET_CONFIG');
 
   const isReadonly = config?.readonly || false;
-  const isEnterprise = (config?.tier && [Tier.Onprem].includes(config.tier)) || false;
-  const installationStatus = config?.installationStatus || InstallationStatus.New;
 
-  return { config, isReadonly, isEnterprise, installationStatus };
+  return { config, isReadonly };
 };
